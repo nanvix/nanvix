20e87292e36439d041dd829e9b7d4a78616f7843
Pedro H. Penna <pedrohenriquepenna@gmail.com>
2014-02-02 19:24:29 -0200

    README added

--- scripts/build-img.sh
+++ scripts/build-img.sh
@@ -24,7 +24,3 @@ echo $1 | sudo -S cp initrd.img /mnt/initrd.img
 echo $1 | sudo -S cp img/menu.lst /mnt/boot/menu.lst
 echo $1 | sudo -S umount /dev/loop2
 echo $1 | sudo -S losetup -d /dev/loop2
-echo $1 | sudo losetup /dev/loop1 Nanvix.img
-cd scripts
-echo $1 | sudo bochs -q -f bochsrc.txt
-echo $1 | sudo losetup -d /dev/loop1
