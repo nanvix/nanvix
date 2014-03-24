250f93e46f1e880708ee80630eda57a370976cb3
Pedro H. Penna <pedrohenriquepenna@gmail.com>
2014-03-23 22:08:42 -0300

    memcmp() ported

---
+++ lib/string/memcmp.c
@@ -0,0 +1,28 @@
+/*
+ * Copyright(C) 2011-2014 Pedro H. Penna <pedrohenriquepenna@gmail.com>
+ * 
+ * string/memcmp.c - memcmp() implementation;
+ */
+
+#include <stdlib.h>
+
+/*
+ * Compares memory areas.
+ */
+int memcmp(const void *s1,const void *s2, size_t n)
+{
+	const unsigned char *p1; /* Indexes s1. */
+	const unsigned char *p2; /* Indexes s2. */
+	
+	p1 = s1;
+	p2 = s2;
+	
+	/* Compare characters. */
+	while (n-- > 0)
+	{
+		if (*p1 != *p2)
+			return (*p1 - *p2);
+	}
+	
+	return (0)
+}
