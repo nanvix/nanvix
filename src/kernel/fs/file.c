8d406ba77b9fe6a4ec5a7ea93f0194dbbe7c6af1
Pedro H. Penna <pedrohenriquepenna@gmail.com>
2014-06-17 19:38:46 -0300

    dirent_search()

--- src/kernel/fs/file.c
+++ src/kernel/fs/file.c
@@ -17,18 +17,19 @@
  * Searches for a directory entry.
  */
 PRIVATE struct dirent *dirent_search
-(struct inode *dinode, const char *filename, struct buffer **buf, int *entry)
+(struct inode *dinode, const char *filename, struct buffer **buf, int create)
 {
-	int i;            /* Working directory entry index. */
-	block_t blk;      /* Working block number.          */
-	struct dirent *d; /* Directory entry.               */
-	int nentries;     /* Number of directory entries.   */
+	int i;              /* Working directory entry index.       */
+	int entry;          /* Index of first free directory entry. */
+	block_t blk;        /* Working block number.                */
+	int nentries;       /* Number of directory entries.         */
+	struct dirent *d;   /* Directory entry.                     */
 	
 	nentries = dinode->size/_SIZEOF_DIRENT;
 	
 	/* Search from very first block. */
 	i = 0;
-	*entry = -1;
+	entry = -1;
 	blk = dinode->blocks[0];		
 	(*buf) = NULL;
 	
@@ -66,9 +67,8 @@ PRIVATE struct dirent *dirent_search
 			if (!kstrncmp(d->d_name, filename, NAME_MAX))
 			{
 				/* Duplicated entry. */
-				if (entry != NULL)
+				if (create)
 				{
-					*entry = -1;
 					brelse((*buf));
 					d = NULL;
 				}
@@ -78,25 +78,52 @@ PRIVATE struct dirent *dirent_search
 		}
 
 		/* Creating entry. */
-		else if (entry != NULL)
+		else if (create)
 		{
 			/* Remember entry index. */
-			if (*entry < 0)
-				*entry = i;
+			if (entry < 0)
+				entry = i;
 		}
 		
 		d++; i++;	
 	}
-	
-	/* Creating entry. */
-	if (entry != NULL)
-		*entry = nentries;
 	
 	/* House keeping. */
 	if ((*buf) != NULL)
 	{
 		brelse((*buf));
 		(*buf) = NULL;
+	}
+	
+	/* Create entry. */
+	if (create)
+	{
+		/* Create entry. */
+		if (entry < 0)
+		{
+			entry = nentries;
+			
+			blk = block_map(dinode, entry*_SIZEOF_DIRENT, 1);
+			
+			/* Failed to create entry. */
+			if (blk == BLOCK_NULL)
+			{
+				curr_proc->errno = -ENOSPC;
+				return (NULL);
+			}
+			
+			dinode->size += sizeof(struct dirent);
+			dinode->flags |= INODE_DIRTY;
+		}
+		
+		else
+			blk = block_map(dinode, entry*_SIZEOF_DIRENT, 0);
+		
+		(*buf) = bread(dinode->dev, blk);
+		entry %= (BLOCK_SIZE/_SIZEOF_DIRENT);
+		d = &((struct dirent *)((*buf)->data))[entry];
+		
+		return (d);
 	}
 	
 	return (NULL);
@@ -110,7 +137,7 @@ PUBLIC ino_t dir_search(struct inode *dinode, const char *filename)
 	struct buffer *buf; /* Block buffer.    */
 	struct dirent *d;   /* Directory entry. */
 	
-	d = dirent_search(dinode, filename, &buf, NULL);
+	d = dirent_search(dinode, filename, &buf, 0);
 	
 	/* Not found. */
 	if (d == NULL)
@@ -129,7 +156,7 @@ PUBLIC int dir_remove(struct inode *dinode, const char *filename)
 	struct dirent *d;   /* Directory entry. */
 	struct inode *file; /* File inode.      */
 	
-	d = dirent_search(dinode, filename, &buf, NULL);
+	d = dirent_search(dinode, filename, &buf, 0);
 	
 	/* Not found. */
 	if (d == NULL)
@@ -172,7 +199,7 @@ PUBLIC int dir_remove(struct inode *dinode, const char *filename)
 	}
 
 	/* Remove directory entry. */
-	d->d_ino = 0;
+	d->d_ino = INODE_NULL;
 	buf->flags |= BUFFER_DIRTY;
 	dinode->time = CURRENT_TIME;
 	dinode->flags |= INODE_DIRTY;
@@ -191,98 +218,15 @@ PUBLIC int dir_remove(struct inode *dinode, const char *filename)
  */
 PUBLIC int dir_add(struct inode *dinode, struct inode *inode, const char *name)
 {
-	int i;              /* Working directory entry index.      */
-	int entry;          /* Inode entry index.                  */
-	block_t blk;        /* Working block number.               */
-	struct buffer *buf; /* Block buffer.                       */
-	struct dirent *d;   /* Disk directory entry.               */
-	int nentries;       /* Actual number of directory entries. */
+	struct buffer *buf; /* Block buffer.         */
+	struct dirent *d;   /* Disk directory entry. */
 	
-	nentries = dinode->size/_SIZEOF_DIRENT;
+	d = dirent_search(dinode, name, &buf, 1);
 	
-	i = 0;
-	entry = -1;
-	blk = inode->blocks[0];
-	buf = NULL;
+	/* Failed to create directory entry. */
+	if (d == NULL)
+		return (-1);
 	
-	/* Search for an empty directory entry. */
-	while(i < nentries)
-	{
-		/* Get valid block. */
-		if (blk == BLOCK_NULL)
-		{
-			i += BLOCK_SIZE/_SIZEOF_DIRENT;
-			blk = block_map(dinode, i*_SIZEOF_DIRENT, 0);
-			continue;
-		}
-		
-		/* Read block buffer. */
-		if (buf == NULL)
-		{
-			buf = bread(dinode->dev, blk);
-			d = buf->data;
-		}
-		
-		/* Get next block. */
-		else if ((char *)d >= BLOCK_SIZE + (char *)buf->data)
-		{
-			brelse(buf);
-			buf = NULL;
-			blk = block_map(dinode, i*_SIZEOF_DIRENT, 0);
-			continue;
-		}
-		
-		/* Empty entry found. */
-		if (d->d_ino == INODE_NULL)
-		{
-			/* Remember entry index. */
-			if (entry < 0)
-				entry = i;
-		}
-		
-		/* Check if entry is not duplicated. */
-		else
-		{
-			/* Found duplicated entry. */
-			if (!kstrncmp(d->d_name, name, NAME_MAX))
-			{
-				brelse(buf);
-				
-				return (-1);
-			}
-		}
-		
-		d++; i++;
-	}
-	
-	/* House keeping. */
-	if (buf != NULL)
-		brelse(buf);
-	
-	/* Create entry. */
-	if (entry < 0)
-	{
-		entry = nentries;
-		
-		blk = block_map(dinode, entry*_SIZEOF_DIRENT, 1);
-		
-		/* Failed to create entry. */
-		if (blk == BLOCK_NULL)
-		{
-			curr_proc->errno = -ENOSPC;
-			return (-1);
-		}
-		
-		dinode->size += sizeof(struct dirent);
-		dinode->flags |= INODE_DIRTY;
-	}
-	
-	else
-		blk = block_map(dinode, entry*_SIZEOF_DIRENT, 0);
-	
-	buf = bread(dinode->dev, blk);
-	entry %= (BLOCK_SIZE/_SIZEOF_DIRENT);
-	d = &((struct dirent *)(buf->data))[entry];
 	kstrncpy(d->d_name, name, NAME_MAX);
 	d->d_ino = inode->num;
 	buf->flags |= BUFFER_DIRTY;
