8876b49f5f6fc84679850b2fe85a74144f824275
Pedro H. Penna <pedrohenriquepenna@gmail.com>
2014-11-11 06:57:10 -0200

    Revert "IRQ priority level implemented"
    This reverts commit f4e0138bc7adc622cf81132d911a7d161f7576ea.

--- src/kernel/core/hwint.c
+++ src/kernel/core/hwint.c
@@ -4,8 +4,6 @@
  * hwint.c - Hardware interrupts.
  */
 
-#include <i386/i386.h>
-#include <asm/util.h>
 #include <nanvix/const.h>
 #include <errno.h>
 
@@ -36,30 +34,4 @@ PUBLIC int set_hwint(int num, void (*handler)(void))
 	hwint_handlers[num] = handler;
 	
 	return (0);
-}
-
-/**
- * @brief Hardware interrupt dispatcher.
- * 
- * @details Dispatches the hardware interrupt to the appropriate interrupt 
- *          handler. To do so, first the processor execution level is raised to
- *          block all interrupts from the same source, and then the specific
- *          interrupt handler routine is called. Finally, after that the
- *          interrupt handler has finished its work, the process level is
- *          restored back.
- * 
- * @param irq Interrupt request number.
- * 
- * @note This function assumes that all interrupts are disable when it is 
- *       called.
- */
-PUBLIC void do_hwint(unsigned irq)
-{
-	processor_raise(int_lvl(irq));
-	
-	enable_interrupts();
-	hwint_handlers[irq]();
-	disable_interrupts();
-	
-	processor_drop();
 }
