ba2873a8228041c29895f0aabaebe5e8458ffe10
Pedro H. Penna <pedrohenriquepenna@gmail.com>
2014-12-12 15:32:00 -0200

    improving portability on stdint.h

--- include/stdint.h
+++ include/stdint.h
@@ -10,15 +10,16 @@
 #ifndef _ASM_FILE_
 
 	/* Signed integers. */
-	typedef char int8_t;   /* 8-bit integer.  */
-	typedef short int16_t; /* 16-bit integer. */
-	typedef int int32_t;   /* 32-bit integer. */
+	typedef __INT8_TYPE__ int8_t;   /* 8-bit integer.  */
+	typedef __INT16_TYPE__ int16_t; /* 16-bit integer. */
+	typedef __INT32_TYPE__ int32_t; /* 32-bit integer. */
+	typedef __INT64_TYPE__ int64_t; /* 64-bit integer. */
 	
 	/* Unsigned integers. */
-	typedef unsigned char uint8_t;           /* 8-bit integer.  */
-	typedef unsigned short uint16_t;         /* 16-bit integer. */
-	typedef unsigned int uint32_t;           /* 32-bit integer. */
-	typedef unsigned long long int uint64_t; /* 64-bit integer. */
+	typedef __UINT8_TYPE__ uint8_t;   /* 8-bit integer.  */
+	typedef __UINT16_TYPE__ uint16_t; /* 16-bit integer. */
+	typedef __UINT32_TYPE__ uint32_t; /* 32-bit integer. */
+	typedef __UINT64_TYPE__ uint64_t; /* 64-bit integer. */
 
 #endif /* _ASM_FILE_*/
 
