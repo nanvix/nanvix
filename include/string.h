cc4759aa60a3f01b9c455354b992b0a9ebd9702c
pedrohenriquepenna@gmail.com <pedrohenriquepenna@gmail.com>
2015-04-05 15:22:13 -0300

    memccpy()

--- include/string.h
+++ include/string.h
@@ -1,7 +1,26 @@
 /*
- * Copyright(C) 2011-2014 Pedro H. Penna <pedrohenriquepenna@gmail.com>
+ * Copyright(C) 2011-2015 Pedro H. Penna <pedrohenriquepenna@gmail.com>
  * 
- * <string.h> - String library.
+ * This file is part of Nanvix.
+ * 
+ * Nanvix is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Nanvix is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Nanvix. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file
+ * 
+ * @brief String library.
  */
 
 #ifndef STRING_H_
@@ -9,49 +28,24 @@
 
 	#include <sys/types.h>
 
-	/*
-	 * Copy bytes in memory.
+	/**
+	 * @defgroup string
+	 * 
+	 * @brief String library.
 	 */
+	/**@{*/
+
+	extern void *memccpy(void *, const void *, int, size_t);
 	extern void *memcpy (void* dest, const void *src, size_t n);
-
-	/*
-	 * Schans for a character in a string.
-	 */
 	extern char *strchr(const char *str, int c);
-
-	/*
-	 * Compares two strings.
-	 */
 	extern int strcmp(const char *str1, const char *str2);
-	
-	/*
-	 * Compares two strings.
-	 */
 	extern int strncmp(const char *str1, const char *str2, size_t n);
-
-	/*
-	 * Concatenates two strings.
-	 */
 	extern char *strcat(char *dest, const char *src);
-
-	/*
-	 * Copies a string.
-	 */
 	extern char *strcpy(char *dest, const char *src);
-
-	/*
-	 * Returns the length of a string.
-	 */
 	extern size_t strlen(const char * str);
-	
-	/*
-	 * Copies a fixed-size string.
-	 */
 	extern char *strncpy(char *dest, const char *src, size_t n);
-	
-	/*
-	 * Gets the length of a fixed-size string.
-	 */
 	extern size_t strnlen(const char *str, size_t maxlen);
+	
+	/**@}*/
 
 #endif /* STRING_H_ */
