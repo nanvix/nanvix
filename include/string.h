ab19c439301ee1914883ca966bc87f245b66333b
Pedro H. Penna <pedrohenriquepenna@gmail.com>
2015-04-12 11:02:35 -0300

    strlen()

--- include/string.h
+++ include/string.h
@@ -49,8 +49,8 @@
 	extern char *strcpy(char *, const char *);
 	extern size_t strcspn(const char *, const char *);
 	extern char *strdup(const char *);
+	extern size_t strlen(const char *);
 	extern int strncmp(const char *str1, const char *str2, size_t n);
-	extern size_t strlen(const char * str);
 	extern char *strncpy(char *dest, const char *src, size_t n);
 	extern size_t strnlen(const char *str, size_t maxlen);
 	
--- src/lib/libc/string/strlen.c
+++ src/lib/libc/string/strlen.c
@@ -1,13 +1,43 @@
 /*
- * Copyright(C) 2011-2014 Pedro H. Penna <pedrohenriquepenna@gmail.com>
+ * Copyright(C) 2011-2015 Pedro H. Penna <pedrohenriquepenna@gmail.com>
  * 
- * stirng/strlen.c - strlen() implementation.
+ * This file is part of Nanvix.
+ * 
+ * Nanvix is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Nanvix is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Nanvix. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file
+ * 
+ * @brief strlen() implementation.
  */
 
 #include <sys/types.h>
 
-/*
- * Returns the length of a string.
+/**
+ * @addtogroup string
+ */
+/**@{*/
+
+/**
+ * @brief Gets string length.
+ * 
+ * @details Computes the number of bytes in the string to which @p str points,
+ *          not including the terminating null byte.
+ * 
+ * @returns The length of @p str; no return value is reserved to indicate an
+ *          error.
  */
 size_t strlen(const char * str)
 {
@@ -20,3 +50,4 @@ size_t strlen(const char * str)
 	return (p - str);
 }
 
+/**@}*/
