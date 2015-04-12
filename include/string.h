a479cf65bdae9a23ae022a266fbf1094640306c3
Pedro H. Penna <pedrohenriquepenna@gmail.com>
2015-04-12 18:07:39 -0300

    strspn()

--- include/string.h
+++ include/string.h
@@ -56,6 +56,7 @@
 	extern char *strncpy(char *, const char *, size_t);
 	extern char *strpbrk(const char *, const char *);
 	extern char *strrchr(const char *, int);
+	extern size_t strspn(const char *, const char *);
 	extern size_t strnlen(const char *str, size_t maxlen);
 	
 	/**@}*/
