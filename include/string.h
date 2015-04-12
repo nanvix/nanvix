c2aa506e5a88e28984caa7918c577a370dc8dbd1
Pedro H. Penna <pedrohenriquepenna@gmail.com>
2015-04-12 18:14:04 -0300

    strstr()

--- include/string.h
+++ include/string.h
@@ -57,6 +57,7 @@
 	extern char *strpbrk(const char *, const char *);
 	extern char *strrchr(const char *, int);
 	extern size_t strspn(const char *, const char *);
+	extern char *strstr(const char *, const char *);
 	extern size_t strnlen(const char *str, size_t maxlen);
 	
 	/**@}*/
---
+++ src/lib/libc/string/strstr.c
@@ -0,0 +1,101 @@
+/*
+ * Copyright (c) 1990 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by the University of
+ *	California, Berkeley and its contributors.
+ * 4. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+/*
+ * Copyright(C) 2011-2015 Pedro H. Penna <pedrohenriquepenna@gmail.com>
+ * 
+ * This file is part of Nanvix.
+ * 
+ * Nanvix is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Nanvix is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Nanvix. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file
+ * 
+ * @brief strstr() implementation.
+ */
+ 
+#include <stdlib.h>
+
+/**
+ * @brief Finds a substring.
+ * 
+ * @details Locates the first occurrence in the string pointed to by @p s1 of
+ *          the sequence of bytes (excluding the terminating null byte) in the
+ *          string pointed to by @p s2.
+ * 
+ * @returns A pointer to the located string or a null pointer if the string is
+ *          not found.
+ * 
+ *          If @p s2 points to a string with zero length, the function returns
+ *          @p s1. 
+ */
+char *strstr(const char *s1, const char *s2)
+{
+	if (*s1 == '\0')
+	{
+		if (*s2 != '\0')
+			return ((char *) NULL);
+		return ((char *) s1);
+	}
+
+	while (*s1 != '\0')
+	{
+		size_t i;
+		i = 0;
+
+		while (1)
+		{
+			if (s2[i] == '\0')
+				return ((char *) s1);
+
+			if (s2[i] != s1[i])
+				break;
+			i++;
+		}
+		s1++;
+	}
+
+	return ((char *) NULL);
+}
