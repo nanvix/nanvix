2697e46303e64ee2ffb90f9ec0788fbba2095c26
pedrohenriquepenna@gmail.com <pedrohenriquepenna@gmail.com>
2015-04-19 15:45:37 -0300

    atol()

---
+++ src/lib/libc/stdlib/atol.c
@@ -0,0 +1,39 @@
+/*
+ * Copyright(C) 2011-2015 Pedro H. Penna <pedrohenriquepenna@gmail.com>
+ * 
+ * This file is part of Nanvix.
+ * 
+ * Nanvix is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Nanvix is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Nanvix. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file
+ * 
+ * @brief atol() implementation.
+ */
+
+#include <stdlib.h>
+
+/**
+ * @brief Converts a string to a long integer.
+ * 
+ * @details Converts the initial portion of the string pointed to by @p str to
+ *          long.
+ * 
+ * @returns The converted value if the value can be represented.
+ */
+long atol(const char *str)
+{
+	return (strtol(str, (char **) NULL, 10));
+}
