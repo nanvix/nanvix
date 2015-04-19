3bb2d0d55be4d38b11ac2e29e3fd0dd82de49198
pedrohenriquepenna@gmail.com <pedrohenriquepenna@gmail.com>
2015-04-19 15:49:40 -0300

    abs()

---
+++ src/lib/libc/stdlib/abs.c
@@ -0,0 +1,37 @@
+/*
+ * Copyright(C) 2011-2015 Pedro H. Penna <pedrohenriquepenna@gmail.com>
+ * 
+ * This file is part of Nanvix.
+ * 
+ * Nanvix is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Nanvix is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Nanvix. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file
+ * 
+ * @brief abs() implementation.
+ */
+
+/**
+ * @brief Returns an integer absolute value.
+ * 
+ * @details Computers the absolute value of its integer operand, @p i. If the
+ *          result cannot be represented, the behavior is undefined.
+ * 
+ * @returns the absolute value of its integer operand.
+ */
+int abs(int i)
+{
+	return ((i < 0) ? -i : i);
+}
