edb96a406a396e8453784cbef29e9cef95dd0eff
pedrohenriquepenna@gmail.com <pedrohenriquepenna@gmail.com>
2015-04-19 10:27:56 -0300

    _XOPEN_SOURCE

--- include/string.h
+++ include/string.h
@@ -27,11 +27,8 @@
 #define STRING_H_
 
 	#include <sys/types.h>
-	
-#ifdef _CX
 	#include <locale.h>
 	#include <stddef.h>
-#endif /* _CX */
 
 	/**
 	 * @defgroup stringlib String Library
@@ -50,46 +47,43 @@
 	 */
 	/**@{*/
 
-#ifdef _XSI
-	extern void *memccpy(void *, const void *, int, size_t);
-#endif /* _XSI */
-
-#ifdef _CX
-	extern char *stpcpy(char *restrict, const char *restrict);
-	extern char *stpncpy(char *restrict, const char *restrict, size_t);
-	extern int strcoll_l(const char *, const char *, locale_t);
-	extern char *strdup(const char *);
-	extern char *strerror_l(int, locale_t);
-	extern int strerror_r(int, char *, size_t);
-	extern char *strndup(const char *, size_t);
-	extern size_t strnlen(const char *, size_t);
-	extern char *strsignal(int);
-	extern char *strtok_r(char *restrict, const char *restrict,char **restrict);
-	extern size_t strxfrm_l(char *restrict,const char *restrict,size_t,locale_t);
-#endif /* _CX */
-
 	extern void *memchr(const void *, int, size_t);
 	extern int memcmp(const void *, const void *, size_t);
-	extern void *memcpy (void *, const void *, size_t);
+	extern void *memcpy (void *restrict, const void *restrict, size_t);
 	extern void *memmove(void *, const void *, size_t);
 	extern void *memset(void *, int, size_t);
-	extern char *strcat(char *, const char *);
+	extern char *stpcpy(char *restrict, const char *restrict);
+	extern char *stpncpy(char *restrict, const char *restrict, size_t);
+	extern char *strcat(char *restrict, const char *restrict);
 	extern char *strchr(const char *, int);
 	extern int strcmp(const char *, const char *);
 	extern int strcoll(const char *, const char *);
-	extern char *strcpy(char *, const char *);
+	extern int strcoll_l(const char *, const char *, locale_t);
+	extern char *strcpy(char *restrict, const char *restrict);
 	extern size_t strcspn(const char *, const char *);
+	extern char *strdup(const char *);
 	extern char *strerror(int);
+	extern char *strerror_l(int, locale_t);
+	extern int strerror_r(int, char *, size_t);
 	extern size_t strlen(const char *);
-	extern char *strncat(char *, const char *, size_t);
-	extern int strncmp(const char *, const char *, size_t);
-	extern char *strncpy(char *, const char *, size_t);
+	extern char *strncat(char *restrict, const char *restrict, size_t);
+	extern int strncmp(const char *restrict, const char *restrict, size_t);
+	extern char *strncpy(char *restrict, const char *restrict, size_t);
+	extern char *strndup(const char *, size_t);
+	extern size_t strnlen(const char *, size_t);
 	extern char *strpbrk(const char *, const char *);
 	extern char *strrchr(const char *, int);
+	extern char *strsignal(int);
 	extern size_t strspn(const char *, const char *);
 	extern char *strstr(const char *, const char *);
 	extern char *strtok(char *, const char *);
+	extern char *strtok_r(char *restrict, const char *restrict,char **restrict);
 	extern size_t strxfrm(char *, const char *, size_t);
+	extern size_t strxfrm_l(char *restrict,const char *restrict,size_t,locale_t);
+
+#if defined(_XOPEN_SOURCE)
+	extern void *memccpy(void *restrict , const void *restrict , int, size_t);
+#endif
 	
 	/**@}*/
 
