c4f1c5bc3c8d23d7ab8f36d17e61a089b2e05155
pedrohenriquepenna@gmail.com <pedrohenriquepenna@gmail.com>
2015-04-21 15:11:49 -0300

    getenv()

--- src/lib/libc/stdlib/getenv.c
+++ src/lib/libc/stdlib/getenv.c
@@ -1,15 +1,51 @@
 /*
- * Copyright(C) 2011-2014 Pedro H. Penna <pedrohenriquepenna@gmail.com>
+ * Copyright(C) 2011-2015 Pedro H. Penna <pedrohenriquepenna@gmail.com>
  * 
- * stdlib/getenv.c - getenv() implementation.
+ * This file is part of Nanvix.
+ * 
+ * Nanvix is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Nanvix is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Nanvix. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file
+ * 
+ * @brief getenv() implementation.
  */
 
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
 
-/*
- * Gets value of an environment variable.
+/**
+ * @brief Gets value of an environment variable.
+ * 
+ * @details Searches the environment of the calling process for the environment
+ *          variable @p name. If it exists, it returns a pointer to the value of
+ *          the environment variable. If the specified environment variable
+ *          cannot be found, a null pointer is returned. The application shall
+ *          ensure that it does not modify the string pointed to by the getenv()
+ *          function.
+ * 
+ *          The returned string pointer might be invalidated or the string
+ *          content might be overwritten by a subsequent call to getenv(),
+ *          setenv(), unsetenv(), or (if supported) putenv() but it is not
+ *          affected by a call to any other function.
+ * 
+ * @returns Upon successful completion, getenv() returns a pointer to a string
+ *          containing the value for the specified name. If the specified
+ *          name cannot be found in the environment of the calling process,
+ *          a null pointer is returned.
  */
 char *getenv(const char *name)
 {
