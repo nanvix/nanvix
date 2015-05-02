230ed1b35678c82a857dee76de84b530ac7b8f67
pedrohenriquepenna@gmail.com <pedrohenriquepenna@gmail.com>
2015-05-02 10:42:19 -0300

    calloc() documentation

--- src/lib/libc/stdlib/calloc.c
+++ src/lib/libc/stdlib/calloc.c
@@ -29,21 +29,8 @@
 /**
  * @brief Allocates a chunk of memory and cleans it.
  * 
- * @details Acllocates unused space for an array of @p n elements each of whose
- *          size in bytes is @p size. The space is initialized to all bits 0.
- * 
- *          The order and contiguity of storage allocated by successive calls to
- *          calloc() is unspecified. The pointer returned if the allocation
- *          succeeds is suitably aligned so that it may be assigned to a pointer
- *          to any type of object and then used to access such an object or an
- *          array of such objects in the space allocated - until the space is
- *          explicitly freed or reallocated. Each such allocation yields a
- *          pointer to an object disjoint from any other object. The pointer
- *          returned points to the start (lowest byte address) of the allocated
- *          space. If the space cannot be allocated, a null pointer is
- *          returned. If the size of the space requested is 0, the behavior is
- *          implementation-defined: the value returned may be either a null
- *          pointer or a unique pointer.
+ * @param n    Number of elements to allocate.
+ * @param size Size of each element.
  * 
  * @returns Upon successful completion with both @n and @p size non-zero,
  *          calloc() returns a pointer to the allocated space. If either @p n
