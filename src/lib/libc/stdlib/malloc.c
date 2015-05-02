ddad28e46fdcfcb5d610303731d9dc8c02e8d7c5
pedrohenriquepenna@gmail.com <pedrohenriquepenna@gmail.com>
2015-05-02 13:40:52 -0300

    malloc() documentation

--- src/lib/libc/stdlib/malloc.c
+++ src/lib/libc/stdlib/malloc.c
@@ -131,20 +131,7 @@ static void *expand(unsigned nblocks)
 /**
  * @brief Allocates memory.
  * 
- * @details Allocates unused space for an object whose size in bytes is
- *          specified by @p size and whose value is unspecified.
- * 
- *          The order and contiguity of storage allocated by successive calls to
- *          malloc() is unspecified. The pointer returned if the allocation
- *          succeeds is suitably aligned so that it may be assigned to a pointer
- *          to any type of object and then used to access such an object in the
- *          space allocated - until the space is explicitly freed or
- *          reallocated. Each such allocation yields a pointer to an object
- *          disjoint from any other object. The pointer returned points to the
- *          start (lowest byte address) of the allocated space. If the space
- *          cannot be allocated, a null pointer is returned. If the size of the
- *          space requested is 0, the behavior is implementation-defined: the
- *          value returned may be either a null pointer or a unique pointer.
+ * @param size Number of bytes to allocate.
  * 
  * @returns Upon successful completion with size not equal to 0, malloc() 
  *          returns a pointer to the allocated space. If size is 0, either a
