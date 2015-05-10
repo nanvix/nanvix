4f1acedacceeb9d64617fe6ea23bdf381efda49b
pedrohenriquepenna@gmail.com <pedrohenriquepenna@gmail.com>
2015-05-10 11:14:36 -0300

    bug fix: not looking for = on getenv()

--- src/lib/libc/stdlib/getenv.c
+++ src/lib/libc/stdlib/getenv.c
@@ -28,29 +28,60 @@
 #include <string.h>
 
 /**
- * @brief Gets value of an environment variable.
+ * @brief Finds environment variable.
  * 
- * @param name Variable name.
+ * @param name   Variable name.
+ * @param offset Variable offset store location.
  * 
- * @returns Upon successful completion, getenv() returns a pointer to a string
- *          containing the value for the specified name. If the specified
- *          name cannot be found in the environment of the calling process,
- *          a null pointer is returned.
+ * @returns A pointer to a string containing the value for the specified name,
+ *          upon successful completion. If the specified name cannot be found
+ *          in the environment of the calling process, a null pointer is
+ *          returned instead.
  */
-char *getenv(const char *name)
+char *findenv(const char *name, int *offset)
 {
-	char **p;   /* Working environment variable. */
-	int length; /* Environment variable name.    */
-	
-	length = strlen(name);
-	
+	register int length; /* Variable name length.         */
+	const char *c;       /* Environment variable name.    */
+	register char **p;   /* Working environment variable. */
+
+	c = name;
+	length = 0;
+	while ((*c != '\0') && (*c != '='))
+	{
+		c++;
+		length++;
+	}
+
 	/* Search for environment variable. */
 	for (p = environ; *p != NULL; p++)
 	{
 		/* Found. */
-		if (!strncmp(name, *p, length))
-			return (*p + length + 1);
+		if (!strncmp (name, *p, length))
+		{
+			if (*(c = *p + length) == '=')
+			{
+				*offset = p - environ;
+				return ((char *) (++c));
+			}
+		}
 	}
 	
 	return (NULL);
+}
+
+/**
+ * @brief Gets value of an environment variable.
+ * 
+ * @param name Variable name.
+ * 
+ * @returns A pointer to a string containing the value for the specified name,
+ *          upon successful completion. If the specified name cannot be found
+ *          in the environment of the calling process, a null pointer is
+ *          returned instead.
+ */
+char *getenv(const char *name)
+{
+	int offset;
+ 
+	return (findenv(name, &offset));
 }
