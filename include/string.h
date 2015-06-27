c73e027a708dd1eee988da0af3ec0d9d04de0045
Pedro H. Penna <pedrohenriquepenna@gmail.com>
2015-06-27 18:43:30 -0300

    formatting string.h

--- include/string.h
+++ include/string.h
@@ -34,57 +34,55 @@
 	 * @defgroup stringlib String Library
 	 * 
 	 * @brief String operations.
-	 * 
-	 * @todo stpcpy()
-	 * @todo stpncpy()
-	 * @todo strcoll_l()
-	 * @todo strerror_l()
-	 * @todo strerror_r()
-	 * @todo strndup()
-	 * @todo strsignal()
-	 * @todo strtok_r()
-	 * @todo strxfrm_l()
 	 */
 	/**@{*/
-
-	/* Forward definitions. */
+	
+	/* ISO C */
 	extern void *memchr(const void *, int, size_t);
 	extern int memcmp(const void *, const void *, size_t);
 	extern void *memcpy (void *restrict, const void *restrict, size_t);
 	extern void *memmove(void *, const void *, size_t);
 	extern void *memset(void *, int, size_t);
-	extern char *stpcpy(char *restrict, const char *restrict);
-	extern char *stpncpy(char *restrict, const char *restrict, size_t);
 	extern char *strcat(char *restrict, const char *restrict);
 	extern char *strchr(const char *, int);
 	extern int strcmp(const char *, const char *);
 	extern int strcoll(const char *, const char *);
-	extern int strcoll_l(const char *, const char *, locale_t);
 	extern char *strcpy(char *restrict, const char *restrict);
 	extern size_t strcspn(const char *, const char *);
-	extern char *strdup(const char *);
 	extern char *strerror(int);
-	extern char *strerror_l(int, locale_t);
-	extern int strerror_r(int, char *, size_t);
 	extern size_t strlen(const char *);
 	extern char *strncat(char *restrict, const char *restrict, size_t);
 	extern int strncmp(const char *restrict, const char *restrict, size_t);
 	extern char *strncpy(char *restrict, const char *restrict, size_t);
-	extern char *strndup(const char *, size_t);
-	extern size_t strnlen(const char *, size_t);
 	extern char *strpbrk(const char *, const char *);
 	extern char *strrchr(const char *, int);
-	extern char *strsignal(int);
 	extern size_t strspn(const char *, const char *);
 	extern char *strstr(const char *, const char *);
 	extern char *strtok(char *, const char *);
-	extern char *strtok_r(char *restrict, const char *restrict,char **restrict);
 	extern size_t strxfrm(char *, const char *, size_t);
+	
+	/* POSIX C */
+#if defined(_POSIX_C_SOURCE) || defined(_XOPEN_SOURCE)
+
+	extern char *stpcpy(char *restrict, const char *restrict);
+	extern char *stpncpy(char *restrict, const char *restrict, size_t);
+	extern int strcoll_l(const char *, const char *, locale_t);
+	extern char *strdup(const char *);
+	extern char *strerror_l(int, locale_t);
+	extern int strerror_r(int, char *, size_t);
+	extern char *strndup(const char *, size_t);
+	extern size_t strnlen(const char *, size_t);
+	extern char *strsignal(int);
+	extern char *strtok_r(char *restrict, const char *restrict,char **restrict);
 	extern size_t strxfrm_l(char *restrict,const char *restrict,size_t,locale_t);
 
+#endif
+	
+	/* XSI C */
 #if defined(_XOPEN_SOURCE)
-	/* Forward definitions. */
+
 	extern void *memccpy(void *restrict , const void *restrict , int, size_t);
+
 #endif
 	
 	/**@}*/
