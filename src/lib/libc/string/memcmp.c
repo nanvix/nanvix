fe2cff500bf3945c99dba45b95c8860741f5bbfb
Pedro H. Penna <pedrohenriquepenna@gmail.com>
2015-07-04 16:26:19 -0300

    memcmp()

--- src/lib/libc/string/memcmp.c
+++ src/lib/libc/string/memcmp.c
@@ -1,37 +1,4 @@
 /*
- * Copyright (c) 1990 The Regents of the University of California.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by the University of
- *	California, Berkeley and its contributors.
- * 4. Neither the name of the University nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- */
-
-/*
  * Copyright(C) 2011-2015 Pedro H. Penna <pedrohenriquepenna@gmail.com>
  * 
  * This file is part of Nanvix.
@@ -61,14 +28,9 @@
 /**
  * @brief Compares bytes in memory.
  * 
- * @details Compares the first @p n bytes (each interpreted as unsigned char)
- *          of the object pointed to by @p s1 to the first @p n bytes of the
- *          object pointed to by @p s2.
- * 
- *          The sign of a non-zero return value is determined by the sign of
- *          the difference between the values of the first pair of bytes
- *          (both interpreted as type unsigned char) that differ in the objects
- *          being compared.
+ * @param s1 Pointer to object 1.
+ * @param s2 Pointer to object 2.
+ * @param n  Number of bytes to compare.
  * 
  * @returns An integer greater than, equal to or less than 0, if the object
  *          pointed to by @p s1 is greater than, equal to or less than the
@@ -76,14 +38,16 @@
  */
 int memcmp(const void *s1, const void *s2, size_t n)
 {
-	if (n != 0)
+	const unsigned char *p1;
+	const unsigned char *p2;
+	
+	p1 = s1;
+	p2 = s2;
+	
+	while (n-- > 0)
 	{
-		const unsigned char *p1 = s1, *p2 = s2;
-
-		do {
-			if (*p1++ != *p2++)
-				return (*--p1 - *--p2);
-		} while (--n != 0);
+		if (*p1++ != *p2++)
+			return (*--p1 - *--p2);
 	}
 	
 	return (0);
