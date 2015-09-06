1b39ee4780bca5c45ac093a463a995a9f4c88e75
Pedro H. Penna <pedrohenriquepenna@gmail.com>
2015-09-06 11:01:00 -0300

    changing copyright notices

--- src/lib/libc/unistd/gticks.c
+++ src/lib/libc/unistd/gticks.c
@@ -1,7 +1,20 @@
 /*
- * Copyleft(C) 2015 Davidson Francis <davidsondfgl@gmail.com>
- * 
- * unistd/gticks.c - gticks() system call.
+ * Copyright(C) 2015-2015 Davidson Francis <davidsondfgl@hotmail.com>
+ *
+ * This file is part of Nanvix.
+ *
+ * Nanvix is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Nanvix is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Nanvix. If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <nanvix/syscall.h>
--- src/ubin/nim/nim.c
+++ src/ubin/nim/nim.c
@@ -1,8 +1,21 @@
 /*
- * Copyleft(C) 2015 - Davidson Francis <davidsondfgl@gmail.com>
- * Nim game
+ * Copyright(C) 2015-2015 Davidson Francis <davidsondfgl@hotmail.com>
+ *
+ * This file is part of Nanvix.
+ *
+ * Nanvix is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Nanvix is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Nanvix. If not, see <http://www.gnu.org/licenses/>.
  */
-
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
