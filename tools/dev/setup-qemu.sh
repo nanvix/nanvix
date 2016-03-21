8c9ffd962e38201ddb48fd6adb2203a1739dfcbf
Pedro H. Penna <pedrohenriquepenna@gmail.com>
2016-03-20 23:42:19 -0300

    setup-qemu script

---
+++ tools/dev/setup-qemu.sh
@@ -0,0 +1,43 @@
+# 
+# Copyright(C) 2011-2016 Pedro H. Penna <pedrohenriquepenna@gmail.com> 
+#
+# This file is part of Nanvix.
+#
+# Nanvix is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Nanvix is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Nanvix.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# NOTES:
+#   - This script should work in any Debian-based Linux distribution.
+#   - You should run this script with superuser privileges.
+#
+
+# Set working directory.
+export CURDIR=`pwd`
+export WORKDIR=$CURDIR/nanvix-toolchain
+mkdir -p $WORKDIR
+cd $WORKDIR
+
+# Get bochs.
+wget "http://wiki.qemu-project.org/download/qemu-2.5.0.tar.bz2"
+
+# Build Bochs
+tar -xjvf qemu-2.5.0.tar.bz2
+./configure
+make all
+make install
+
+# Cleans files.
+cd $WORKDIR
+cd ..
+rm -R -f $WORKDIR
