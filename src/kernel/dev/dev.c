05d29902f768535996d8e2927c985b133901aaa0
Subhra Sarkar <rurtle.coder@gmail.com>
2016-04-25 21:59:57 -0400

    Cosmetic changes

--- src/kernel/dev/dev.c
+++ src/kernel/dev/dev.c
@@ -1,6 +1,6 @@
 /*
  * Copyright(C) 2011-2016 Pedro H. Penna <pedrohenriquepenna@gmail.com>
- * 				2016-2016 Subhra S. Sarkar <rurtle.coder@gmail.com>
+ *              2016-2016 Subhra S. Sarkar <rurtle.coder@gmail.com>
  * 
  * This file is part of Nanvix.
  * 
