efb29f6998d3aebd75116d3316f3b1ef57355a33
Subhra Sarkar <rurtle.coder@gmail.com>
2016-05-11 01:29:14 -0400

    Switched from linux 0.01 implementation of BCD to BIN conversion to the procedure mentioned in OSDev CMOS page

--- src/kernel/arch/x86/cmos.c
+++ src/kernel/arch/x86/cmos.c
@@ -33,24 +33,37 @@ static unsigned int read_cmos_reg(unsigned int addr)
 PUBLIC void cmos_init(void)
 {
 	struct cmos cmos_tm;
+	unsigned int registerB;		/* Controls format of RTC bytes */
 	/* Well, ideally we should add checks for all cmos attributes */
  	/* to ensure we don't get the same value twice                */
 	/* NOTE: Need to discuss this with the group                  */
 	do
 	{
-		cmos_tm.cmos_sec  = read_cmos_reg(0x00);
-		cmos_tm.cmos_min  = read_cmos_reg(0x02);
-		cmos_tm.cmos_hour = read_cmos_reg(0x04);
-		cmos_tm.cmos_dom  = read_cmos_reg(0x07);
-		cmos_tm.cmos_mon  = read_cmos_reg(0x08);
-		cmos_tm.cmos_year = read_cmos_reg(0x09);
-	} while (cmos_tm.cmos_sec != read_cmos_reg(0));
+		cmos_tm.sec  = read_cmos_reg(0x00);
+		cmos_tm.min  = read_cmos_reg(0x02);
+		cmos_tm.hour = read_cmos_reg(0x04);
+		cmos_tm.dom  = read_cmos_reg(0x07);
+		cmos_tm.mon  = read_cmos_reg(0x08);
+		cmos_tm.year = read_cmos_reg(0x09);
+	} while (cmos_tm.sec != read_cmos_reg(0));
 
-	/* Copied from linux-0.01. Thanks Mr. Torvalds! */
-	BCD_TO_BIN(cmos_tm.cmos_sec);
-	BCD_TO_BIN(cmos_tm.cmos_min);
-	BCD_TO_BIN(cmos_tm.cmos_hour);
-	BCD_TO_BIN(cmos_tm.cmos_dom);
-	BCD_TO_BIN(cmos_tm.cmos_mon);
-	BCD_TO_BIN(cmos_tm.cmos_year);
+	/* Read output format information from CMOS registers */
+	registerB = read_cmos_reg(0x0B);
+
+	/* If output is in BCD format, convert it to binary */
+	if (!(registerB & 0x04))
+	{
+		cmos_tm.sec  = (cmos_tm.sec & 0x0F) + ((cmos_tm.sec / 16) * 10);
+		cmos_tm.min  = (cmos_tm.min & 0x0F) + ((cmos_tm.min / 16) * 10);
+		cmos_tm.hour = ( (cmos_tm.hour & 0x0F) + \
+						(((cmos_tm.hour & 0x70) / 16) * 10) ) | \
+						(cmos_tm.hour & 0x80);
+		cmos_tm.dom  = (cmos_tm.dom & 0x0F) + ((cmos_tm.dom / 16) * 10);
+		cmos_tm.mon  = (cmos_tm.mon & 0x0F) + ((cmos_tm.mon / 16) * 10);
+		cmos_tm.year = (cmos_tm.year & 0x0F) + ((cmos_tm.year / 16) * 10);
+	}
+
+	/* Convert 12 hr clock to 24 hr clock if necessary */
+	if (!(registerB & 0x02) && (cmos_tm.hour & 0x80))
+		cmos_tm.hour = ((cmos_tm.hour & 0x7F) + 12) % 24;
 }
