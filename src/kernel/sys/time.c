ec0202ffd7d5f9a21b92a0f3d4774e0dddaa90c9
Subhra Sarkar <rurtle.coder@gmail.com>
2016-05-31 17:19:23 -0400

    Added time system call implementation which basically returns the number of clock ticks since Epoch

---
+++ src/kernel/sys/time.c
@@ -0,0 +1,30 @@
+/*
+ * Copyright(C) 2011-2016 Pedro H. Penna <pedrohenriquepenna@gmail.com>
+ * 		2016-2016 Subhra S. Sarkar <rurtle.coder@gmail.com>
+ * 
+ * This file is part of Nanvix.
+ * 
+ * Nanvix is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Nanvix is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Nanvix. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <nanvix/clock.h>
+#include <stdint.h>
+
+/*
+ * @brief Returns the current time since Epoch (00:00:00 UTC 1st Jan, 1970)
+ */
+PUBLIC uint64_t sys_time(void)
+{
+	return ((uint64_t)(cmos_getticks() + ticks));
+}
