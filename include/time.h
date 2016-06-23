1617be672b143d01de16fdaf7ba93eb7ac04d8d0
Subhra Sarkar <rurtle.coder@gmail.com>
2016-06-23 02:52:30 -0400

    Place holder for external declaration of time system call wrapper

---
+++ include/time.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright(C) 2011-2016 Pedro H. Penna <pedrohenriquepenna@gmail.com>
+ * 		2016-2016 Subhra S. Sarkar <rurtle.coder@gmail.com>
+ * 
+ * This file is part of Nanvix.
+ * 
+ * Nanvix is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Nanvix is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Nanvix. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef TIME_H_
+#define TIME_H_
+#ifndef _ASM_FILE_
+
+	#include <sys/types.h>
+
+	/*
+ 	 * Returns time in seconds since Epoch
+ 	 */
+	extern time_t time(time_t *tloc);
+
+#endif	/* _ASM_FILE_ */
+#endif	/* TIME_H_ */
