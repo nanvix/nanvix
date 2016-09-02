9ff356a3382c3a8273d75eff54cca2a6c579d110
Pedro H. Penna <pedrohenriquepenna@gmail.com>
2016-09-02 13:19:28 -0300

    using INITRD

--- include/nanvix/config.h
+++ include/nanvix/config.h
@@ -36,8 +36,8 @@
 	#define INITRD_SIZE      0x80000 /* Init RAM disk size.             */
 	#define NR_INODES           1024 /* Number of in-core inodes.       */
 	#define NR_SUPERBLOCKS         4 /* Number of in-core super blocks. */
-	#define ROOT_DEV          0x0101 /* Root device number.             */
-	#define SWAP_DEV          0x0101 /* Swap device number.             */
+	#define ROOT_DEV          0x0001 /* Root device number.             */
+	#define SWAP_DEV          0x0001 /* Swap device number.             */
 	#define NR_FILES             256 /* Number of opened files.         */
 	#define NR_REGIONS           128 /* Number of memory regions.       */
 	#define NR_BUFFERS           256 /* Number of block buffers.        */
