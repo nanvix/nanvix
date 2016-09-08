81d83673755459a8bffc8c69ae08b24d7ada5afe
Theldus <davidsondfgl@gmail.com>
2016-09-08 20:30:57 -0300

    removing legacy functions from strings.h

--- include/strings.h
+++ include/strings.h
@@ -14,18 +14,6 @@
 
 _BEGIN_STD_C
 
-#if !defined __STRICT_ANSI__ && _POSIX_VERSION < 200809L
-/* 
- * Marked LEGACY in Open Group Base Specifications Issue 6/IEEE Std 1003.1-2004
- * Removed from Open Group Base Specifications Issue 7/IEEE Std 1003.1-2008
- */
-int	 _EXFUN(bcmp,(const void *, const void *, size_t));
-void	 _EXFUN(bcopy,(const void *, void *, size_t));
-void	 _EXFUN(bzero,(void *, size_t));
-char 	*_EXFUN(index,(const char *, int));
-char 	*_EXFUN(rindex,(const char *, int));
-#endif /* ! __STRICT_ANSI__ */
-
 int	 _EXFUN(ffs,(int));
 int	 _EXFUN(strcasecmp,(const char *, const char *));
 int	 _EXFUN(strncasecmp,(const char *, const char *, size_t));
