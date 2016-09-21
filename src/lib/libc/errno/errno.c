f7d18d3ef540153ec58d5152e067717c7f73a1c1
Theldus <davidsondfgl@gmail.com>
2016-09-20 22:09:20 -0300

    adding newlib errno/ to nanvix

--- src/lib/libc/errno/errno.c
+++ src/lib/libc/errno/errno.c
@@ -1,30 +1,16 @@
-/*
- * Copyright(C) 2011-2016 Pedro H. Penna <pedrohenriquepenna@gmail.com>
- * 
- * This file is part of Nanvix.
- * 
- * Nanvix is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
- * (at your option) any later version.
- * 
- * Nanvix is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with Nanvix. If not, see <http://www.gnu.org/licenses/>.
- */
+/* The errno variable is stored in the reentrancy structure.  This
+   function returns its address for use by the macro errno defined in
+   errno.h.  */
 
-/**
- * @file
- * 
- * @brief errno variable.
- */
- 
-/**
- * @brief Number of last error.
- */
-int errno = 0;
- 
+#include <errno.h>
+#include <reent.h>
+
+#ifndef _REENT_ONLY
+
+int *
+__errno ()
+{
+  return &_REENT->_errno;
+}
+
+#endif
