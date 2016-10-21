a860fd6de14e85932f155a4370ba8847e3692949
Pedro H. Penna <pedrohenriquepenna@gmail.com>
2016-10-21 01:46:37 -0200

    ispunct()

--- src/lib/libc/ctype/ispunct.c
+++ src/lib/libc/ctype/ispunct.c
@@ -16,45 +16,25 @@
  * You should have received a copy of the GNU General Public License
  * along with Nanvix. If not, see <http://www.gnu.org/licenses/>.
  */
-/*
-FUNCTION
-<<ispunct>>---punctuation character predicate
-
-INDEX
-ispunct
-
-ANSI_SYNOPSIS
-#include <ctype.h>
-int ispunct(int <[c]>);
-
-TRAD_SYNOPSIS
-#include <ctype.h>
-int ispunct(<[c]>);
-
-DESCRIPTION
-<<ispunct>> is a macro which classifies ASCII integer values by table
-lookup.  It is a predicate returning non-zero for printable
-punctuation characters, and 0 for other characters.  It is defined only
-if <[c]> is representable as an unsigned char or if <[c]> is EOF.
-
-You can use a compiled subroutine instead of the macro definition by
-undefining the macro using `<<#undef ispunct>>'.
-
-RETURNS
-<<ispunct>> returns non-zero if <[c]> is a printable punctuation character 
-(<<isgraph(<[c]>) && !isalnum(<[c]>)>>).
-
-PORTABILITY
-<<ispunct>> is ANSI C.
-
-No supporting OS subroutines are required.
-*/
 
 #include <_ansi.h>
 #include <ctype.h>
 
-int
-_DEFUN(ispunct,(c),int c)
+/**
+ * @brief Tests for a punctuation character. 
+ *
+ * @details Tests whether @p c is a character of class punct in the
+ * current locale. The @p c argument is an int, the value of which the
+ * application shall ensure is representable as an unsigned char or
+ * equal to the value of the macro #EOF. If the argument has any other
+ * value, the behavior is undefined.
+ *
+ * @param Character to test.
+ *
+ * @returns Returns non-zero if @p c is a punctuation character;
+ * otherwise, it returns 0.
+ */
+int ispunct(int c)
 {
 	return(__ctype_ptr__[c+1] & _P);
 }
