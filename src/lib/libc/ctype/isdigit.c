7d6932ad0cb16fc8609a1036ee5418555c6de81a
Pedro H. Penna <pedrohenriquepenna@gmail.com>
2016-10-21 01:35:01 -0200

    isdigit()

--- src/lib/libc/ctype/isdigit.c
+++ src/lib/libc/ctype/isdigit.c
@@ -16,44 +16,25 @@
  * You should have received a copy of the GNU General Public License
  * along with Nanvix. If not, see <http://www.gnu.org/licenses/>.
  */
-/*
-FUNCTION
-<<isdigit>>---decimal digit predicate
-
-INDEX
-isdigit
-
-ANSI_SYNOPSIS
-#include <ctype.h>
-int isdigit(int <[c]>);
-
-TRAD_SYNOPSIS
-#include <ctype.h>
-int isdigit(<[c]>);
-
-DESCRIPTION
-<<isdigit>> is a macro which classifies ASCII integer values by table
-lookup.  It is a predicate returning non-zero for decimal digits, and 0 for
-other characters.  It is defined only if <[c]> is representable as an
-unsigned char or if <[c]> is EOF.
-
-You can use a compiled subroutine instead of the macro definition by
-undefining the macro using `<<#undef isdigit>>'.
-
-RETURNS
-<<isdigit>> returns non-zero if <[c]> is a decimal digit (<<0>>--<<9>>).
-
-PORTABILITY
-<<isdigit>> is ANSI C.
-
-No supporting OS subroutines are required.
-*/
 
 #include <_ansi.h>
 #include <ctype.h>
 
-int
-_DEFUN(isdigit,(c),int c)
+/**
+ * @brief Tests for a decimal digit.
+ *
+ * @details Tests whether @p c is a character of class digit in the
+ * current locale. The @p c argument is an int, the value of which the
+ * application shall ensure is representable as an unsigned char or
+ * equal to the value of the macro #EOF. If the argument has any other
+ * value, the behavior is undefined.
+ *
+ * @param Character to test.
+ *
+ * @returns Returns non-zero if @p c is a decimal digit; otherwise, it
+ * returns 0.
+ */
+int isdigit(int c)
 {
 	return(__ctype_ptr__[c+1] & _N);
 }
