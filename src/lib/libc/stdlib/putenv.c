72480d69e81db15c57c05708a0891392b4e45cfe
Theldus <davidsondfgl@gmail.com>
2016-11-02 03:17:28 -0200

    conditional compiling

--- src/lib/libc/stdlib/putenv.c
+++ src/lib/libc/stdlib/putenv.c
@@ -1,3 +1,22 @@
+/*
+ * Copyright(C) 2016 Davidson Francis <davidsondfgl@gmail.com>
+ * 
+ * This file is part of Nanvix.
+ * 
+ * Nanvix is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Nanvix is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Nanvix. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 /*-
  * Copyright (c) 1988 The Regents of the University of California.
  * All rights reserved.
@@ -22,11 +41,19 @@
 #include <stdlib.h>
 #include <string.h>
 
-int
-_DEFUN (putenv, (str),
-	char *str)
+#ifdef _XOPEN_SOURCE
+
+/**
+ * @brief Changes or add a value to an environment.
+ *
+ * @details Uses the @p str argument to set environment variable values.
+ *
+ * @return Returns 0 in success, otherwise, an non-zero value.
+ */
+int putenv(char *str)
 {
   return _putenv_r (_REENT, str);
 }
 
+#endif /* _XOPEN_SOURCE */
 #endif /* !_REENT_ONLY */
