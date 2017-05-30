89df3ead9c01c7fd8e446189c88045dd077ab593
Turbolay <clement.ogame@gmail.com>
2017-05-30 13:59:59 -0300

    kmain recognizes debug parameter and calls dbg_init()

--- src/kernel/init/main.c
+++ src/kernel/init/main.c
@@ -1,6 +1,7 @@
 /*
  * Copyright(C) 2011-2016 Pedro H. Penna   <pedrohenriquepenna@gmail.com>
  *              2016-2016 Subhra S. Sarkar <rurtle.coder@gmail.com>
+ *              2017-2017 Clement Rouquier <clementrouquier@gmail.com>
  * 
  * This file is part of Nanvix.
  * 
@@ -27,8 +28,8 @@
 #include <nanvix/mm.h>
 #include <nanvix/syscall.h>
 #include <nanvix/clock.h>
+#include <nanvix/debug.h>
 #include <fcntl.h>
-
 
 /**
  * @brief Forks the current process.
@@ -112,8 +113,9 @@ PRIVATE void init(void)
 
 /**
  * @brief Initializes the kernel.
+ * @param boot mode passed by menu.lst
  */
-PUBLIC void kmain(void)
+PUBLIC void kmain(const char* cmdline)
 {		
 	pid_t pid;         /* Child process ID. */
 	struct process *p; /* Working process.  */
@@ -125,6 +127,9 @@ PUBLIC void kmain(void)
 	fs_init();
 	
 	chkout(DEVID(TTY_MAJOR, 0, CHRDEV));
+
+	if(!kstrcmp(cmdline,"debug"))
+		dbg_init();
 	
 	/* Spawn init process. */
 	if ((pid = fork()) < 0)
