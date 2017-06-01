34e976134d3f1cdba1b7172b4ad5c25b63a17ae7
Unknown <vincent.turrin@gmail.com>
2017-06-01 18:04:31 -0300

    changing ksem structure, adding function to manipulate semaphores

--- include/sys/sem.h
+++ include/sys/sem.h
@@ -24,24 +24,31 @@
 #ifndef _ASM_FILE_
 
 #define SEM_IS_VALID(idx) \
-		(idx>=0 && idx<MAX_SEM_NAME)
+		(idx>=0 && idx<SEM_OPEN_MAX)
 
 #define SEM_IS_FREE(idx) \
 		(semtable[idx].nbproc=-1)
 
+#define SEM_VALID_VALUE(val) \
+		(val<=SEM_VALUE_MAX)
+
 	/* kernel semaphore */
-	typedef struct ksem {
-		int value;                   /* value of the semaphore                    */
-		char name[MAX_SEM_NAME];    /* name of the named semaphore               */
-		int mode;                    /* permissions                               */
-		int nbproc;                  /* number of processes sharing the semaphore */
+	struct ksem {	
+		int value;                   	/* value of the semaphore                    */
+		char name[MAX_SEM_NAME];    	/* name of the named semaphore               */
+		int mode;                   	/* permissions                               */
+		int nbproc;                 	/* number of processes sharing the semaphore */
 		int unlinked;
-	} ksem;
+		uid_t uid;              		/* Semaphore User ID                 */
+		gid_t gid;              		/* Semaphore Group idx               */
+	};
 
 	/* Forward definitions. */
-	extern ksem semtable[SEM_OPEN_MAX];
+	extern struct ksem semtable[SEM_OPEN_MAX];
 
 	/* Frees a semaphore */
-	extern int freesem(int idx);
+	int freesem(int idx);
+
+	int namevalid(const char* name);
 
 #endif
