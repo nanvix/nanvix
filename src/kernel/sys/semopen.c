74a60a63a8a7e1570698d341ed1d9290268c36b7
Unknown <vincent.turrin@gmail.com>
2017-06-01 15:26:54 -0300

    semopen error gestion (not finished)

--- src/kernel/sys/semopen.c
+++ src/kernel/sys/semopen.c
@@ -4,6 +4,7 @@
 #include <sys/sem.h>
 #include <nanvix/klib.h>
 #include <semaphore.h>
+#include <errno.h>
 //maybe unecessary #include <sys/mm.h>
 
 	/**
@@ -13,26 +14,33 @@
 	 * 	
 	 *	@returns the index of the named semaphore in the semaphore table
 	 */
-	int add_entry(int value, const char* name, int mode){
+	int add_entry(int value, const char* name, int mode)
+	{
 		int idx;
 
 		kprintf("nom du semaphore que l'on va ajouter : %s",name);
 
 		for (idx=0; idx<SEM_OPEN_MAX; idx++)
 		{
-			//kprintf("checking for empty slot\n");
+			kprintf("checking for empty slot\n");
 			if(semtable[idx].nbproc==0)
 			{
-				//kprintf("nb proc = 0 : place vide trouvée \n");
+				kprintf("nb proc = 0 : place vide trouvée \n");
 				semtable[idx].value=value;
 				kstrcpy(semtable[idx].name,name);
 				semtable[idx].mode=mode;
 				semtable[idx].nbproc=0;
+				/* semaphore uid is set to the euid of the calling process */
+				semtable[idx].uid=curr_proc->euid;
+				/* semaphore gid is set to the egid of the calling process */
+				semtable[idx].gid=curr_proc->egid;
 				return idx;
 			}
 		}
 
-		return -1; /* semaphore table full */
+		/* semaphore table full */
+		curr_proc->errno = ENFILE;
+		return SEM_FAILED; 
 	}
 
 	/*
@@ -43,7 +51,8 @@
 	 *          semaphore table if it exists, -1
 	 *          otherwise
 	 */
-	int existance(const char* semname){
+	int existance(const char* semname)
+	{
 		int idx;
 
 		for (idx=0; idx<SEM_OPEN_MAX; idx++)
@@ -60,14 +69,21 @@
 	}
 
 
-/* TODO : verify char* adresses so the user doesn't break anything */
-	PUBLIC int sys_semopen(const char* name, int oflag, ...){
+	/* TODO : 	ENOMEM :  There is insufficient memory for the creation of the new named semaphore.
+	 *			EACCES : The named semaphore exists and the permissions specified by oflag are denied, or the
+	 *					 named semaphore does not exist and permission to create the named semaphore is denied.
+	 *			ENOSPC : There is insufficient space on a storage device for the creation of the new named semaphore.
+	 *			EMFILE : Too many semaphore descriptors or file descriptors are currently in use by this process.
+	 */
+	PUBLIC int sys_semopen(const char* name, int oflag, ...)
+	{
 
  		mode_t mode;
  		int value;
  		va_list arg;	/* Variable argument */
- 		//sem_t *s;		/* Opened semaphore */
  		int idx;		/* Index of the opened semaphore */
+
+		idx = existance(name);
 
  		if(oflag & O_CREAT)
  		{
@@ -75,21 +91,22 @@
  			 * and the semaphore name already exists
  			 * return an error
  			 */
-
- 			/* correct this to have everything in the if */
- 			idx = existance(name);
-
  			if(idx!=(-1))				/* if this name already exists */
  			{
  			 	kprintf("name exists\n");
 
- 				if(oflag & O_EXCL)
+ 				if (oflag & O_EXCL)
  				{
- 					return -1; 			/* error */
+ 					/* O_EXCL flag result in an error if sem exists */
+ 					curr_proc->errno = EEXIST;
+ 					return SEM_FAILED;
  				}
- 				else
- 				{
 
+ 				if ( !namevalid(name) )
+ 				{
+ 					/* sem name is not valid */
+ 					curr_proc->errno = EINVAL;
+ 					return SEM_FAILED;
  				}
  			}
  			else
@@ -101,7 +118,24 @@
 	 			mode = va_arg(arg, mode_t);
 	 			value = va_arg(arg, int);
 	 			va_end(arg);
+
+ 				if ( !SEM_VALID_VALUE(value) )
+ 				{
+ 					/* value greater than maximum */
+ 					curr_proc->errno = EINVAL;
+ 					return SEM_FAILED;
+ 				}
+
  				idx=add_entry (value,name,mode);
+ 			}
+ 		}
+ 		else
+ 		{
+ 			if(idx==(-1))
+ 			{
+ 				/* O_CREAT not set and sem does not exist */
+ 				curr_proc->errno = ENOENT;
+ 				return SEM_FAILED;
  			}
  		}
 
