c42ad68513ce47c86dd1f6a0b4e6b378c1795d3e
Theldus <davidsondfgl@gmail.com>
2017-08-27 04:59:33 -0300

    use kvm only when using perf counters

--- tools/run/run-qemu.sh
+++ tools/run/run-qemu.sh
@@ -1,6 +1,6 @@
 # 
 # Copyright(C) 2011-2016 Pedro H. Penna   <pedrohenriquepenna@gmail.com> 
-#              2016-2016 Davidson Francis <davidsondfgl@gmail.com>
+#              2016-2017 Davidson Francis <davidsondfgl@gmail.com>
 #
 # This file is part of Nanvix.
 #
@@ -24,14 +24,19 @@
 #
 
 if [ "$1" = "--dbg" ]; then
-	qemu-system-x86_64 -s -S                                     \
+	qemu-system-i386 -s -S                                   \
 		-drive file=nanvix.iso,format=raw,if=ide,media=cdrom \
 		-m 256M                                              \
-		-mem-prealloc -cpu host --enable-kvm -sdl &
+		-mem-prealloc &
 	ddd --debugger "/usr/local/cross/bin/i386-elf-gdb"
-else
-	qemu-system-x86_64                                     \
+elif [ "$1" = "--perf" ]; then
+	qemu-system-i386                                         \
 		-drive file=nanvix.iso,format=raw,if=ide,media=cdrom \
 		-m 256M                                              \
-		-mem-prealloc -cpu host --enable-kvm -sdl
+		-mem-prealloc -cpu host --enable-kvm
+else
+	qemu-system-i386                                         \
+		-drive file=nanvix.iso,format=raw,if=ide,media=cdrom \
+		-m 256M                                              \
+		-mem-prealloc
 fi
