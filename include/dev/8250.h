73db759e25d00c23303d000434f1aa978bb936bd
Theldus <davidsondfgl@gmail.com>
2018-01-04 00:54:19 -0200

    8250 UART header

---
+++ include/dev/8250.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright(C) 2017 Davidson Francis <davidsondfgl@gmail.com>
+ * 
+ * This file is part of Nanvix.
+ * 
+ * Nanvix is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Nanvix is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Nanvix. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef UART_8250_H
+#define UART_8250_H
+
+	#include <nanvix/klib.h>
+
+	/**
+	 * UART functions
+	 */
+	/**@{*/
+	EXTERN void uart_init(void);
+	EXTERN void uart_write(char);
+	/**@}*/
+
+#endif /* UART_8250_H */
