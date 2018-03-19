22e7dc6d7c7f102b5ba2e8089fa0b42def59c293
Theldus <davidsondfgl@gmail.com>
2018-03-19 05:25:34 -0300

    or1k: temporarily not enabling/masking interrupts

--- src/kernel/arch/or1k/hwint.c
+++ src/kernel/arch/or1k/hwint.c
@@ -80,13 +80,5 @@ PUBLIC int set_hwint(int num, void (*handler)(void))
  */
 PUBLIC void do_hwint(unsigned irq)
 {
-	unsigned old_irqlvl;
-	
-	old_irqlvl = processor_raise(irq_lvl(irq));
-
-	enable_interrupts();
 	hwint_handlers[irq]();
-	disable_interrupts();
-
-	processor_drop(old_irqlvl);
 }
