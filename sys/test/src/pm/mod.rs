b90b62d3b8255fc5f978c81e246fea1d0a55fb52
Pedro Henrique Penna <pedrohenriquepenna@gmail.com>
2024-07-26 21:20:35 +0000

    [test] E: test_capctl()

--- sys/test/src/pm/mod.rs
+++ sys/test/src/pm/mod.rs
@@ -190,6 +190,225 @@ fn test_getegid() -> bool {
     }
 }
 
+//==================================================================================================
+// Tests for capctl()
+//==================================================================================================
+
+///
+/// # Description
+///
+/// Tests if [`libnanvix::Capability::ExceptionControl`] capability may be acquired and release.
+///
+/// # Returns
+///
+/// If the test passed, `true` is returned. Otherwise, `false` is returned instead.
+///
+fn test_capctl_exception_control() -> bool {
+    // Attempt to acquire and release exception control capability.
+    match libnanvix::capctl(libnanvix::Capability::ExceptionControl, true) {
+        Ok(()) => match libnanvix::capctl(libnanvix::Capability::ExceptionControl, false) {
+            Ok(()) => true,
+            _ => false,
+        },
+        _ => false,
+    }
+}
+
+///
+/// # Description
+///
+/// Tests if [`libnanvix::Capability::InterruptControl`] capability may be acquired and release.
+///
+/// # Returns
+///
+/// If the test passed, `true` is returned. Otherwise, `false` is returned instead.
+///
+fn test_capctl_interrupt_control() -> bool {
+    // Attempt to acquire and release interrupt control capability.
+    match libnanvix::capctl(libnanvix::Capability::InterruptControl, true) {
+        Ok(()) => match libnanvix::capctl(libnanvix::Capability::InterruptControl, false) {
+            Ok(()) => true,
+            _ => false,
+        },
+        _ => false,
+    }
+}
+
+///
+/// # Description
+///
+/// Tests if [`libnanvix::Capability::IoManagement`] capability may be acquired and release.
+///
+/// # Returns
+///
+/// If the test passed, `true` is returned. Otherwise, `false` is returned instead.
+///
+fn test_capctl_io_management() -> bool {
+    // Attempt to acquire and release I/O management capability.
+    match libnanvix::capctl(libnanvix::Capability::IoManagement, true) {
+        Ok(()) => match libnanvix::capctl(libnanvix::Capability::IoManagement, false) {
+            Ok(()) => true,
+            _ => false,
+        },
+        _ => false,
+    }
+}
+
+///
+/// # Description
+///
+/// Tests if [`libnanvix::Capability::MemoryManagement`] capability may be acquired and release.
+///
+/// # Returns
+///
+/// If the test passed, `true` is returned. Otherwise, `false` is returned instead.
+///
+fn test_capctl_memory_management() -> bool {
+    // Attempt to acquire and release memory management capability.
+    match libnanvix::capctl(libnanvix::Capability::MemoryManagement, true) {
+        Ok(()) => match libnanvix::capctl(libnanvix::Capability::MemoryManagement, false) {
+            Ok(()) => true,
+            _ => false,
+        },
+        _ => false,
+    }
+}
+
+///
+/// # Description
+///
+/// Tests if [`libnanvix::Capability::ProcessManagement`] capability may be acquired and release.
+///
+/// # Returns
+///
+/// If the test passed, `true` is returned. Otherwise, `false` is returned instead.
+///
+fn test_capctl_process_management() -> bool {
+    // Attempt to acquire and release process management capability.
+    match libnanvix::capctl(libnanvix::Capability::ProcessManagement, true) {
+        Ok(()) => match libnanvix::capctl(libnanvix::Capability::ProcessManagement, false) {
+            Ok(()) => true,
+            _ => false,
+        },
+        _ => false,
+    }
+}
+
+///
+/// # Description
+///
+/// Attempts to acquire the same capability twice.
+///
+/// # Returns
+///
+/// If the test passed, `true` is returned. Otherwise, `false` is returned instead.
+///
+fn test_capctl_invalid_acquire() -> bool {
+    // Attempt to acquire exception control capability twice.
+    match libnanvix::capctl(libnanvix::Capability::ExceptionControl, true) {
+        Ok(()) => match libnanvix::capctl(libnanvix::Capability::ExceptionControl, true) {
+            Ok(()) => return false,
+            _ => match libnanvix::capctl(libnanvix::Capability::ExceptionControl, false) {
+                Ok(()) => (),
+                _ => return false,
+            },
+        },
+        _ => return false,
+    }
+
+    // Attempt to acquire interrupt control capability twice.
+    match libnanvix::capctl(libnanvix::Capability::InterruptControl, true) {
+        Ok(()) => match libnanvix::capctl(libnanvix::Capability::InterruptControl, true) {
+            Ok(()) => return false,
+            _ => match libnanvix::capctl(libnanvix::Capability::InterruptControl, false) {
+                Ok(()) => (),
+                _ => return false,
+            },
+        },
+        _ => return false,
+    }
+
+    // Attempt to acquire I/O management capability twice.
+    match libnanvix::capctl(libnanvix::Capability::IoManagement, true) {
+        Ok(()) => match libnanvix::capctl(libnanvix::Capability::IoManagement, true) {
+            Ok(()) => return false,
+            _ => match libnanvix::capctl(libnanvix::Capability::IoManagement, false) {
+                Ok(()) => (),
+                _ => return false,
+            },
+        },
+        _ => return false,
+    }
+
+    // Attempt to acquire memory management capability twice.
+    match libnanvix::capctl(libnanvix::Capability::MemoryManagement, true) {
+        Ok(()) => match libnanvix::capctl(libnanvix::Capability::MemoryManagement, true) {
+            Ok(()) => return false,
+            _ => match libnanvix::capctl(libnanvix::Capability::MemoryManagement, false) {
+                Ok(()) => (),
+                _ => return false,
+            },
+        },
+        _ => return false,
+    }
+
+    // Attempt to acquire process management capability twice.
+    match libnanvix::capctl(libnanvix::Capability::ProcessManagement, true) {
+        Ok(()) => match libnanvix::capctl(libnanvix::Capability::ProcessManagement, true) {
+            Ok(()) => return false,
+            _ => match libnanvix::capctl(libnanvix::Capability::ProcessManagement, false) {
+                Ok(()) => (),
+                _ => return false,
+            },
+        },
+        _ => return false,
+    }
+
+    true
+}
+
+///
+/// # Description
+///
+/// Attempts to release a capability that was not acquired.
+///
+/// # Returns
+///
+/// If the test passed, `true` is returned. Otherwise, `false` is returned instead.
+///
+fn test_capctl_invalid_release() -> bool {
+    // Attempt to release exception control capability without acquiring it.
+    match libnanvix::capctl(libnanvix::Capability::ExceptionControl, false) {
+        Ok(()) => return false,
+        _ => (),
+    }
+
+    // Attempt to release interrupt control capability without acquiring it.
+    match libnanvix::capctl(libnanvix::Capability::InterruptControl, false) {
+        Ok(()) => return false,
+        _ => (),
+    }
+
+    // Attempt to release I/O management capability without acquiring it.
+    match libnanvix::capctl(libnanvix::Capability::IoManagement, false) {
+        Ok(()) => return false,
+        _ => (),
+    }
+
+    // Attempt to release memory management capability without acquiring it.
+    match libnanvix::capctl(libnanvix::Capability::MemoryManagement, false) {
+        Ok(()) => return false,
+        _ => (),
+    }
+
+    // Attempt to release process management capability without acquiring it.
+    match libnanvix::capctl(libnanvix::Capability::ProcessManagement, false) {
+        Ok(()) => return false,
+        _ => (),
+    }
+
+    true
+}
 
 //==================================================================================================
 // Public Standalone Functions
@@ -207,4 +426,11 @@ pub fn test() {
     crate::test!(test_geteuid());
     crate::test!(test_getgid());
     crate::test!(test_getegid());
+    crate::test!(test_capctl_exception_control());
+    crate::test!(test_capctl_interrupt_control());
+    crate::test!(test_capctl_io_management());
+    crate::test!(test_capctl_memory_management());
+    crate::test!(test_capctl_process_management());
+    crate::test!(test_capctl_invalid_acquire());
+    crate::test!(test_capctl_invalid_release());
 }
