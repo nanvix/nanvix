81df8671949143c70b87088441dd971142a22e90
Pedro Henrique Penna <pedrohenriquepenna@gmail.com>
2024-08-03 18:21:50 -0700

    [sys] E: Moving Out of Source Tree

--- sys/src/constants.rs
+++
@@ -1,8 +0,0 @@
-// Copyright(c) The Maintainers of Nanvix.
-// Licensed under the MIT License.
-
-/// Kilobyte.
-pub const KILOBYTE: usize = 1 << 10;
-
-/// Megabyte.
-pub const MEGABYTE: usize = 1 << 20;
