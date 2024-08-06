703ca5553e9178598f88b83fd465d4733d67f1da
Pedro Henrique Penna <pedrohenriquepenna@gmail.com>
2024-08-06 11:53:25 -0700

    [nvx] F: Process Management Message

---
+++ nvx/src/pm/message.rs
@@ -0,0 +1,128 @@
+// Copyright(c) The Maintainers of Nanvix.
+// Licensed under the MIT License.
+
+//==================================================================================================
+// Imports
+//==================================================================================================
+
+use crate::ipc::SystemMessage;
+use ::core::mem;
+use ::error::{
+    Error,
+    ErrorCode,
+};
+
+//==================================================================================================
+// Process Management Message Header
+//==================================================================================================
+
+///
+/// # Description
+///
+/// A type that encodes the header of a process management message.
+///
+#[repr(u8)]
+#[derive(Debug, Clone, Copy)]
+pub enum ProcessManagementMessageHeader {
+    /// Shutdown operation.
+    Shutdown = 1,
+}
+
+impl TryFrom<u8> for ProcessManagementMessageHeader {
+    type Error = Error;
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        match value {
+            1 => Ok(ProcessManagementMessageHeader::Shutdown),
+            _ => Err(Error::new(ErrorCode::InvalidArgument, "invalid process management message")),
+        }
+    }
+}
+
+impl From<&ProcessManagementMessageHeader> for u8 {
+    fn from(value: &ProcessManagementMessageHeader) -> Self {
+        match value {
+            ProcessManagementMessageHeader::Shutdown => 1,
+        }
+    }
+}
+
+//==================================================================================================
+// Process Management Message
+//==================================================================================================
+
+///
+/// # Description
+///
+/// A type that encodes a process management message.
+///
+#[repr(C, packed)]
+pub struct ProcessManagementMessage {
+    /// Message header.
+    pub header: ProcessManagementMessageHeader,
+    /// Message payload.
+    pub payload: [u8; Self::PAYLOAD_SIZE],
+}
+
+// NOTE: the size of a process management message must match the size of a system message payload.
+static_assert_size!(ProcessManagementMessage, SystemMessage::PAYLOAD_SIZE);
+
+impl ProcessManagementMessage {
+    /// Size of payload.
+    pub const PAYLOAD_SIZE: usize =
+        SystemMessage::PAYLOAD_SIZE - mem::size_of::<ProcessManagementMessageHeader>();
+
+    ///
+    /// # Description
+    ///
+    /// Instantiates a new process management message.
+    ///
+    /// # Parameters
+    ///
+    /// - `header`: Message header.
+    /// - `payload`: Message payload.
+    ///
+    /// # Returns
+    ///
+    /// A process management message.
+    ///
+    pub fn new(header: ProcessManagementMessageHeader, payload: [u8; Self::PAYLOAD_SIZE]) -> Self {
+        Self { header, payload }
+    }
+
+    ///
+    /// # Description
+    ///
+    /// Converts a byte array into a process management message.
+    ///
+    /// # Parameters
+    ///
+    /// - `bytes`: Byte array.
+    ///
+    /// # Returns
+    ///
+    /// A process management message.
+    ///
+    pub fn from_bytes(bytes: [u8; SystemMessage::PAYLOAD_SIZE]) -> Result<Self, Error> {
+        // Check if message header is valid.
+        let _header: ProcessManagementMessageHeader =
+            ProcessManagementMessageHeader::try_from(bytes[0])?;
+
+        let message: ProcessManagementMessage = unsafe { mem::transmute(bytes) };
+
+        Ok(message)
+    }
+
+    ///
+    /// # Description
+    ///
+    /// Converts a process management message into a byte array.
+    ///
+    /// # Returns
+    ///
+    /// The corresponding byte array.
+    ///
+    pub fn into_bytes(self) -> [u8; SystemMessage::PAYLOAD_SIZE] {
+        unsafe { mem::transmute(self) }
+    }
+}
