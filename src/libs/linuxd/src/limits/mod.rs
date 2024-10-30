503da0082a9fc89d28adebbf9c65a43726c23b7b
Pedro Henrique Penna <ppenna@microsoft.com>
2024-10-30 11:04:00 -0700

    [daemons] E: linuxd SSIZE_MAX

--- src/libs/linuxd/src/limits/mod.rs
+++ src/libs/linuxd/src/limits/mod.rs
@@ -7,3 +7,6 @@ pub const NAME_MAX: usize = 16;
 /// Maximum number of bytes the implementation stores as a pathname in a user-supplied buffer of
 /// unspecified size, including the terminating null character.
 pub const PATH_MAX: usize = 256;
+
+/// Maximum value for an object of type [`crate::sys::types::ssize_t`].
+pub const SSIZE_MAX: crate::sys::types::ssize_t = crate::sys::types::ssize_t::MAX;
