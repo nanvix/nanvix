a74cdae93c3355a91162adc85e9ad9d46d834453
Pedro Henrique Penna <ppenna@microsoft.com>
2024-12-23 10:56:44 -0800

    [libs] E: Set errno in kill

--- src/libs/posix/src/signal/bindings.rs
+++ src/libs/posix/src/signal/bindings.rs
@@ -5,7 +5,10 @@
 // Imports
 //==================================================================================================
 
+use nvx::sys::error::ErrorCode;
+
 use crate::{
+    errno::errno,
     ffi::c_int,
     sys::types::pid_t,
 };
@@ -18,5 +21,8 @@ use crate::{
 pub extern "C" fn kill(_pid: pid_t, _signal: c_int) -> c_int {
     ::nvx::log!("kill(): pid = {}, signal = {}", _pid, _signal);
     // TODO: Implement this system call.
+    unsafe {
+        errno = ErrorCode::InvalidSysCall.into_errno();
+    }
     -1
 }
