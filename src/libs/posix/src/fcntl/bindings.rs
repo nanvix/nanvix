b89c121fc5fe72525fde7e8eb88e4798d98ef580
Pedro Henrique Penna <ppenna@microsoft.com>
2024-12-23 10:47:17 -0800

    [libs] W: TODO variadic in open

--- src/libs/posix/src/fcntl/bindings.rs
+++ src/libs/posix/src/fcntl/bindings.rs
@@ -19,6 +19,8 @@ use crate::ffi::{
 ///
 /// The function has undefined behavior if the `path` points to an invalid memory location.
 ///
+/// TODO: Change function signature to use a variable argument list.
+///
 #[no_mangle]
 pub unsafe extern "C" fn open(path: *const c_char, flags: c_int, mode: u32) -> c_int {
     ::nvx::log!("open(): path = {:?}, flags = {}, mode = {}", path, flags, mode);
