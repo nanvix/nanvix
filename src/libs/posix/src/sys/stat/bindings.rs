1b78342d5258c85e2569c22b838b9c4a698f19eb
Pedro Henrique Penna <ppenna@microsoft.com>
2024-12-23 10:41:32 -0800

    [libs] E: Use ffi types in fstat

--- src/libs/posix/src/sys/stat/bindings.rs
+++ src/libs/posix/src/sys/stat/bindings.rs
@@ -1,13 +1,26 @@
 // Copyright(c) The Maintainers of Nanvix.
 // Licensed under the MIT License.
 
+//==================================================================================================
+// Imports
+//==================================================================================================
+
+use crate::{
+    ffi::c_int,
+    sys::stat,
+};
+
+//==================================================================================================
+// Standalone Functions
+//==================================================================================================
+
 ///
 /// # Safety
 ///
 /// This function has undefined behavior if buf points to an invalid memory location.
 ///
 #[no_mangle]
-pub unsafe extern "C" fn fstat(fd: i32, buf: *mut crate::sys::stat::stat) -> i32 {
+pub unsafe extern "C" fn fstat(fd: c_int, buf: *mut stat::stat) -> c_int {
     ::nvx::log!("fstat(): fd = {}, buf = {:?}", fd, buf);
     crate::sys::stat::fstat(fd, &mut *buf)
 }
