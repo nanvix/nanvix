ebcf6a5d30751e7fc09ad81808376e946a66b81c
Pedro Henrique Penna <ppenna@microsoft.com>
2024-12-23 10:56:03 -0800

    [libs] E: Use ffi types in kill

--- src/libs/posix/src/signal/bindings.rs
+++ src/libs/posix/src/signal/bindings.rs
@@ -1,8 +1,21 @@
 // Copyright(c) The Maintainers of Nanvix.
 // Licensed under the MIT License.
 
+//==================================================================================================
+// Imports
+//==================================================================================================
+
+use crate::{
+    ffi::c_int,
+    sys::types::pid_t,
+};
+
+//==================================================================================================
+// Standalone Functions
+//==================================================================================================
+
 #[no_mangle]
-pub extern "C" fn kill(_pid: i32, _signal: i32) -> i32 {
+pub extern "C" fn kill(_pid: pid_t, _signal: c_int) -> c_int {
     ::nvx::log!("kill(): pid = {}, signal = {}", _pid, _signal);
     // TODO: Implement this system call.
     -1
