b8d67bc4160af0d583ba0bf693f1430a1b84c8b3
Pedro Henrique Penna <ppenna@microsoft.com>
2024-12-23 10:43:59 -0800

    [libs] E: Refactoring Source Tree

--- src/libs/posix/src/bindings.rs
+++ src/libs/posix/src/bindings.rs
@@ -14,17 +14,6 @@ pub extern "C" fn kill(_pid: i32, _signal: i32) -> i32 {
 /// The function has undefined behavior if the `path` points to an invalid memory location.
 ///
 #[no_mangle]
-pub unsafe extern "C" fn open(path: *const i8, flags: i32, mode: u32) -> i32 {
-    ::nvx::log!("open(): path = {:?}, flags = {}, mode = {}", path, flags, mode);
-    crate::unistd::open(path, flags, mode)
-}
-
-///
-/// # Safety
-///
-/// The function has undefined behavior if the `path` points to an invalid memory location.
-///
-#[no_mangle]
 pub unsafe extern "C" fn getentropy(_buffer: *mut u8, _length: u32) -> i32 {
     ::nvx::log!("getentropy(): buffer = {:?}, length = {}", _buffer, _length);
     -1
