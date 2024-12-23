c2acc369c247b5ac4419615da5d8e8c18c8b3274
Pedro Henrique Penna <ppenna@microsoft.com>
2024-12-23 10:45:42 -0800

    [libs] E: User ffi types in open

--- src/libs/posix/src/fcntl/bindings.rs
+++ src/libs/posix/src/fcntl/bindings.rs
@@ -1,13 +1,26 @@
 // Copyright(c) The Maintainers of Nanvix.
 // Licensed under the MIT License.
 
+//==================================================================================================
+// Imports
+//==================================================================================================
+
+use crate::ffi::{
+    c_char,
+    c_int,
+};
+
+//==================================================================================================
+// Standalone Functions
+//==================================================================================================
+
 ///
 /// # Safety
 ///
 /// The function has undefined behavior if the `path` points to an invalid memory location.
 ///
 #[no_mangle]
-pub unsafe extern "C" fn open(path: *const i8, flags: i32, mode: u32) -> i32 {
+pub unsafe extern "C" fn open(path: *const c_char, flags: c_int, mode: u32) -> c_int {
     ::nvx::log!("open(): path = {:?}, flags = {}, mode = {}", path, flags, mode);
     crate::unistd::open(path, flags, mode)
 }
