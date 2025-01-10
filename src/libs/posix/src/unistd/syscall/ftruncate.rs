f0d81db03215db0ceb70d186784e7ecbb3c522a9
Pedro Henrique Penna <ppenna@microsoft.com>
2025-01-10 07:34:28 -0800

    [libs] E: Return for ftruncate()

--- src/libs/posix/src/unistd/syscall/ftruncate.rs
+++ src/libs/posix/src/unistd/syscall/ftruncate.rs
@@ -6,66 +6,64 @@
 //==================================================================================================
 
 use crate::{
+    ffi::c_int,
     sys::types::off_t,
-    unistd::message::{
-        FileTruncateRequest,
-        FileTruncateResponse,
-    },
+    unistd::message::FileTruncateRequest,
     LinuxDaemonMessage,
     LinuxDaemonMessageHeader,
 };
 use ::nvx::{
     ipc::Message,
     pm::ProcessIdentifier,
-    sys::error::ErrorCode,
+    sys::error::{
+        Error,
+        ErrorCode,
+    },
 };
 
 //==================================================================================================
 // Standalone Functions
 //==================================================================================================
 
-pub fn ftruncate(fd: i32, length: off_t) -> i32 {
-    let pid: ProcessIdentifier = match ::nvx::pm::getpid() {
-        Ok(pid) => pid,
-        Err(e) => return e.code.into_errno(),
-    };
+///
+/// # Description
+///
+/// Truncates a file to a specified length.
+///
+/// # Parameters
+///
+/// `fd`: File descriptor.
+/// `length`: New size of the file.
+///
+/// # Returns
+///
+/// Upon successful completion, empty is returned. Otherwise, an error is returned.
+///
+pub fn ftruncate(fd: c_int, length: off_t) -> Result<(), Error> {
+    let pid: ProcessIdentifier = ::nvx::pm::getpid()?;
 
     // Build request and send it.
     let request: Message = FileTruncateRequest::build(pid, fd, length);
-    if let Err(e) = ::nvx::ipc::send(&request) {
-        return e.code.into_errno();
-    }
+    ::nvx::ipc::send(&request)?;
 
     // Receive response.
-    let response: Message = match ::nvx::ipc::recv() {
-        Ok(response) => response,
-        Err(e) => return e.code.into_errno(),
-    };
+    let response: Message = ::nvx::ipc::recv()?;
 
     // Check whether system call succeeded or not.
     if response.status != 0 {
         // System call failed, parse error code and return it.
-        match ErrorCode::try_from(response.status) {
-            Ok(e) => e.into_errno(),
-            Err(_) => ErrorCode::InvalidMessage.into_errno(),
-        }
+        let error_code: ErrorCode = ErrorCode::try_from(response.status)?;
+        ::nvx::log!("ftruncate(): failed ({:?})", error_code);
+        Err(Error::new(error_code, "ftruncate() failed"))
     } else {
         // System call succeeded, parse response.
-        match LinuxDaemonMessage::try_from_bytes(response.payload) {
+        let message: LinuxDaemonMessage = LinuxDaemonMessage::try_from_bytes(response.payload)?;
+        // Response was successfully parsed.
+        match message.header {
             // Response was successfully parsed.
-            Ok(message) => match message.header {
-                // Response was successfully parsed.
-                LinuxDaemonMessageHeader::FileTruncateResponse => {
-                    // Parse response.
-                    let response: FileTruncateResponse =
-                        FileTruncateResponse::from_bytes(message.payload);
-                    response.ret
-                },
-                // Response was not successfully parsed.
-                _ => ErrorCode::InvalidMessage.into_errno(),
-            },
-            // Response was not successfully parsed.
-            Err(_) => ErrorCode::InvalidMessage.into_errno(),
+            LinuxDaemonMessageHeader::FileTruncateResponse => Ok(()),
+            // Invalid response.
+            _ => Err(Error::new(ErrorCode::InvalidMessage, "unexpected message header")),
         }
     }
 }
