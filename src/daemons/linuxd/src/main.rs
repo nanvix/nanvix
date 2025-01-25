0bb6f81280f82c5174b6ba9ab74e53f7d51d445d
Pedro Henrique Penna <ppenna@microsoft.com>
2025-01-24 16:22:04 -0800

    [daemons] E: Read from STDIN if No Gateway

--- src/daemons/linuxd/src/main.rs
+++ src/daemons/linuxd/src/main.rs
@@ -352,8 +352,22 @@ impl ProcessDaemon {
                                                 },
                                             }
                                         } else {
-                                            error!("not connected to the gateway");
-                                            build_error(source, ErrorCode::HostUnreachable)
+                                            // Not connected to the gateway, read from stdin.
+                                            let mut buffer: [u8; ReadResponse::BUFFER_SIZE] =
+                                                [0u8; ReadResponse::BUFFER_SIZE];
+                                            let count: usize = match ::std::io::stdin()
+                                                .read(&mut buffer)
+                                            {
+                                                Ok(count) => count,
+                                                Err(e) => {
+                                                    debug!(
+                                                        "failed to read from stdin (error={:?})",
+                                                        e
+                                                    );
+                                                    0
+                                                },
+                                            };
+                                            ReadResponse::build(source, count as ssize_t, buffer)
                                         }
                                     } else {
                                         // Read from other file descriptor.
