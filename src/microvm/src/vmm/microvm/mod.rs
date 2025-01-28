91fae4846d0ae09fea812f366ec8a1ae7ed1b77f
Pedro Henrique Penna <ppenna@microsoft.com>
2025-01-28 11:40:38 -0800

    [microvm] F: Memory Thread

--- src/microvm/src/vmm/microvm/mod.rs
+++ src/microvm/src/vmm/microvm/mod.rs
@@ -41,11 +41,13 @@ use ::std::{
         mpsc,
         mpsc::{
             Receiver,
+            RecvError,
             Sender,
             TryRecvError,
         },
         Arc,
         Mutex,
+        MutexGuard,
     },
     thread::JoinHandle,
     time::Duration,
@@ -62,6 +64,7 @@ use ::sys::ipc::{
 pub struct Vmm {
     _gateway_tx: Sender<Message>,
     _io_thread: Option<JoinHandle<Result<()>>>,
+    _memory_thread: JoinHandle<Result<()>>,
     microvm: Arc<Mutex<MicroVm>>,
 }
 
@@ -80,7 +83,8 @@ impl Vmm {
         crate::timer!("vmm_creation");
 
         let (vm_tx, gateway_rx) = mpsc::channel::<Message>();
-        let (gateway_tx, vm_rx) = mpsc::channel::<Message>();
+        let (gateway_tx, memory_thread_rx) = mpsc::channel::<Message>();
+        let (memory_thread_tx, vm_rx) = mpsc::channel::<Message>();
         let gateway_conn: Option<UnixStream> = match gateway_addr {
             Some(addr) => match UnixStream::connect(addr.clone()) {
                 Ok(conn) => {
@@ -120,9 +124,44 @@ impl Vmm {
         microvm.reset(rip)?;
 
         let microvm: Arc<Mutex<MicroVm>> = Arc::new(Mutex::new(microvm));
+
+        let vmem: Arc<Mutex<VirtualMemory>> = microvm
+            .lock()
+            .map_err(|e| anyhow::anyhow!("failed to acquire lock {:?}", e))?
+            .vmem();
+
+        vmem.lock()
+            .map_err(|e| anyhow::anyhow!("failed to acquire lock {:?}", e))?
+            .reset_credits()?;
+
+        // Create a thread that reads from vm_rx and writes to vm_rx2.
+        let memory_thread_tx: Sender<Message> = memory_thread_tx.clone();
+        let memory_thread: JoinHandle<Result<(), anyhow::Error>> =
+            std::thread::spawn(move || loop {
+                match memory_thread_rx.try_recv() {
+                    Ok(msg) => {
+                        if let Err(e) = memory_thread_tx.send(msg) {
+                            let reason: String = format!("failed to send message: {:?}", e);
+                            error!("memory_thread(): {}", reason);
+                            continue;
+                        }
+                        vmem.lock()
+                            .map_err(|e| anyhow::anyhow!("failed to acquire lock {:?}", e))?
+                            .add_credit()?;
+                    },
+                    Err(TryRecvError::Disconnected) => {
+                        error!("memory_thread(): channel has been disconnected");
+                        break Ok(());
+                    },
+                    Err(TryRecvError::Empty) => {
+                        // No message available.
+                    },
+                }
+            });
         Ok(Self {
             _gateway_tx: gateway_tx,
             _io_thread,
+            _memory_thread: memory_thread,
             microvm,
         })
     }
@@ -177,30 +216,25 @@ impl Vmm {
 
     fn build_input_fn(input_queue: Receiver<Message>) -> Box<microvm::InputFn> {
         // Input function used for emulating I/O port reads.
-        let input = move |vm: &Arc<Mutex<VirtualMemory>>, data, size| -> Result<()> {
+        let input = move |vmem: &Arc<Mutex<VirtualMemory>>, data, size| -> Result<()> {
             // Check for invalid operand size.
-            if size != 4 {
+            if size != mem::size_of::<u32>() {
                 let reason: String = format!("invalid operand size (size={:?})", size);
                 error!("input(): {}", reason);
                 anyhow::bail!(reason);
             }
 
-            match input_queue.try_recv() {
+            match input_queue.recv() {
                 Ok(mut msg) => {
                     msg.message_type = MessageType::Ikc;
-                    vm.lock()
-                        .map_err(|e| anyhow::anyhow!("failed to acquire lock {:?}", e))?
-                        .write_bytes(data as u64, &msg.to_bytes())?;
-                },
-                // No message available.
-                Err(TryRecvError::Empty) => {
-                    let empty_message = Message::default();
-                    vm.lock()
-                        .map_err(|e| anyhow::anyhow!("failed to acquire lock {:?}", e))?
-                        .write_bytes(data as u64, &empty_message.to_bytes())?;
+                    let mut locked_vm: MutexGuard<'_, VirtualMemory> = vmem
+                        .lock()
+                        .map_err(|e| anyhow::anyhow!("failed to acquire lock {:?}", e))?;
+                    locked_vm.write_bytes(data as u64, &msg.to_bytes())?;
+                    locked_vm.consume_credit().unwrap();
                 },
                 // Channel has disconnected.
-                Err(TryRecvError::Disconnected) => {
+                Err(RecvError) => {
                     let reason: String = "channel has been disconnected".to_string();
                     error!("input(): {}", reason);
                     anyhow::bail!(reason);
