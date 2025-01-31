81da839134eb27c9f799e49d7d8b92ce0d4d361d
Pedro Henrique Penna <ppenna@microsoft.com>
2025-01-31 07:50:53 -0800

    [kernel] B: Copy Exact Number of Bytes When Loading Binary

--- src/kernel/src/mm/elf.rs
+++ src/kernel/src/mm/elf.rs
@@ -28,7 +28,10 @@ use crate::{
         Vmem,
     },
 };
-use ::core::cmp::max;
+use ::core::cmp::{
+    max,
+    min,
+};
 use ::sys::{
     arch::mem,
     config,
@@ -248,8 +251,7 @@ fn do_elf32_load(
             (elf as *const Elf32Fhdr as *const u8).offset(phdr.p_offset as isize) as usize
         };
 
-        let phys_addr_end: usize =
-            ::sys::mm::align_up(phys_addr_base + phdr.p_filesz as usize, mmu::PAGE_ALIGNMENT);
+        let phys_addr_end: usize = phys_addr_base + phdr.p_filesz as usize;
 
         // Load segment page by page.
         for phys_addr in (phys_addr_base..phys_addr_end).step_by(mem::PAGE_SIZE) {
@@ -264,14 +266,16 @@ fn do_elf32_load(
             let paddr: PageAligned<PhysicalAddress> = PageAligned::from_raw_value(phys_addr)?;
             let vaddr: PageAligned<VirtualAddress> = PageAligned::from_address(vaddr)?;
 
+            let size: usize = min(mem::PAGE_SIZE, phys_addr_end - phys_addr);
+
             // Check if we should load the segment.
             if !dry_run {
                 // Load the segment.
                 // TODO: rollback memory allocation on failure.
-                vmem.memcpy(vaddr, paddr)?;
+                vmem.memcpy(vaddr, paddr, size)?;
             }
 
-            virt_addr += mem::PAGE_SIZE;
+            virt_addr += size;
         }
     }
 
