ca513b694a3756cc9e62c905ba82a18ea1f62183
Pedro Henrique Penna <ppenna@microsoft.com>
2025-02-06 13:49:06 -0800

    [libs] E: Safe Typing for send()

--- src/libs/posix/src/sys/socket/syscall/send.rs
+++ src/libs/posix/src/sys/socket/syscall/send.rs
@@ -6,6 +6,7 @@
 //==================================================================================================
 
 use crate::{
+    ffi::c_int,
     sys::{
         socket::message::{
             SendSocketRequest,
@@ -23,32 +24,23 @@ use ::core::cmp;
 use ::nvx::{
     ipc::Message,
     pm::ProcessIdentifier,
-    sys::error::ErrorCode,
+    sys::error::{
+        Error,
+        ErrorCode,
+    },
 };
 
 //==================================================================================================
 // Standalone Functions
 //==================================================================================================
 
-#[allow(clippy::not_unsafe_ptr_arg_deref)] // TODO: Wrap this in a safe function.
-pub fn send(sockfd: i32, buffer: *const u8, count: size_t, flags: i32) -> ssize_t {
-    let pid: ProcessIdentifier = match ::nvx::pm::getpid() {
-        Ok(pid) => pid,
-        Err(e) => return e.code.into_errno(),
-    };
-
-    // Check if buffer is invalid.
-    if buffer.is_null() {
-        return ErrorCode::InvalidArgument.into_errno();
-    }
+pub fn send(sockfd: c_int, buffer: &[u8], flags: c_int) -> Result<ssize_t, Error> {
+    let pid: ProcessIdentifier = ::nvx::pm::getpid()?;
 
     // Check if count is invalid.
-    if count == 0 {
-        return ErrorCode::InvalidArgument.into_errno();
+    if buffer.len() == 0 {
+        return Err(Error::new(ErrorCode::InvalidArgument, "buffer length is zero"));
     }
-
-    // Construct buffer from raw parts.
-    let buffer: &[u8] = unsafe { ::core::slice::from_raw_parts(buffer, count as usize) };
 
     let mut total_written: ssize_t = 0;
     let mut offset: usize = 0;
@@ -61,22 +53,19 @@ pub fn send(sockfd: i32, buffer: *const u8, count: size_t, flags: i32) -> ssize_
         // Build request and send it.
         let request: Message =
             SendSocketRequest::build(pid, sockfd, chunk_size as size_t, flags, chunk);
-        if let Err(e) = ::nvx::ipc::send(&request) {
-            return e.code.into_errno();
-        }
+        ::nvx::ipc::send(&request)?;
 
         // Receive response.
-        let response: Message = match ::nvx::ipc::recv() {
-            Ok(response) => response,
-            Err(e) => return e.code.into_errno(),
-        };
+        let response: Message = ::nvx::ipc::recv()?;
 
         // Check whether system call succeeded or not.
         if response.status != 0 {
             // System call failed, parse error code and return it.
             match ErrorCode::try_from(response.status) {
-                Ok(e) => return e.into_errno(),
-                Err(_) => return ErrorCode::InvalidMessage.into_errno(),
+                Ok(error_code) => {
+                    return Err(Error::new(error_code, "failed to send data through socket"))
+                },
+                Err(e) => return Err(e),
             }
         } else {
             // System call succeeded, parse response.
@@ -93,13 +82,18 @@ pub fn send(sockfd: i32, buffer: *const u8, count: size_t, flags: i32) -> ssize_
                         total_written += response.count;
                         offset += chunk_size;
                     },
-                    _ => return ErrorCode::InvalidMessage.into_errno(),
+                    _ => {
+                        return Err(Error::new(
+                            ErrorCode::InvalidMessage,
+                            "unexpected message header",
+                        ))
+                    },
                 },
                 // Response was not successfully parsed.
-                Err(_) => return ErrorCode::InvalidMessage.into_errno(),
+                Err(_) => return Err(Error::new(ErrorCode::InvalidMessage, "invalid response")),
             }
         }
     }
 
-    total_written
+    Ok(total_written)
 }
