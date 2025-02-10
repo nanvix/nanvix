d2b55bf206d6af74211664abdcdaf7a2a487a22f
Eduardo Lemos <eduardolemosep@gmail.com>
2025-02-10 18:59:44 -0300

    [benchmarks] F: Added CPU bound microbenchmark

--- build/user/build.rs
+++ build/user/build.rs
@@ -13,7 +13,12 @@
 
 use std::{
     env,
-    path::Path,
+    fs::File,
+    io::Read,
+    path::{
+        Path,
+        PathBuf,
+    },
     process::{
         Command,
         ExitStatus,
@@ -34,6 +39,26 @@ fn main() {
         Ok(out_dir) => out_dir,
         Err(_) => panic!("failed to get OUT_DIR environment variable"),
     };
+
+    //==============================================================================================
+    //Benchmark-Specific Environment Variables
+    //==============================================================================================
+
+    // Get CARGO_MANIFEST_DIR Environment Variable.
+    let manifest_dir: String = match env::var("CARGO_MANIFEST_DIR") {
+        Ok(mdir) => mdir,
+        Err(_) => panic!("failed to get CARGO_MANIFEST_DIR environment variable"),
+    };
+
+    let config_path: PathBuf = Path::new(&manifest_dir).join("config.json");
+    if config_path.exists() {
+        println!("cargo:rerun-if-changed=./config.json");
+        let mut file: File = File::open(&config_path).expect("Failed to open file");
+        let mut raw_content: String = String::new();
+        let _ = file.read_to_string(&mut raw_content);
+        let content = raw_content.replace("\n", "").replace(" ", "");
+        println!("cargo:rustc-env=CONFIG={}", content);
+    }
 
     //==============================================================================================
     // Configure Toolchain
@@ -147,5 +172,21 @@ fn main() {
         let name: String = name.to_uppercase().replace("NANVIX_", "");
         let feature: String = name.to_lowercase();
         println!("cargo:rustc-cfg=feature=\"{}\"", feature);
+    }
+
+    // Get CARGO_MANIFEST_DIR Environment Variable.
+    let manifest_dir: String = match env::var("CARGO_MANIFEST_DIR") {
+        Ok(mdir) => mdir,
+        Err(_) => panic!("failed to get CARGO_MANIFEST_DIR environment variable"),
+    };
+
+    let config_path: PathBuf = Path::new(&manifest_dir).join("config.json");
+    if config_path.exists() {
+        println!("cargo:rerun-if-changed=config.json");
+        let mut file: File = File::open(&config_path).expect("Failed to open config.json file");
+        let mut raw_content: String = String::new();
+        let _ = file.read_to_string(&mut raw_content);
+        let content = raw_content.replace("\n", "").replace(" ", "");
+        println!("cargo:rustc-env=CONFIG={}", content);
     }
 }
