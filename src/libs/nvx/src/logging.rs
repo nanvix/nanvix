a2f5b58180e1569b4235156bd302db6aabf6f60e
Pedro Henrique Penna <ppenna@microsoft.com>
2025-02-10 13:08:06 -0800

    [libs] E: Logging Support

--- src/libs/nvx/src/logging.rs
+++ src/libs/nvx/src/logging.rs
@@ -5,7 +5,29 @@
 // Imports
 //==================================================================================================
 
-use ::core::fmt;
+use ::core::{
+    fmt,
+    fmt::Write,
+};
+
+//==================================================================================================
+// Constants
+//==================================================================================================
+
+/// Maximum log level.
+pub const MAX_LEVEL: LogLevel = if cfg!(feature = "trace") {
+    LogLevel::Trace
+} else if cfg!(feature = "debug") {
+    LogLevel::Debug
+} else if cfg!(feature = "info") {
+    LogLevel::Info
+} else if cfg!(feature = "warn") {
+    LogLevel::Warn
+} else if cfg!(feature = "error") {
+    LogLevel::Error
+} else {
+    LogLevel::Panic
+};
 
 //==================================================================================================
 // Structures
@@ -13,6 +35,30 @@ use ::core::fmt;
 
 /// A formatter object
 pub struct Logger;
+
+/// Log levels.
+#[derive(PartialEq, Eq, PartialOrd, Ord)]
+pub enum LogLevel {
+    Panic,
+    Error,
+    Warn,
+    Info,
+    Debug,
+    Trace,
+}
+
+impl core::fmt::Debug for LogLevel {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            LogLevel::Trace => write!(f, "TRACE"),
+            LogLevel::Debug => write!(f, "DEBUG"),
+            LogLevel::Info => write!(f, "INFO"),
+            LogLevel::Warn => write!(f, "WARN"),
+            LogLevel::Error => write!(f, "ERROR"),
+            LogLevel::Panic => write!(f, "PANIC"),
+        }
+    }
+}
 
 //==================================================================================================
 // Trait Implementations
@@ -22,5 +68,13 @@ impl fmt::Write for Logger {
     fn write_str(&mut self, s: &str) -> fmt::Result {
         let _ = ::sys::kcall::debug::debug(s.as_ptr(), s.len());
         Ok(())
+    }
+}
+
+impl Logger {
+    pub fn get(tag: &str, level: LogLevel) -> Self {
+        let mut ret: Self = Self;
+        let _ = write!(&mut ret, "[{:?}][{}] ", level, tag);
+        ret
     }
 }
