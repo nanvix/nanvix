088a2791749a440ddb05d8f67cda68f959823ca2
Pedro Henrique Penna <ppenna@microsoft.com>
2025-02-28 07:32:54 -0800

    [libs] E: sched_param

--- src/libs/posix/src/sched/mod.rs
+++ src/libs/posix/src/sched/mod.rs
@@ -2,6 +2,18 @@
 // Licensed under the MIT License.
 
 //==================================================================================================
+// Configuration
+//==================================================================================================
+
+#![allow(non_camel_case_types)]
+
+//==================================================================================================
+// Imports
+//==================================================================================================
+
+use crate::ffi::c_int;
+
+//==================================================================================================
 // Modules
 //==================================================================================================
 
@@ -16,3 +28,28 @@ cfg_if::cfg_if! {
 
 #[cfg(all(feature = "syscall", feature = "staticlib"))]
 pub mod bindings;
+
+//==================================================================================================
+// Structures
+//==================================================================================================
+
+///
+/// # Description
+///
+/// Used to set and get scheduling parameters.
+///
+#[derive(Default, Debug, Clone, Copy)]
+#[repr(C, packed)]
+pub struct sched_param {
+    /// Process or thread scheduling priority.
+    pub sched_priority: c_int,
+}
+::nvx::sys::static_assert_size!(sched_param, sched_param::SIZE);
+
+impl sched_param {
+    /// Size of the `sched_priority` field.
+    const SIZE_OF_SCHED_PRIORITY: usize = core::mem::size_of::<c_int>();
+
+    /// Size of `sched_param` structure.
+    pub const SIZE: usize = Self::SIZE_OF_SCHED_PRIORITY;
+}
