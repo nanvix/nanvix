05ba9a5ef3eee2c589afac87ecf4454bc5ce8381
Pedro Henrique Penna <ppenna@microsoft.com>
2025-03-07 10:07:47 -0800

    [tests] E: Dynamic Mutex Initialization

--- src/tests/thread-c/main.c
+++ src/tests/thread-c/main.c
@@ -90,6 +90,7 @@ int main(int argc, const char *argv[])
     test_pthread_self();
     test_pthread_create_join();
     test_pthread_mutex_static_init();
+    test_pthread_mutex_dynamic_init();
 
     // Must be last test.
     test_pthread_nowait();
