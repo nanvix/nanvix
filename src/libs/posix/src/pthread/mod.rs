4b8d8b3cf4209b88bcc6cb253fbc9b8247d7bafe
Pedro Henrique Penna <ppenna@microsoft.com>
2025-03-13 15:11:09 -0700

    [libs] F: Operations for Thread-Specific Data Area

--- src/libs/posix/src/pthread/mod.rs
+++ src/libs/posix/src/pthread/mod.rs
@@ -36,6 +36,10 @@ cfg_if::cfg_if! {
         pub use syscall::pthread_cond_init;
         pub use syscall::pthread_cond_signal;
         pub use syscall::pthread_cond_wait;
+        pub use syscall::pthread_key_create;
+        pub use syscall::pthread_key_delete;
+        pub use syscall::pthread_setspecific;
+        pub use syscall::pthread_getspecific;
     }
 }
 
