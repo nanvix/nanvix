139258a2f3ddea2c614ada8c310c6166ef054bba
Pedro Henrique Penna <ppenna@microsoft.com>
2025-03-14 08:13:00 -0700

    [libs] F: Initial uname() Impl
    This commit resolves https://github.com/nanvix/nanvix/issues/475.

---
+++ src/libs/posix/src/sys/utsname/bindings.rs
@@ -0,0 +1,66 @@
+// Copyright(c) The Maintainers of Nanvix.
+// Licensed under the MIT License.
+
+//==================================================================================================
+// Imports
+//==================================================================================================
+
+use crate::{
+    errno::errno,
+    ffi::c_int,
+    sys::utsname::{
+        syscall,
+        utsname,
+    },
+};
+use nvx::sys::error::ErrorCode;
+
+//==================================================================================================
+// Standalone Functions
+//==================================================================================================
+
+///
+/// # Description
+///
+/// Get information of the current system.
+///
+/// # Parameters
+///
+/// - `name`: Storage location for the system information.
+///
+/// # Returns
+///
+/// If successful, zero is returned. Otherwise, `-1`` is returned and `errno` is set to indicate the
+/// error.
+///
+/// # Safety
+///
+/// This function is unsafe because it may deference raw pointers.
+///
+/// It is safe to use this function if and only if all the following conditions are met:
+///
+/// - The `name` points to a valid [`utsname`] structure.
+///
+#[no_mangle]
+pub unsafe extern "C" fn uname(name: *mut utsname) -> c_int {
+    // Check if name is not valid.
+    if name.is_null() {
+        ::nvx::error!("uname(): name is null");
+        errno = ErrorCode::InvalidArgument.into_errno();
+        return -1;
+    }
+
+    // Execute system call and check for errors.
+    match syscall::uname() {
+        // Success, copy data to user buffer.
+        Ok(name_) => {
+            *name = name_;
+            0
+        },
+        // Error, set errno.
+        Err(error) => {
+            errno = error.code.into_errno();
+            -1
+        },
+    }
+}
