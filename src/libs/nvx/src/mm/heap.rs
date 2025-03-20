35708dd58b50e1672aa311bacbf328ccc22a1d22
Pedro Henrique Penna <ppenna@microsoft.com>
2025-03-19 17:01:07 -0700

    [libs] E: Use Logging Facility

--- src/libs/nvx/src/mm/heap.rs
+++ src/libs/nvx/src/mm/heap.rs
@@ -5,15 +5,6 @@
 // Imports
 //==================================================================================================
 
-// The following imports are used only when any logging feature is enabled.
-#[allow(unused_imports)]
-use crate::logging::{
-    LogLevel,
-    Logger,
-};
-#[allow(unused_imports)]
-use ::core::fmt::Write;
-
 use ::sys::{
     arch::{
         mem,
@@ -51,48 +42,29 @@ impl Heap {
         size: usize,
         capacity: usize,
     ) -> Result<Self, Error> {
-        #[cfg(feature = "trace")]
-        let _ = writeln!(
-            &mut Logger::get(module_path!(), LogLevel::Trace),
-            "new(): base={:X?}, size={:X?}, capacity={:X?}",
-            base,
-            size,
-            capacity
-        );
+        crate::trace!("new(): base={:X?}, size={:X?}, capacity={:X?}", base, size, capacity);
 
         // Check if base address is page-aligned.
         if !base.is_aligned(PAGE_ALIGNMENT) {
-            #[cfg(feature = "error")]
-            let _ = writeln!(
-                &mut Logger::get(module_path!(), LogLevel::Error),
-                "new(): unaligned base address {:X?}",
-                base
-            );
+            crate::error!("new(): unaligned base address {:X?}", base);
             return Err(Error::new(ErrorCode::BadAddress, "unaligned base address"));
         }
 
         // Check if size is zero.
         if size == 0 {
-            #[cfg(feature = "error")]
-            let _ = writeln!(&mut Logger::get(module_path!(), LogLevel::Error), "new(): zero size");
+            crate::error!("new(): zero size");
             return Err(Error::new(ErrorCode::BadAddress, "zero size"));
         }
 
         // Check if capacity is zero.
         if capacity == 0 {
-            #[cfg(feature = "error")]
-            let _ =
-                writeln!(&mut Logger::get(module_path!(), LogLevel::Error), "new(): zero capacity");
+            crate::error!("new(): zero capacity");
             return Err(Error::new(ErrorCode::BadAddress, "zero capacity"));
         }
 
         // Check if capacity is smaller than size.
         if capacity < size {
-            #[cfg(feature = "error")]
-            let _ = writeln!(
-                &mut Logger::get(module_path!(), LogLevel::Error),
-                "new(): capacity is too small"
-            );
+            crate::error!("new(): capacity is too small");
             return Err(Error::new(ErrorCode::BadAddress, "capacity is too small"));
         }
 
@@ -118,40 +90,23 @@ impl Heap {
     }
 
     pub fn grow(&mut self, increment: usize) -> Result<(), Error> {
-        #[cfg(feature = "trace")]
-        let _ = writeln!(
-            &mut Logger::get(module_path!(), LogLevel::Trace),
-            "grow(): increment={:X?}",
-            increment
-        );
+        crate::trace!("grow(): increment={:X?}", increment);
 
         // Check if increment is page-aligned.
         if !mm::is_aligned(increment, PAGE_ALIGNMENT) {
-            #[cfg(feature = "error")]
-            let _ = writeln!(
-                &mut Logger::get(module_path!(), LogLevel::Error),
-                "grow(): unaligned increment"
-            );
+            crate::error!("grow(): unaligned increment");
             return Err(Error::new(ErrorCode::BadAddress, "unaligned increment"));
         }
 
         // Check if increment is zero.
         if increment == 0 {
-            #[cfg(feature = "error")]
-            let _ = writeln!(
-                &mut Logger::get(module_path!(), LogLevel::Error),
-                "grow(): zero increment"
-            );
+            crate::error!("grow(): zero increment");
             return Err(Error::new(ErrorCode::BadAddress, "zero increment"));
         }
 
         // Check if increment would exceed capacity.
         if self.size + increment > self.capacity {
-            #[cfg(feature = "error")]
-            let _ = writeln!(
-                &mut Logger::get(module_path!(), LogLevel::Error),
-                "grow(): exceeds capacity"
-            );
+            crate::error!("grow(): exceeds capacity");
             return Err(Error::new(ErrorCode::BadAddress, "exceeds capacity"));
         }
 
@@ -173,13 +128,7 @@ pub fn map_range(
     start: VirtualAddress,
     end: VirtualAddress,
 ) -> Result<(), Error> {
-    #[cfg(feature = "trace")]
-    let _ = writeln!(
-        &mut Logger::get(module_path!(), LogLevel::Trace),
-        "map_range(): start={:X?}, end={:X?}",
-        start,
-        end
-    );
+    crate::trace!("map_range(): start={:X?}, end={:X?}", start, end);
 
     debug_assert!(start.is_aligned(PAGE_ALIGNMENT));
     debug_assert!(end.is_aligned(PAGE_ALIGNMENT));
@@ -196,9 +145,7 @@ pub fn map_range(
         if let Err(error) = kcall::mm::mmap(pid, vaddr, AccessPermission::RDWR) {
             // Failed to map page, attempt to rollback.
 
-            #[cfg(feature = "error")]
-            let _ = writeln!(
-                &mut Logger::get(module_path!(), LogLevel::Error),
+            crate::error!(
                 "map_range(): failed to map page at {:X?}, rolling back (error={:?})",
                 vaddr,
                 error
@@ -207,9 +154,7 @@ pub fn map_range(
             // Attempt to unmap pages.
             if let Err(_error) = unmap_range(pid, VirtualAddress::new(start), vaddr) {
                 // Failed to unmap range, warn.
-                #[cfg(feature = "warn")]
-                let _ = writeln!(
-                    &mut Logger::get(module_path!(), LogLevel::Warn),
+                crate::warn!(
                     "map_range(): failed to unmap pages at {:X?}..{:X?} (error={:?})",
                     start,
                     vaddr,
@@ -232,12 +177,7 @@ pub fn unmap_range(
     start: VirtualAddress,
     end: VirtualAddress,
 ) -> Result<(), Error> {
-    let _ = writeln!(
-        &mut Logger::get(module_path!(), LogLevel::Trace),
-        "unmap_range(): start={:X?}, end={:X?}",
-        start,
-        end
-    );
+    crate::trace!("unmap_range(): start={:X?}, end={:X?}", start, end);
 
     debug_assert!(start.is_aligned(PAGE_ALIGNMENT));
     debug_assert!(end.is_aligned(PAGE_ALIGNMENT));
@@ -252,9 +192,7 @@ pub fn unmap_range(
         let vaddr: VirtualAddress = VirtualAddress::from_raw_value(vaddr);
 
         if let Err(error) = kcall::mm::munmap(pid, vaddr) {
-            #[cfg(feature = "error")]
-            let _ = writeln!(
-                &mut Logger::get(module_path!(), LogLevel::Error),
+            crate::error!(
                 "unmap_range(): failed to unmap page at {:X?}, skipping (error={:?})",
                 vaddr,
                 error
@@ -269,9 +207,7 @@ pub fn unmap_range(
 }
 impl Drop for Heap {
     fn drop(&mut self) {
-        #[cfg(feature = "trace")]
-        let _ = writeln!(
-            &mut Logger::get(module_path!(), LogLevel::Trace),
+        crate::trace!(
             "drop(): base={:X?}, size={:X?}, capacity={:X?}",
             self.base,
             self.size,
@@ -284,12 +220,7 @@ impl Drop for Heap {
             self.base,
             VirtualAddress::from_raw_value(self.base.into_raw_value() + self.size),
         ) {
-            #[cfg(feature = "warn")]
-            let _ = writeln!(
-                &mut Logger::get(module_path!(), LogLevel::Warn),
-                "drop(): failed to unmap pages (error={:?})",
-                _error
-            );
+            crate::warn!("drop(): failed to unmap pages (error={:?})", _error);
         }
     }
 }
