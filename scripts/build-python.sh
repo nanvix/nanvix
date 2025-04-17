dd9423a533ea62630828cefc68e4b26861477fda
Pedro Henrique Penna <ppenna@microsoft.com>
2025-04-17 07:36:11 -0700

    [scripts] B: Force Python Binary Relink

--- scripts/build-python.sh
+++ scripts/build-python.sh
@@ -124,6 +124,10 @@ build() {
 	# Check if we need to configure or not.
 	if [ ! -f "${CPYTHON_HOME}/Makefile" ]; then
 		configure
+	else
+		# Remove the existing binary to ensure it links with the updated system libraries.
+		# Note: Running 'make clean' would remove all object files, which is unnecessary here.
+		rm -f python
 	fi
 
 	make_all
