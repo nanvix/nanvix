47d1a5536b52e0ed50a999f6f880d6623a018dc3
Pedro Henrique Penna <ppenna@microsoft.com>
2025-04-22 11:43:29 -0700

    [tests] E: Improving creat unlink Test

--- src/tests/file-c/create_unlink.c
+++ src/tests/file-c/create_unlink.c
@@ -9,6 +9,9 @@
 
 #include <assert.h>
 #include <fcntl.h>
+#include <limits.h>
+#include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 
 //==================================================================================================
@@ -18,12 +21,18 @@
 // Tests wether we can create and unlink a file.
 void test_create_unlink(void)
 {
-    const char *filename = "testfile.txt";
+    printf("testing create()/unlink() ... ");
 
-    // Create a file.
+    const char *filename = "testfile.txt";
+    assert(strlen(filename) <= NAME_MAX);
+
+    // Create a test file.
     int fd = open(filename, O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR);
     assert(fd != -1);
+    assert(close(fd) == 0);
 
-    // Unlink the file.
+    // Remove test file.
     assert(unlink(filename) == 0);
+
+    printf("ok\n");
 }
--- src/tests/file-c/main.c
+++ src/tests/file-c/main.c
@@ -95,7 +95,7 @@ int main(int argc, const char *argv[])
 
     // Run tests.
     test_open_close();
-    test_create_unlink();
+    test_create_unlink(); // tests open() and unlink().
     test_stat();
     test_renameat(); // requires open(), close() and unlink().
     test_unlinkat(); // requires open() and close().
