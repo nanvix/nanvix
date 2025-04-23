557a9c990fbd1cedcb5f584449eb515601dc2297
Pedro Henrique Penna <ppenna@microsoft.com>
2025-04-23 09:50:58 -0700

    [libs] B: Fixing Time Modules

--- src/libs/posix/src/sys/time/mod.rs
+++ src/libs/posix/src/sys/time/mod.rs
@@ -8,36 +8,29 @@
 #![allow(non_camel_case_types)]
 
 //==================================================================================================
-// Types
+// Structures
 //==================================================================================================
 
 // TODO: fix this to reflect the actual implementation.
 pub type timeval = ();
 
-// TODO: fix this to reflect the actual implementation.
-pub type timezone = ();
+//==================================================================================================
+// Re-Exports
+//==================================================================================================
+
+// TODO: import `fd_set`` type and the `timeval` structure from `sys::select`.
+
+pub use crate::sys::types::time_t;
+
+// TODO: import `suseconds_t ` type from `sys::types`.
+
+// TODO: import  `FD_CLR()`, `FD_ISSET()`, `FD_SET()`, `FD_ZERO()`, functions and  FD_SETSIZE constant from `sys::select`.
+
+// TODO: import all symbols from `sys::select` visible.
 
 //==================================================================================================
 // Standalone Functions
 //==================================================================================================
 
 #[cfg(all(feature = "syscall", feature = "staticlib"))]
-mod bindings {
-    use super::*;
-    use crate::{
-        errno::errno,
-        ffi::c_int,
-    };
-    use ::nvx::sys::error::ErrorCode;
-
-    #[allow(clippy::missing_safety_doc)]
-    #[no_mangle]
-    pub unsafe extern "C" fn gettimeofday(_tp: *mut timeval, _tzp: *mut timezone) -> c_int {
-        // TODO: https://github.com/nanvix/nanvix/issues/317
-        ::nvx::error!("gettimeofday(): not implemented");
-        unsafe {
-            errno = ErrorCode::InvalidSysCall.into_errno();
-        }
-        -1
-    }
-}
+pub mod bindings;
