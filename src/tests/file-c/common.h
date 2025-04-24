b780649f0e23fdaa87f57ac89dec44c1eeb336c9
Pedro Henrique Penna <ppenna@microsoft.com>
2025-04-24 08:15:28 -0700

    [tests] E: C Test for mkdir()

--- src/tests/file-c/common.h
+++ src/tests/file-c/common.h
@@ -27,6 +27,9 @@ extern void test_fchmod(void);
 // Tests whether we can create a directory.
 extern void test_mkdirat(void);
 
+// Tests whether we can create a directory.
+extern void test_mkdir(void);
+
 // Tests whether we can rename a file.
 extern void test_renameat(void);
 
