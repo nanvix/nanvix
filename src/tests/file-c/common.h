70c75c08a9b24f6c2fe1442631548fb53747ac7a
Pedro Henrique Penna <ppenna@microsoft.com>
2025-04-24 08:05:54 -0700

    [tests] E: C Test for mkdirat

--- src/tests/file-c/common.h
+++ src/tests/file-c/common.h
@@ -24,6 +24,9 @@ extern void test_fchdir(void);
 // Tests whether we can change access permissions of a file.
 extern void test_fchmod(void);
 
+// Tests whether we can create a directory.
+extern void test_mkdirat(void);
+
 // Tests whether we can rename a file.
 extern void test_renameat(void);
 
