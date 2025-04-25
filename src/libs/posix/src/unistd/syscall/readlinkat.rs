b4113a9ebdce0bd67cee0ef13c6e7bc2910fdd86
Pedro Henrique Penna <ppenna@microsoft.com>
2025-04-25 06:28:45 -0700

    [libs] E: Return Result for readlinkat

--- src/libs/posix/src/unistd/syscall/readlinkat.rs
+++ src/libs/posix/src/unistd/syscall/readlinkat.rs
@@ -11,6 +11,7 @@ use crate::{
         LinuxDaemonMessagePart,
         MessagePartitioner,
     },
+    sys::types::ssize_t,
     unistd::message::{
         ReadLinkAtRequest,
         ReadLinkAtResponse,
@@ -25,103 +26,136 @@ use ::alloc::{
 use ::nvx::{
     ipc::Message,
     pm::ProcessIdentifier,
-    sys::error::ErrorCode,
+    sys::error::{
+        Error,
+        ErrorCode,
+    },
 };
 
 //==================================================================================================
 // Standalone Functions
 //==================================================================================================
 
-pub fn readlinkat(dirfd: i32, path: &str, buf: &mut [u8]) -> i32 {
-    // Send request.
-    let status: i32 = readlinkat_request(dirfd, path, buf);
-    if status != 0 {
-        return status;
-    }
+///
+/// # Description
+///
+/// Reads the value of a symbolic link relative to a directory file descriptor.
+///
+/// # Parameters
+///
+/// - `dirfd`: Directory file descriptor.
+/// - `path`: Path to the symbolic link.
+/// - `buf`: Buffer to store the value of the symbolic link.
+///
+/// # Returns
+///
+/// Upon successful completion, `readlinkat()` returns the number of bytes read. Otherwise, it
+/// returns an error.
+///
+pub fn readlinkat(dirfd: i32, path: &str, buf: &mut [u8]) -> Result<ssize_t, Error> {
+    ::nvx::trace!("readlinkat(): dirfd={:?}, path={:?}, buf.len={:?}", dirfd, path, buf.len());
 
-    // Wait for response.
-    readlinkat_response(buf)
-}
+    let pid: ProcessIdentifier = ::nvx::pm::getpid()?;
 
-fn readlinkat_request(dirfd: i32, path: &str, buf: &[u8]) -> i32 {
-    let pid: ProcessIdentifier = match ::nvx::pm::getpid() {
-        Ok(pid) => pid,
-        Err(e) => return e.code.into_errno(),
-    };
+    let request: ReadLinkAtRequest = ReadLinkAtRequest::new(dirfd, path.to_string(), buf.len())?;
 
-    let request: ReadLinkAtRequest =
-        match ReadLinkAtRequest::new(dirfd, path.to_string(), buf.len()) {
-            Ok(request) => request,
-            Err(e) => return e.code.into_errno(),
-        };
-
-    let requests: Vec<Message> = match request.into_parts(pid) {
-        Ok(requests) => requests,
-        Err(e) => return e.code.into_errno(),
-    };
+    let requests: Vec<Message> = request.into_parts(pid)?;
 
     for request in requests {
-        match ::nvx::ipc::send(&request) {
-            Ok(_) => (),
-            Err(e) => return e.code.into_errno(),
-        }
+        ::nvx::ipc::send(&request)?;
     }
 
-    0
-}
-
-fn readlinkat_response(buf: &mut [u8]) -> i32 {
     let capacity: usize =
         ReadLinkAtResponse::MAX_SIZE.div_ceil(LinuxDaemonMessagePart::PAYLOAD_SIZE);
 
-    let mut assembler: LinuxDaemonLongMessage = match LinuxDaemonLongMessage::new(capacity) {
-        Ok(assembler) => assembler,
-        Err(e) => return e.code.into_errno(),
-    };
+    let mut assembler: LinuxDaemonLongMessage = LinuxDaemonLongMessage::new(capacity)?;
 
     loop {
-        let response: Message = match ::nvx::ipc::recv() {
-            Ok(response) => response,
-            Err(e) => break e.code.into_errno(),
-        };
+        let response: Message = ::nvx::ipc::recv()?;
 
         // Check whether system call succeeded or not.
         if response.status != 0 {
-            // System call failed, parse error code and return it.
+            ::nvx::error!(
+                "readlinkat(): system call failed (dirfd={:?}, path={:?}, error_code={:?})",
+                dirfd,
+                path,
+                { response.status }
+            );
+            // System call failed, parse error code and return.
             match ErrorCode::try_from(response.status) {
-                Ok(e) => break e.into_errno(),
-                Err(_) => break ErrorCode::InvalidMessage.into_errno(),
+                Ok(error_code) => break Err(Error::new(error_code, "system call failed")),
+                Err(error) => {
+                    ::nvx::error!(
+                        "readlinkat(): failed to parse error code (dirfd={:?}, path={:?}, \
+                         error_code={:?})",
+                        dirfd,
+                        path,
+                        error
+                    );
+                    break Err(Error::new(ErrorCode::InvalidMessage, "failed to parse error code"));
+                },
             }
         } else {
             // System call succeeded, parse response.
-            match LinuxDaemonMessage::try_from_bytes(response.payload) {
-                Ok(message) => match message.header {
-                    LinuxDaemonMessageHeader::ReadLinkAtResponsePart => {
-                        let part: LinuxDaemonMessagePart =
-                            LinuxDaemonMessagePart::from_bytes(message.payload);
+            let message: LinuxDaemonMessage = LinuxDaemonMessage::try_from_bytes(response.payload)?;
+            match message.header {
+                LinuxDaemonMessageHeader::ReadLinkAtResponsePart => {
+                    let part: LinuxDaemonMessagePart =
+                        LinuxDaemonMessagePart::from_bytes(message.payload);
 
-                        if let Err(e) = assembler.add_part(part) {
-                            break e.code.into_errno();
-                        }
+                    if let Err(error) = assembler.add_part(part) {
+                        ::nvx::error!(
+                            "readlinkat(): failed to add part (dirfd={:?}, path={:?}, \
+                             error_code={:?})",
+                            dirfd,
+                            path,
+                            error
+                        );
+                        break Err(Error::new(
+                            ErrorCode::InvalidMessage,
+                            "failed to assemble response part",
+                        ));
+                    }
 
-                        if !assembler.is_complete() {
-                            continue;
-                        }
+                    if !assembler.is_complete() {
+                        continue;
+                    }
 
-                        let parts: Vec<LinuxDaemonMessagePart> = assembler.take_parts();
+                    let parts: Vec<LinuxDaemonMessagePart> = assembler.take_parts();
 
-                        match ReadLinkAtResponse::from_parts(&parts) {
-                            Ok(response) => {
-                                assert!(response.buffer.len() <= buf.len());
-                                buf[..response.buffer.len()].copy_from_slice(&response.buffer);
-                                break response.buffer.len() as i32;
-                            },
-                            Err(_) => break ErrorCode::InvalidMessage.into_errno(),
-                        }
-                    },
-                    _ => break ErrorCode::InvalidMessage.into_errno(),
+                    match ReadLinkAtResponse::from_parts(&parts) {
+                        Ok(response) => {
+                            assert!(response.buffer.len() <= buf.len());
+                            buf[..response.buffer.len()].copy_from_slice(&response.buffer);
+                            break Ok(response.buffer.len() as i32);
+                        },
+                        Err(error) => {
+                            ::nvx::error!(
+                                "readlinkat(): failed to assemble response (dirfd={:?}, \
+                                 path={:?}, error_code={:?})",
+                                dirfd,
+                                path,
+                                error
+                            );
+                            break Err(Error::new(
+                                ErrorCode::InvalidMessage,
+                                "failed to assemble response",
+                            ));
+                        },
+                    }
                 },
-                Err(_) => break ErrorCode::InvalidMessage.into_errno(),
+                header => {
+                    break {
+                        ::nvx::error!(
+                            "readlinkat(): failed to parse response (dirfd={:?}, path={:?}, \
+                             header={:?})",
+                            dirfd,
+                            path,
+                            header
+                        );
+                        Err(Error::new(ErrorCode::InvalidMessage, "failed to parse response"))
+                    }
+                },
             }
         }
     }
