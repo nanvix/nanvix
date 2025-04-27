9ded8168100e22d7d2c44d65d16c5911c165c991
Pedro Henrique Penna <ppenna@microsoft.com>
2025-04-27 13:56:54 -0700

    [libs] E: Return Result for clock_getres

--- src/libs/posix/src/time/syscall/clock_getres.rs
+++ src/libs/posix/src/time/syscall/clock_getres.rs
@@ -17,11 +17,13 @@ use crate::{
     LinuxDaemonMessage,
     LinuxDaemonMessageHeader,
 };
-use ::core::ffi;
 use ::nvx::{
     ipc::Message,
     pm::ProcessIdentifier,
-    sys::error::ErrorCode,
+    sys::error::{
+        Error,
+        ErrorCode,
+    },
 };
 
 //==================================================================================================
@@ -30,55 +32,72 @@ use ::nvx::{
 
 /// Get clock resolution.
 #[allow(clippy::not_unsafe_ptr_arg_deref)]
-pub fn clock_getres(clock_id: clockid_t, res: *mut timespec) -> ffi::c_int {
-    let pid: ProcessIdentifier = match ::nvx::pm::getpid() {
-        Ok(pid) => pid,
-        Err(e) => return e.code.into_errno(),
-    };
+pub fn clock_getres(clock_id: clockid_t, res: Option<&mut timespec>) -> Result<(), Error> {
+    ::nvx::error!("clock_getres(): clock_id={:?}, res={:?}", clock_id, res);
+
+    let pid: ProcessIdentifier = ::nvx::pm::getpid()?;
 
     // Build request and send it.
     let request: Message = ClockResolutionRequest::build(pid, clock_id);
-    if let Err(e) = ::nvx::ipc::send(&request) {
-        return e.code.into_errno();
-    }
+    ::nvx::ipc::send(&request)?;
 
     // Receive response.
-    let response: Message = match ::nvx::ipc::recv() {
-        Ok(response) => response,
-        Err(e) => return e.code.into_errno(),
-    };
+    let response: Message = ::nvx::ipc::recv()?;
 
     // Check whether system call succeeded or not.
     if response.status != 0 {
+        ::nvx::error!(
+            "clock_getres(): failed (clock_id={:?}, res={:?}, status={:?})",
+            clock_id,
+            res,
+            { response.status }
+        );
         // System call failed, parse error code and return it.
         match ErrorCode::try_from(response.status) {
-            Ok(e) => e.into_errno(),
-            Err(_) => ErrorCode::InvalidMessage.into_errno(),
+            // Error code was successfully parsed.
+            Ok(error_code) => {
+                // Return error code.
+                Err(Error::new(error_code, "clock_getres() failed"))
+            },
+            // Error code was not successfully parsed.
+            Err(error) => {
+                ::nvx::error!(
+                    "clock_getres(): failed to parse error code (clock_id={:?}, res={:?}, \
+                     error={:?})",
+                    clock_id,
+                    res,
+                    error
+                );
+                Err(Error::new(ErrorCode::TryAgain, "clock_getres() failed"))
+            },
         }
     } else {
         // System call succeeded, parse response.
-        match LinuxDaemonMessage::try_from_bytes(response.payload) {
-            // Response was successfully parsed.
-            Ok(message) => match message.header {
-                LinuxDaemonMessageHeader::GetClockResolutionResponse => {
-                    let response: ClockGetResolutionResponse =
-                        ClockGetResolutionResponse::from_bytes(message.payload);
+        let message: LinuxDaemonMessage = LinuxDaemonMessage::try_from_bytes(response.payload)?;
+        // Response was successfully parsed.
+        match message.header {
+            LinuxDaemonMessageHeader::GetClockResolutionResponse => {
+                let response: ClockGetResolutionResponse =
+                    ClockGetResolutionResponse::from_bytes(message.payload);
 
-                    // Copy resolution if requested.
-                    if !res.is_null() {
-                        unsafe {
-                            (*res).tv_sec = response.res.tv_sec;
-                            (*res).tv_nsec = response.res.tv_nsec;
-                        }
-                    }
+                // Copy resolution if requested.
+                if let Some(res) = res {
+                    res.tv_sec = response.res.tv_sec;
+                    res.tv_nsec = response.res.tv_nsec;
+                }
 
-                    0
-                },
-                // Unexpected response message.
-                _ => ErrorCode::InvalidMessage.into_errno(),
+                Ok(())
             },
-            // Failed to parse response.
-            Err(e) => e.code.into_errno(),
+            // Unexpected response message.
+            header => {
+                ::nvx::error!(
+                    "clock_getres(): invalid response (clock_id={:?}, res={:?}, header={:?})",
+                    clock_id,
+                    res,
+                    header
+                );
+                Err(Error::new(ErrorCode::TryAgain, "clock_getres() failed"))
+            },
         }
     }
 }
