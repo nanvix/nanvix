b2ac0455db0cce70554c11f3d02c7390ba7c0998
Pedro Henrique Penna <ppenna@microsoft.com>
2025-04-26 20:16:26 -0700

    [libs] E: C Bindings for pread and preadv

--- src/libs/posix/src/sys/uio/bindings.rs
+++ src/libs/posix/src/sys/uio/bindings.rs
@@ -164,6 +164,125 @@ pub unsafe extern "C" fn pwritev(
 ///
 /// # Description
 ///
+/// Reads data from a file descriptor.
+///
+/// # Parameters
+///
+/// - `fd`: File descriptor.
+/// - `iov`: Pointer to an array of `iovec` structures.
+/// - `iovcnt`: Number of elements in the array.
+/// - `offset`: Offset to read from.
+///
+/// # Returns
+///
+/// Upon successful completion, `preadv()` returns the number of bytes read. Otherwise, it
+/// returns `-1` and sets `errno` to indicate the error.
+///
+/// # Safety
+///
+/// This function is unsafe because:
+/// - It may dereference raw pointers.
+/// - It may access global variables.
+///
+/// It is safe to call this function if and only if the following conditions are met:
+/// // - The `iov` pointer is valid and points to an array of `iovec` structures.
+/// // - This function is called from multiple threads at the same time.
+///
+#[no_mangle]
+pub unsafe extern "C" fn preadv(fd: i32, iov: *const iovec, iovcnt: i32, offset: off_t) -> ssize_t {
+    ::nvx::trace!("preadv(): fd={fd}, iov={iov:?}, iovcnt={iovcnt}, offset={offset}");
+
+    // Check if number of elements in the vector is valid.
+    if (iovcnt < 0) || (iovcnt > limits::IOV_MAX as i32) {
+        ::nvx::error!("preadv(): invalid iovcnt {iovcnt}");
+        errno = ErrorCode::InvalidArgument.get();
+        return -1;
+    }
+
+    // Check if vector base is invalid.
+    if iov.is_null() {
+        ::nvx::error!("preadv(): invalid iov {iov:?}");
+        errno = ErrorCode::InvalidArgument.get();
+        return -1;
+    }
+
+    // Check for zero-length vector.
+    if iovcnt == 0 {
+        return 0;
+    }
+
+    let do_preadv = |dry_run: bool| -> Result<size_t, Error> {
+        let mut offset: off_t = offset;
+        let mut total: size_t = 0;
+
+        // Traverse i/o vector.
+        for i in 0..iovcnt {
+            let iov: *const iovec = unsafe { iov.offset(i as isize) };
+
+            // Check if base address is invalid.
+            if iov.is_null() {
+                ::nvx::error!("preadv(): invalid iov {iov:?}");
+                return Err(Error::new(ErrorCode::InvalidArgument, "invalid iov"));
+            }
+
+            let iov_base: *mut u8 = unsafe { (*iov).iov_base };
+            let iov_len: size_t = unsafe { (*iov).iov_len };
+
+            // Check if base address is invalid.
+            if iov_base.is_null() {
+                ::nvx::error!("preadv(): invalid iov_base {iov_base:?}");
+                return Err(Error::new(ErrorCode::InvalidArgument, "invalid iov_base"));
+            }
+
+            total += if !dry_run {
+                let buffer: &mut [u8] =
+                    unsafe { slice::from_raw_parts_mut(iov_base, iov_len as usize) };
+
+                // Read data and check if read failed.
+                match unistd::pread(fd, buffer, offset) {
+                    Ok(count) => {
+                        offset += count as off_t;
+                        count as size_t
+                    },
+                    Err(error) => {
+                        ::nvx::error!("preadv(): read failed (errno={:?})", error);
+                        errno = error.code.get();
+                        return Err(error);
+                    },
+                }
+            } else {
+                iov_len as size_t
+            };
+        }
+
+        Ok(total)
+    };
+
+    // Perform a dry-run first.
+    match do_preadv(true) {
+        Ok(_count) => {
+            // Perform the actual read.
+            match do_preadv(false) {
+                Ok(count) => count as ssize_t,
+                Err(error) => {
+                    ::nvx::error!("preadv(): read failed (errno={:?})", error);
+                    errno = error.code.get();
+                    -1
+                },
+            }
+        },
+        // Dry-run failed because some other error.
+        Err(error) => {
+            ::nvx::error!("preadv(): dry-run failed (errno={:?})", error);
+            errno = error.code.get();
+            -1
+        },
+    }
+}
+
+///
+/// # Description
+///
 /// Reads a vector of data from a file.
 ///
 /// # Parameters
