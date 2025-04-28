93958df67794f51acee03a2bf1ab13f130563469
Pedro Henrique Penna <ppenna@microsoft.com>
2025-04-28 16:47:42 -0700

    [tests] E: Test Case for utimes

--- src/tests/file-c/main.c
+++ src/tests/file-c/main.c
@@ -120,6 +120,7 @@ int main(int argc, const char *argv[])
     test_mkdir();      // requires stat() and unlinkat().
     test_dirent();
     test_utimensat();
+    test_utimes(); // requires open(), close(), stat() and unlinkat().
     test_getcwd();
     test_chdir(); // requires getcwd().
     test_fchdir();
