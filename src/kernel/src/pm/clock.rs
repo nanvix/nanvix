73f111cc91fb68fbbc914777f165b110080b557c
Pedro Henrique Penna <ppenna@microsoft.com>
2025-04-30 13:11:21 -0700

    [kernel] E: TimerTicks Structure
    This commit introduces the TimerTicks structure, which wraps access to
    the TIMER_TICKS variable. It also improves counter precision to 64-bit
    value and uses a relaxed atomic implementation.

--- src/kernel/src/pm/clock.rs
+++ src/kernel/src/pm/clock.rs
@@ -9,6 +9,57 @@ use crate::{
     hal::arch::InterruptNumber,
     pm::ProcessManager,
 };
+use ::core::sync::atomic::{
+    AtomicU32,
+    Ordering,
+};
+
+//==================================================================================================
+// Global Variables
+//==================================================================================================
+
+///
+/// # Description
+///
+/// Counts the number of times in which the [`timer_handler`] was called.
+///
+static TIMER_TICKS: TimerTicks = TimerTicks::new();
+
+//==================================================================================================
+// TimerTicks
+//==================================================================================================
+
+/// A counter for the number of timer ticks.
+struct TimerTicks {
+    minor: AtomicU32,
+    major: AtomicU32,
+}
+
+impl TimerTicks {
+    const fn new() -> Self {
+        Self {
+            minor: AtomicU32::new(0),
+            major: AtomicU32::new(0),
+        }
+    }
+
+    /// Increments the number of ticks and returns the minor tick.
+    fn increment(&self) -> u32 {
+        // Safely increment `TIMER_TICKS`, assuming single-writer.
+        let minor: u32 = self.minor.load(Ordering::SeqCst);
+        let new_minor: u32 = minor.wrapping_add(1);
+        self.minor.store(new_minor, Ordering::SeqCst);
+
+        // Check if the minor tick overflowed.
+        if new_minor == 0 {
+            // Safely increment `TIMER_TICKS_MAJOR`, assuming single-writer.
+            let major: u32 = self.major.load(Ordering::SeqCst);
+            self.major.store(major.wrapping_add(1), Ordering::SeqCst);
+        }
+
+        new_minor
+    }
+}
 
 //==================================================================================================
 // Standalone Functions
@@ -27,15 +78,12 @@ use crate::{
 ///
 /// This function is unsafe because:
 /// - It may perform a context switch, suspending the current thread.
-/// - It may modify global variables.
+/// - It modifies global variables.
 ///
 pub unsafe fn timer_handler(_intnum: InterruptNumber) {
-    /// Counts the number of times in which the [`timer_handler`] was called.
-    static mut TIMER_TICKS: usize = 0;
+    let timer_ticks: usize = TIMER_TICKS.increment() as usize;
 
-    TIMER_TICKS = TIMER_TICKS.wrapping_add(1);
-
-    if TIMER_TICKS % config::kernel::SCHEDULER_FREQ == 0 {
+    if timer_ticks % config::kernel::SCHEDULER_FREQ == 0 {
         if let Err(error) = ProcessManager::switch() {
             error!("context switch failed: {:?}", error);
         }
