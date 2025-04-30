8efbcf3b29b1c4307d3b813c097dd019738302ae
Pedro Henrique Penna <ppenna@microsoft.com>
2025-04-30 15:34:45 -0700

    [kernel] E: Improve PIT Frequency Initialization
    This commit improves logic for setting the PIT frequency. We added check
    for the valid frequency range and apply round logic.

--- src/kernel/src/hal/platform/pit.rs
+++ src/kernel/src/hal/platform/pit.rs
@@ -9,10 +9,27 @@ use crate::hal::io::{
     IoPortAllocator,
     ReadWriteIoPort,
 };
+use ::core::sync::atomic::{
+    AtomicU32,
+    Ordering,
+};
 use ::sys::{
     arch::cpu::pit,
-    error::Error,
+    error::{
+        Error,
+        ErrorCode,
+    },
 };
+
+//==================================================================================================
+// Frequency
+//==================================================================================================
+
+///
+/// # Description
+///
+/// The frequency of the PIT device. The default value is overwritten after the PIT is initialized.
+static TIMER_FREQUENCY: AtomicU32 = AtomicU32::new(pit::PIT_MAX_FREQUENCY);
 
 //==================================================================================================
 // Structures
@@ -46,6 +63,27 @@ impl Pit {
     pub fn new(ioports: &mut IoPortAllocator, freq: u32) -> Result<Self, Error> {
         info!("initializing pit...");
 
+        // Check if frequency is valid.
+        if !(pit::PIT_MIN_FREQUENCY..=pit::PIT_MAX_FREQUENCY).contains(&freq) {
+            error!("new(): invalid frequency for pit (freq={})", freq);
+            return Err(Error::new(ErrorCode::InvalidArgument, "invalid frequency"));
+        }
+
+        // Calculate reload value rounding it to the nearest integer.
+        let freq_divisor: u32 = if pit::PIT_MAX_FREQUENCY % freq > pit::PIT_MAX_FREQUENCY / 2 {
+            pit::PIT_MAX_FREQUENCY / freq + 1
+        } else {
+            pit::PIT_MAX_FREQUENCY / freq
+        };
+
+        // Compute actual frequency based on frequency divisor.
+        let actual_freq: u32 = if pit::PIT_MAX_FREQUENCY % freq_divisor > pit::PIT_MAX_FREQUENCY / 2
+        {
+            pit::PIT_MAX_FREQUENCY / freq_divisor + 1
+        } else {
+            pit::PIT_MAX_FREQUENCY / freq_divisor
+        };
+
         // Allocate I/O ports.
         let ctrl: ReadWriteIoPort = ioports.allocate_read_write(pit::PIT_CTRL)?;
         let data: ReadWriteIoPort = ioports.allocate_read_write(pit::PIT_DATA)?;
@@ -56,17 +94,15 @@ impl Pit {
         pit.ctrl
             .write8(pit::PIT_SEL0 | pit::PIT_ACC_LOHI | pit::PIT_MODE_WAVE | pit::PIT_BINARY);
 
-        let freq_divisor: u32 = pit::PIT_MAX_FREQUENCY / freq;
-
         // Send data byte: divisor low and high bytes.
         pit.data.write8((freq_divisor & 0xff) as u8);
         pit.data.write8(((freq_divisor >> 8) & 0xff) as u8);
 
         // Set timer frequency.
-        TIMER_FREQUENCY.store(freq, Ordering::SeqCst);
+        TIMER_FREQUENCY.store(actual_freq, Ordering::SeqCst);
+
+        info!("pit set to {} Hz", actual_freq);
 
         Ok(pit)
     }
-}
-
 }
