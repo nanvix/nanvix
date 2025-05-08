3c7446d17dfd90f25520c70443cba2c6b1061289
Carlos Segarra <carlos@carlossegarra.com>
2025-05-08 17:23:49 +0000

    [build] E: Run python-lint
    Signed-off-by: Carlos Segarra <carlos@carlossegarra.com>

--- src/user/hello-python/__main__.py
+++ src/user/hello-python/__main__.py
@@ -1,9 +1,10 @@
 # Copyright(c) The Maintainers of Nanvix.
 # Licensed under the MIT License.
 
+
 def main():
     print("Hello, from Python!")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
