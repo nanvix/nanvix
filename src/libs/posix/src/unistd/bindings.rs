94d0da7ee71783caafeec9aa396a0124523d95f4
Pedro Henrique Penna <ppenna@microsoft.com>
2025-05-09 14:11:25 -0700

    [libs] E: seteuid()

--- src/libs/posix/src/unistd/bindings.rs
+++ src/libs/posix/src/unistd/bindings.rs
@@ -1616,6 +1616,46 @@ pub unsafe extern "C" fn setgid(gid: gid_t) -> c_int {
 ///
 /// # Description
 ///
+/// Sets the effective user ID of the calling process.
+///
+/// # Parameters
+///
+/// - `uid`: New user ID.
+///
+/// # Returns
+///
+/// Upon successful completion, `seteuid()` returns `0`. Otherwise, it returns `-1` and sets
+/// `errno` to indicate the error.
+///
+/// # Safety
+///
+/// This function is unsafe because it may modify global variables.
+///
+/// This function is safe to use if the following conditions are met:
+/// - This function is not called from multiple threads at the same time.
+///
+#[no_mangle]
+pub unsafe extern "C" fn seteuid(uid: uid_t) -> c_int {
+    ::nvx::error!("seteuid(): uid={:?}", uid);
+
+    // Check wether `uid` equals to the effective user ID of the calling process.
+    match unistd::geteuid() {
+        Ok(euid) if uid == euid => 0,
+        Ok(euid) => {
+            ::nvx::error!("seteuid(): operation not permitted (uid={:?}, euid={:?})", uid, euid);
+            *__errno_location() = ErrorCode::OperationNotPermitted.get();
+            -1
+        },
+        Err(error) => {
+            ::nvx::error!("seteuid(): failed (uid={:?}, error={:?})", uid, error);
+            -1
+        },
+    }
+}
+
+///
+/// # Description
+///
 /// Creates a symbolic link named `linkpath` which contains the string `target`.
 ///
 /// # Parameters
--- src/tests/misc-c/main.c
+++ src/tests/misc-c/main.c
@@ -40,6 +40,7 @@ int main(int argc, const char *argv[])
     test_getgid();
     test_geteuid();
     test_getegid();
+    test_seteuid();
     test_setgid();
     test_setegid();
     test_clock_getres();
