3d37fa401d18bcdc70da068ad541753a3809afd3
Pedro Henrique Penna <ppenna@microsoft.com>
2025-05-09 15:25:33 -0700

    [kernel] E: Move Identity Mngt out of the Kernel
    This commits moves identity management out of the kernel. And places it
    to in the Process Management Daemon. Note that the set/get uid/gid
    system call family is also provided by the Linux Daemon. In the long
    run, we should place it in the process daemon and have the POSIX layer
    to forward system calls to it.
    
    Finally, by moving this functionality out of the kernel, we need to
    devise another strategy  to verify if a process has the rights to change
    capabilities.

--- src/libs/proc/src/syscall/geteuid.rs
+++ src/libs/proc/src/syscall/geteuid.rs
@@ -2,23 +2,28 @@
 // Licensed under the MIT License.
 
 //==================================================================================================
-// Modules
+// Imports
 //==================================================================================================
 
-mod getegid;
-mod geteuid;
-mod getgid;
-mod getuid;
-mod lookup;
-mod signup;
+use ::nvx::{
+    pm::UserIdentifier,
+    sys::error::Error,
+};
 
 //==================================================================================================
-// Exports
+// Standalone Functions
 //==================================================================================================
 
-pub use getegid::getegid;
-pub use geteuid::geteuid;
-pub use getgid::getgid;
-pub use getuid::getuid;
-pub use lookup::lookup;
-pub use signup::signup;
+///
+/// # Description
+///
+/// Returns the effective user ID of the calling process.
+///
+/// # Returns
+///
+/// Upon successful completion, `geteuid()` returns the effective user ID of the calling process.
+/// Otherwise, it returns an error.
+///
+pub fn geteuid() -> Result<UserIdentifier, Error> {
+    unimplemented!("geteuid()");
+}
