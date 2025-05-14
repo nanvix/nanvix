c4ebc50c02ebd8c8942e37db3a0912336c8fe333
Pedro Henrique Penna <ppenna@microsoft.com>
2025-05-14 11:35:33 -0700

    [microvm] E: Use Socket

--- src/microvm/src/gateway.rs
+++ src/microvm/src/gateway.rs
@@ -10,23 +10,41 @@ use ::std::{
     io::{
         Error,
         ErrorKind,
-        Read,
-        Write,
     },
     mem,
-    os::unix::net::UnixStream,
 };
 use ::sys::ipc::Message;
+use ::syscomm::{
+    SocketError,
+    SocketStream,
+};
 
 //==================================================================================================
 // Structure
 //==================================================================================================
 
-pub enum Gateway {
-    UnixStream(UnixStream),
+pub struct Gateway {
+    stream: SocketStream,
 }
 
 impl Gateway {
+    ///
+    /// # Description
+    ///
+    /// Creates a new gateway.
+    ///
+    /// # Parameters
+    ///
+    /// - `stream`: Socket stream to be used by the gateway.
+    ///
+    /// # Returns
+    ///
+    /// A new gateway instance.
+    ///
+    pub fn new(stream: SocketStream) -> Self {
+        Gateway { stream }
+    }
+
     ///
     /// # Description
     ///
@@ -40,22 +58,18 @@ impl Gateway {
     ///
     /// If the message was successfully sent, `Ok(())` is returned. Otherwise, an error is returned.
     ///
-    pub fn try_send(&mut self, message: Message) -> Result<(), Error> {
-        match self {
-            Gateway::UnixStream(stream) => {
-                let bytes: [u8; mem::size_of::<Message>()] = message.to_bytes();
-                match stream.write_all(&bytes) {
-                    Ok(_) => Ok(()),
-                    Err(e) => {
-                        // Print error messages only if it is not a WouldBlock error to avoid spamming the logs.
-                        if e.kind() != ErrorKind::WouldBlock {
-                            let reason: String = format!("failed to send message ({:?}", e);
-                            error!("send(): {}", reason);
-                        }
-
-                        Err(e)
-                    },
+    pub fn try_send(&mut self, message: Message) -> Result<(), SocketError> {
+        let bytes: [u8; mem::size_of::<Message>()] = message.to_bytes();
+        match self.stream.write_all(&bytes) {
+            Ok(_) => Ok(()),
+            Err(e) => {
+                // Print error messages only if it is not a WouldBlock error to avoid spamming the logs.
+                if e.kind() != ErrorKind::WouldBlock {
+                    let reason: String = format!("failed to send message ({:?}", e);
+                    error!("send(): {}", reason);
                 }
+
+                Err(e)
             },
         }
     }
@@ -69,33 +83,29 @@ impl Gateway {
     ///
     /// Upon success, the received message is returned. Otherwise, an error is returned.
     ///
-    pub fn try_receive(&mut self) -> Result<Message, Error> {
-        match self {
-            Gateway::UnixStream(stream) => {
-                let mut bytes: [u8; mem::size_of::<Message>()] = [0; mem::size_of::<Message>()];
-                match stream.read_exact(&mut bytes) {
-                    Ok(_) => {
-                        let message: Message = match Message::try_from_bytes(bytes) {
-                            Ok(message) => message,
-                            Err(e) => {
-                                let reason: String = format!("failed to parse message ({:?})", e);
-                                error!("receive(): {}", reason);
-                                return Err(Error::new(ErrorKind::InvalidData, reason));
-                            },
-                        };
-
-                        Ok(message)
-                    },
+    pub fn try_receive(&mut self) -> Result<Message, SocketError> {
+        let mut bytes: [u8; mem::size_of::<Message>()] = [0; mem::size_of::<Message>()];
+        match self.stream.read_exact(&mut bytes) {
+            Ok(_) => {
+                let message: Message = match Message::try_from_bytes(bytes) {
+                    Ok(message) => message,
                     Err(e) => {
-                        // Print error messages only if it is not a WouldBlock error to avoid spamming the logs.
-                        if e.kind() != ErrorKind::WouldBlock {
-                            let reason: String = format!("failed to receive message ({:?})", e);
-                            error!("receive(): {}", reason);
-                        }
-
-                        Err(e)
+                        let reason: String = format!("failed to parse message ({:?})", e);
+                        error!("receive(): {}", reason);
+                        return Err(SocketError::new(Error::new(ErrorKind::InvalidData, reason)));
                     },
+                };
+
+                Ok(message)
+            },
+            Err(e) => {
+                // Print error messages only if it is not a WouldBlock error to avoid spamming the logs.
+                if e.kind() != ErrorKind::WouldBlock {
+                    let reason: String = format!("failed to receive message ({:?})", e);
+                    error!("receive(): {}", reason);
                 }
+
+                Err(SocketError::new(e))
             },
         }
     }
