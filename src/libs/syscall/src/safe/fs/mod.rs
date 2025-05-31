5f3b6d08539609bdae4de6d2bfc45d32151d81e9
Pedro Henrique Penna <ppenna@microsoft.com>
2025-05-31 15:07:03 -0700

    [libs] F: Safe Syscall Wrappers
    This commit introduces safe wrappers for the following system calls:
    
    FileSystem::change_current_directory => unistd::chdir()
    FileSystem::create_regular_file => unistd::creat()
    FileSystem::get_current_directory => unistd::getcwd()
    FileSystem::get_file_attributes => sys::stat::stat()
    FileSystem::open_regular_file => unistd::open()
    FileSystem::remove_file => unistd::unlink()
    RegularFile::allocate => fcntl::posix_allocate()
    RegularFile::attributes => sys::stat::fstat()
    RegularFile::advise => fcntl::posix_fadvise()
    RegularFile::read => unistd::read()
    RegularFile::seek => unistd::lseek()
    RegularFile::synchronize => unistd::sync()
    RegularFile::write => unistd::write()
    RegularFile::drop => unistd::close()
    StandardInput::read => unistd::read()
    StandardInput::is_terminal => unistd::isatty()
    StandardOutput::synchronize => unistd::isatty()
    StandardOutput::write => unistd::write()
    StandardOutput::is_terminal => unistd::isatty()
    StandardError::synchronize => unistd::sync()
    StandardError::write => unistd::write()
    StandardError::is_terminal => unistd::isatty()

--- src/libs/syscall/src/safe/fs/mod.rs
+++ src/libs/syscall/src/safe/fs/mod.rs
@@ -11,6 +11,25 @@ mod path;
 mod permissions;
 
 //==================================================================================================
+// Imports
+//==================================================================================================
+
+use crate::{
+    fcntl,
+    safe::{
+        RegularFile,
+        RegularFileOpenFlags,
+    },
+    sys::{
+        self,
+        types::mode_t,
+    },
+    unistd,
+};
+use ::alloc::string::String;
+use ::sys::error::Error;
+
+//==================================================================================================
 // Exports
 //==================================================================================================
 
@@ -18,3 +37,141 @@ pub use attributes::FileSystemAttributes;
 pub use fd::RawFileDescriptor;
 pub use path::FileSystemPath;
 pub use permissions::FileSystemPermissions;
+
+//==================================================================================================
+// File System
+//==================================================================================================
+
+pub struct FileSystem;
+
+impl FileSystem {
+    ///
+    /// # Description
+    ///
+    /// Changes the current working directory.
+    ///
+    /// # Parameters
+    ///
+    /// - `path`: The new working directory path.
+    ///
+    /// # Returns
+    ///
+    /// Upon successful completion, empty is returned. Otherwise, an error is returned instead.
+    ///
+    pub fn change_current_directory(path: &FileSystemPath) -> Result<(), Error> {
+        unistd::chdir(path.as_str())
+    }
+
+    ///
+    /// # Description
+    ///
+    /// Creates a new regular file in the file system.
+    ///
+    /// # Parameters
+    ///
+    /// - `filename`: The name of the file to be created.
+    /// - `permissions`: The permissions for the new file.
+    ///
+    /// # Returns
+    ///
+    /// Upon successful completion, a regular file is created and returned. Otherwise, an error
+    /// is returned instead.
+    ///
+    pub fn create_regular_file(
+        filename: &FileSystemPath,
+        permissions: Option<FileSystemPermissions>,
+    ) -> Result<RegularFile, Error> {
+        let mode: mode_t = match permissions {
+            Some(permissions) => permissions.into(),
+            None => 0,
+        };
+        let fd: RawFileDescriptor = fcntl::syscall::creat(filename.as_str(), mode)?;
+        Ok(RegularFile::new(fd))
+    }
+
+    ///
+    /// # Description
+    ///
+    /// Gets the current working directory.
+    ///
+    /// # Returns
+    ///
+    /// Upon successful completion, the current working directory is returned.  Otherwise, an error
+    /// is returned instead.
+    ///
+    pub fn get_current_directory() -> Result<FileSystemPath, Error> {
+        // Get the current working directory.
+        let path: String = unistd::getcwd()?;
+        FileSystemPath::new(&path)
+    }
+
+    ///
+    /// # Description
+    ///
+    /// Gets the attributes of a file.
+    ///
+    /// # Parameters
+    ///
+    /// - `filename`: The name of the file whose attributes are to be retrieved.
+    ///
+    /// # Returns
+    ///
+    /// Upon successful completion, the status information of the file is returned. Otherwise, an
+    /// error is returned instead.
+    ///
+    pub fn get_file_attributes(filename: &FileSystemPath) -> Result<FileSystemAttributes, Error> {
+        let mut st: sys::stat::stat = sys::stat::stat::default();
+        sys::stat::stat(filename.as_str(), &mut st)?;
+        Ok(FileSystemAttributes::from(st))
+    }
+
+    ///
+    /// # Description
+    ///
+    /// Opens a regular file in the file system.
+    ///
+    /// # Parameters
+    ///
+    /// - `pathname`: The path to the file.
+    /// - `flags`: The flags to open the file.
+    /// - `permissions`: File permissions when creating a new file.
+    ///
+    /// # Returns
+    ///
+    /// Upon successful completion, a `RegularFile` structure is returned. Otherwise, an error is
+    /// returned instead.
+    ///
+    pub fn open_regular_file(
+        pathname: &FileSystemPath,
+        flags: RegularFileOpenFlags,
+        permissions: Option<FileSystemPermissions>,
+    ) -> Result<RegularFile, Error> {
+        let mode: mode_t = match permissions {
+            Some(permissions) => permissions.into(),
+            None => 0,
+        };
+        let fd: RawFileDescriptor = fcntl::syscall::open(pathname.as_str(), flags.into(), mode)?;
+        Ok(RegularFile::new(fd))
+    }
+
+    ///
+    /// # Description
+    ///
+    /// Removes a file from the file system.
+    ///
+    /// # Parameters
+    ///
+    /// - `pathname`: The path to the file to be removed.
+    ///
+    /// # Returns
+    ///
+    /// Upon successful completion, empty is returned. Otherwise, an error is returned instead.
+    ///
+    pub fn remove_file(pathname: &FileSystemPath) -> Result<(), Error> {
+        // Unlink the file.
+        match unistd::unlink(pathname.as_str()) {
+            Ok(()) => Ok(()),
+            Err(error) => Err(error),
+        }
+    }
+}
