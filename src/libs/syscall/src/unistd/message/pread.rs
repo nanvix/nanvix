2ed78cb7be00ea86f2275970a54fb29876b327fa
Pedro Henrique Penna <ppenna@microsoft.com>
2025-06-19 17:29:09 -0700

    [libs] E: Dont Use ssize_t in IKC Messages
    This commit fixes IKC messages to use i32 instead of ssize_t to make
    them platform independent.

--- src/libs/syscall/src/unistd/message/pread.rs
+++ src/libs/syscall/src/unistd/message/pread.rs
@@ -79,15 +79,15 @@ impl PartialReadRequest {
 #[derive(Debug)]
 #[repr(C, packed)]
 pub struct PartialReadResponse {
-    pub count: ssize_t,
+    pub count: i32,
     pub buffer: [u8; Self::BUFFER_SIZE],
 }
 ::static_assert::assert_eq_size!(PartialReadResponse, LinuxDaemonMessage::PAYLOAD_SIZE);
 
 impl PartialReadResponse {
-    pub const BUFFER_SIZE: usize = LinuxDaemonMessage::PAYLOAD_SIZE - mem::size_of::<ssize_t>();
+    pub const BUFFER_SIZE: usize = LinuxDaemonMessage::PAYLOAD_SIZE - mem::size_of::<i32>();
 
-    fn new(count: ssize_t, buffer: [u8; Self::BUFFER_SIZE]) -> Self {
+    fn new(count: i32, buffer: [u8; Self::BUFFER_SIZE]) -> Self {
         Self { count, buffer }
     }
 
