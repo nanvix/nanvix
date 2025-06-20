79443693a065f55d9efb98e5246a1662e670424a
Pedro Henrique Penna <ppenna@microsoft.com>
2025-06-19 18:10:42 -0700

    [libs] E: Update iovec Structure
    In this commit I update the iovec structure to use size_t. This changes
    makes the structure architecture dependent, so I gated it behind a
    feature flag. Additionally, I added some derives that are required by
    libc.

--- src/libs/sysapi/src/sys_uio.rs
+++ src/libs/sysapi/src/sys_uio.rs
@@ -11,14 +11,18 @@
 // Imports
 //==================================================================================================
 
-use crate::sys_types::c_size_t;
+#[cfg(target_pointer_width = "32")]
+use crate::sys_types::size_t;
 
 //==================================================================================================
 
 /// An I/O vector.
+#[cfg(target_pointer_width = "32")]
+#[repr(C, packed)]
+#[derive(Debug, Clone, Copy)]
 pub struct iovec {
     /// Base address of a memory region for input or output.
     pub iov_base: *mut u8,
     /// The size of the memory pointer to by `iov_base`.
-    pub iov_len: c_size_t,
+    pub iov_len: size_t,
 }
