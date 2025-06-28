f020e35144f6aa010d9b5328ef69ce3729a369c7
Pedro Henrique Penna <ppenna@microsoft.com>
2025-06-28 08:55:46 -0700

    [build] E: Fix Formatting

--- scripts/build-openssl.sh
+++ scripts/build-openssl.sh
@@ -60,7 +60,7 @@ make_clean() {
 
 distclean() {
     cd ${OPENSSL_HOME}
-	git clean -fdx
+    git clean -fdx
 }
 
 #===================================================================================================
@@ -90,8 +90,8 @@ make_install() {
 build() {
     cd ${OPENSSL_HOME}
     configure
-	make_all
-	make_install
+    make_all
+    make_install
 }
 
 #===================================================================================================
@@ -142,18 +142,18 @@ unset LIBC
 unset LIBM
 
 case $RULE in
-	build)
-		build
-		;;
-	clean)
-		make_clean
-		;;
-	distclean)
-		distclean
-		;;
-	init)
-		init
-		;;
+    build)
+        build
+        ;;
+    clean)
+        make_clean
+        ;;
+    distclean)
+        distclean
+        ;;
+    init)
+        init
+        ;;
 esac
 
 # Restore original environment variables.
