b9b3403e4aeec7850fe41d5dd5f3d06d4a375f0a
Pedro Henrique Penna <ppenna@microsoft.com>
2025-07-02 08:42:03 -0700

    [libs] F: Dummy Impl for posix_memalign()
    This commit adds a dummy implementation for posix_memalign() using
    malloc().

---
+++ src/libs/posix/src/stdlib/mod.rs
@@ -0,0 +1,31 @@
+// Copyright(c) The Maintainers of Nanvix.
+// Licensed under the MIT License.
+
+//==================================================================================================
+// Lint Configuration
+//==================================================================================================
+
+#![forbid(
+    clippy::cast_possible_truncation,
+    clippy::cast_possible_wrap,
+    clippy::cast_precision_loss,
+    clippy::cast_sign_loss,
+    clippy::char_lit_as_u8,
+    clippy::fn_to_numeric_cast,
+    clippy::fn_to_numeric_cast_with_truncation,
+    clippy::ptr_as_ptr,
+    clippy::unnecessary_cast,
+    invalid_reference_casting
+)]
+
+//==================================================================================================
+// Modules
+//==================================================================================================
+
+mod posix_memalign;
+
+//==================================================================================================
+// Exports
+//==================================================================================================
+
+pub use self::posix_memalign::posix_memalign;
