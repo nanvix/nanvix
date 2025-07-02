75f2fa11efcececb8856c86f86cf5def1c413261
Pedro Henrique Penna <ppenna@microsoft.com>
2025-07-02 09:23:24 -0700

    [libs] E: Move Binding for open()
    This commits moves the binding for the open() system call to the syscall
    crate.

--- src/libs/syscall/src/fcntl/bindings/mod.rs
+++ src/libs/syscall/src/fcntl/bindings/mod.rs
@@ -2,7 +2,25 @@
 // Licensed under the MIT License.
 
 //==================================================================================================
+// Lint Configuration
+//==================================================================================================
+
+#![forbid(
+    clippy::cast_possible_truncation,
+    clippy::cast_possible_wrap,
+    clippy::cast_precision_loss,
+    clippy::cast_sign_loss,
+    clippy::char_lit_as_u8,
+    clippy::fn_to_numeric_cast,
+    clippy::fn_to_numeric_cast_with_truncation,
+    clippy::ptr_as_ptr,
+    clippy::unnecessary_cast,
+    invalid_reference_casting
+)]
+
+//==================================================================================================
 // Modules
 //==================================================================================================
 
 pub mod fcntl;
+pub mod open;
