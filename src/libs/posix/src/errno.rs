1e19aeae02b67db1e42f173fd65236f25ce18c26
Pedro Henrique Penna <ppenna@microsoft.com>
2025-07-02 13:13:27 -0700

    [libs] B: Define __errno_location in syscall crate
    This commits moves definition of the __errno_location function to the
    syscall crate. This fixes the bug of having two definitions for this
    function.

--- src/libs/posix/src/errno.rs
+++ src/libs/posix/src/errno.rs
@@ -5,31 +5,4 @@
 // Imports
 //==================================================================================================
 
-cfg_if::cfg_if! {
-    if #[cfg(all(feature = "syscall", feature = "staticlib"))] {
-        use ::sysapi::ffi::c_int;
-
-        unsafe extern "C" {
-            pub fn __errno() -> *mut c_int;
-        }
-
-        ///
-        /// # Description
-        ///
-        /// Returns a pointer to `errno` variable.
-        ///
-        /// # Returns
-        ///
-        /// A mutable pointer to the `errno` variable.
-        ///
-        /// # Safety
-        ///
-        /// This function is unsafe because it may interoperate with external code.
-        ///
-        pub unsafe fn  __errno_location() -> *mut c_int {
-            __errno()
-        }
-    } else {
-        pub use ::syscall::errno::__errno_location;
-    }
-}
+pub use ::syscall::errno::__errno_location;
--- src/libs/syscall/src/errno.rs
+++ src/libs/syscall/src/errno.rs
@@ -11,22 +11,47 @@ use ::sysapi::ffi::c_int;
 // Global Variables
 //==================================================================================================
 
-#[allow(non_upper_case_globals)]
-static mut errno: c_int = 0;
+cfg_if::cfg_if! {
+    if #[cfg(all(feature = "syscall", feature = "staticlib"))] {
+        unsafe extern "C" {
+            pub fn __errno() -> *mut c_int;
+        }
 
-///
-/// # Description
-///
-/// Returns a pointer to `errno` variable.
-///
-/// # Returns
-///
-/// A mutable pointer to the `errno` variable.
-///
-/// # Safety
-///
-/// This function is unsafe because it may interoperate with external code.
-///
-pub unsafe fn __errno_location() -> *mut c_int {
-    &raw mut errno as *mut c_int
+        ///
+        /// # Description
+        ///
+        /// Returns a pointer to `errno` variable.
+        ///
+        /// # Returns
+        ///
+        /// A mutable pointer to the `errno` variable.
+        ///
+        /// # Safety
+        ///
+        /// This function is unsafe because it may interoperate with external code.
+        ///
+        pub unsafe fn  __errno_location() -> *mut c_int {
+            __errno()
+        }
+    } else {
+        #[allow(non_upper_case_globals)]
+        static mut errno: c_int = 0;
+
+        ///
+        /// # Description
+        ///
+        /// Returns a pointer to `errno` variable.
+        ///
+        /// # Returns
+        ///
+        /// A mutable pointer to the `errno` variable.
+        ///
+        /// # Safety
+        ///
+        /// This function is unsafe because it may interoperate with external code.
+        ///
+        pub unsafe fn __errno_location() -> *mut c_int {
+            &raw mut errno as *mut c_int
+        }
+    }
 }
