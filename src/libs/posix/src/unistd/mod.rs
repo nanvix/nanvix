b7fd62f18535f8d6df9d60d9ecf50eac21354f82
Jiaxiao (mossaka) Zhou <duibao55328@gmail.com>
2025-07-03 01:13:18 +0000

    [libs] E: Move Binding for write()
    This commits moves the binding for the write() system calls to the
    syscall crate
    
    Signed-off-by: Jiaxiao (mossaka) Zhou <duibao55328@gmail.com>

--- src/libs/posix/src/unistd/mod.rs
+++ src/libs/posix/src/unistd/mod.rs
@@ -40,11 +40,7 @@ use ::sysapi::{
         pid_t,
         uid_t,
     },
-    unistd::{
-        STDERR_FILENO,
-        STDIN_FILENO,
-        STDOUT_FILENO,
-    },
+    unistd::STDIN_FILENO,
 };
 use ::syscall::unistd::syscall;
 
@@ -1926,44 +1922,4 @@ pub unsafe extern "C" fn waitpid(pid: pid_t, status: *mut c_int, options: c_int)
         *__errno_location() = ErrorCode::InvalidSysCall.get();
     }
     -1
-}
-
-///
-/// # Safety
-///
-/// The function has undefined behavior if the `buffer` points to an invalid memory location.
-///
-#[unsafe(no_mangle)]
-pub unsafe extern "C" fn write(fd: c_int, buffer: *const c_void, count: c_size_t) -> c_ssize_t {
-    // Skip logging for stdout and stderr to avoid spamming the output.
-    if fd != STDOUT_FILENO && fd != STDERR_FILENO {
-        ::syslog::trace!("write(): fd = {}, buffer = {:?}, count = {}", fd, buffer, count);
-    }
-
-    // Check if buffer is invalid.
-    if buffer.is_null() {
-        ::syslog::error!("write(): invalid buffer");
-        *__errno_location() = ErrorCode::InvalidArgument.get();
-        return -1;
-    }
-
-    // Check if count is invalid.
-    if count == 0 {
-        ::syslog::error!("write(): invalid write count");
-        *__errno_location() = ErrorCode::InvalidArgument.get();
-        return -1;
-    }
-
-    // Construct buffer from raw parts.
-    let buffer: &[u8] = slice::from_raw_parts(buffer as *const u8, count as usize);
-
-    // Attempt to write to file descriptor and check for errors.
-    match ::syscall::unistd::write(fd, buffer) {
-        Ok(bytes_written) => bytes_written as c_ssize_t,
-        Err(error) => {
-            ::syslog::error!("write(): failed (error={:?})", error);
-            *__errno_location() = error.code.get();
-            -1
-        },
-    }
 }
