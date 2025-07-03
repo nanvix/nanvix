55a7a4d49a4dfe8bcd8257a3f4f5a164e71e4461
Jiaxiao (mossaka) Zhou <duibao55328@gmail.com>
2025-07-03 20:20:13 +0000

    [libs] B: Missing Binding for __errno_location()
    This commits adds the binding for the __errno_location() system calls to
    the syscall crate
    
    Signed-off-by: Jiaxiao (mossaka) Zhou <duibao55328@gmail.com>

--- src/libs/syscall/src/errno.rs
+++ src/libs/syscall/src/errno.rs
@@ -2,17 +2,13 @@
 // Licensed under the MIT License.
 
 //==================================================================================================
-// Imports
-//==================================================================================================
-
-use ::sysapi::ffi::c_int;
-
-//==================================================================================================
 // Global Variables
 //==================================================================================================
 
 cfg_if::cfg_if! {
     if #[cfg(all(feature = "syscall", feature = "staticlib"))] {
+        use ::sysapi::ffi::c_int;
+
         unsafe extern "C" {
             pub fn __errno() -> *mut c_int;
         }
@@ -30,10 +26,13 @@ cfg_if::cfg_if! {
         ///
         /// This function is unsafe because it may interoperate with external code.
         ///
-        pub unsafe fn  __errno_location() -> *mut c_int {
+        #[unsafe(no_mangle)]
+        pub unsafe extern "C" fn __errno_location() -> *mut c_int {
             __errno()
         }
-    } else {
+    } else if #[cfg(feature = "syscall")] {
+        use ::sysapi::ffi::c_int;
+
         #[allow(non_upper_case_globals)]
         static mut errno: c_int = 0;
 
@@ -50,7 +49,8 @@ cfg_if::cfg_if! {
         ///
         /// This function is unsafe because it may interoperate with external code.
         ///
-        pub unsafe fn __errno_location() -> *mut c_int {
+        #[unsafe(no_mangle)]
+        pub unsafe extern "C" fn __errno_location() -> *mut c_int {
             &raw mut errno as *mut c_int
         }
     }
