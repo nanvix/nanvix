f92d089735d22f59e75bcfad07157aad023c6692
Pedro Henrique Penna <ppenna@microsoft.com>
2025-07-13 09:19:40 -0700

    [libs] E: Move Binding for lseek()
    This commits moves the binding for the lseek() system calls to the
    syscall crate

--- src/libs/posix/src/unistd/mod.rs
+++ src/libs/posix/src/unistd/mod.rs
@@ -40,52 +40,6 @@ use ::syscall::unistd::syscall;
 ///
 /// # Description
 ///
-/// Sets the file offset of a file descriptor.
-///
-/// # Parameters
-///
-/// - `fd`: File descriptor.
-/// - `offset`: Offset to set.
-/// - `whence`: Reference point for the offset.
-///
-/// # Returns
-///
-/// Upon successful completion, `lseek()` returns the resulting offset. Otherwise, it returns
-/// `-1` and sets `errno` to indicate the error.
-///
-/// # Safety
-///
-/// The function is unsafe because it may access global variables.
-///
-/// It is safe to use this function if the following conditions are met:
-/// - This function is not called from multiple threads at the same time.
-///
-#[unsafe(no_mangle)]
-pub extern "C" fn lseek(fd: c_int, offset: off_t, whence: c_int) -> off_t {
-    ::syslog::trace!("lseek(): fd={:?}, offset={:?}, whence={:?}", fd, offset, whence);
-
-    // Attempt to seek the file descriptor and check for errors.
-    match ::syscall::unistd::lseek(fd, offset, whence) {
-        Ok(offset) => offset,
-        Err(error) => {
-            ::syslog::error!(
-                "lseek(): failed (fd={:?}, offset={:?}, whence={:?}, error={:?})",
-                fd,
-                offset,
-                whence,
-                error
-            );
-            unsafe {
-                *__errno_location() = error.code.get();
-            }
-            -1
-        },
-    }
-}
-
-///
-/// # Description
-///
 /// Reads data from a file descriptor.
 ///
 /// # Parameters
