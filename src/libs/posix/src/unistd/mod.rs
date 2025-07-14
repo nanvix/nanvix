ccf8fe972dabb3fc8349064da1673b9c5017ac11
Pedro Henrique Penna <ppenna@microsoft.com>
2025-07-14 06:52:45 -0700

    [libs] E: Move Binding for seteuid()
    This commits moves the binding for the seteuid() system calls to the
    syscall crate

--- src/libs/posix/src/unistd/mod.rs
+++ src/libs/posix/src/unistd/mod.rs
@@ -29,46 +29,6 @@ use ::syscall::unistd::syscall;
 ///
 /// # Description
 ///
-/// Sets the effective user ID of the calling process.
-///
-/// # Parameters
-///
-/// - `uid`: New user ID.
-///
-/// # Returns
-///
-/// Upon successful completion, `seteuid()` returns `0`. Otherwise, it returns `-1` and sets
-/// `errno` to indicate the error.
-///
-/// # Safety
-///
-/// This function is unsafe because it may modify global variables.
-///
-/// This function is safe to use if the following conditions are met:
-/// - This function is not called from multiple threads at the same time.
-///
-#[unsafe(no_mangle)]
-pub unsafe extern "C" fn seteuid(uid: uid_t) -> c_int {
-    ::syslog::error!("seteuid(): uid={:?}", uid);
-
-    // Check wether `uid` equals to the effective user ID of the calling process.
-    match syscall::geteuid() {
-        Ok(euid) if uid == euid => 0,
-        Ok(euid) => {
-            ::syslog::error!("seteuid(): operation not permitted (uid={:?}, euid={:?})", uid, euid);
-            *__errno_location() = ErrorCode::OperationNotPermitted.get();
-            -1
-        },
-        Err(error) => {
-            ::syslog::error!("seteuid(): failed (uid={:?}, error={:?})", uid, error);
-            -1
-        },
-    }
-}
-
-///
-/// # Description
-///
 /// Sets the real user ID of the calling process.
 ///
 /// # Parameters
