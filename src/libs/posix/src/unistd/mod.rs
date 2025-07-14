8fbb858e045a79d9a7f34eaf14556396ee3a1bf6
Pedro Henrique Penna <ppenna@microsoft.com>
2025-07-14 06:44:11 -0700

    [libs] E: Move Binding for sbrk()
    This commits moves the binding for the sbrk() system calls to the
    syscall crate

--- src/libs/posix/src/unistd/mod.rs
+++ src/libs/posix/src/unistd/mod.rs
@@ -28,40 +28,6 @@ use ::syscall::unistd::syscall;
 // Standalone Functions
 //==================================================================================================
 
-///
-/// # Description
-///
-/// Increments the program break.
-///
-/// # Parameters
-///
-/// - `size`: Number of bytes to increment the program break.
-///
-/// # Returns
-///
-/// Upon successful completion, the `sbrk()` function returns the address of the start of the newly
-/// allocated memory. Otherwise, it returns `(void *) -1` and sets `errno` to indicate the error.
-///
-/// # See Also
-///
-/// - [`crate::unistd::syscall::sbrk()`]
-///
-#[unsafe(no_mangle)]
-pub extern "C" fn sbrk(size: isize) -> *mut u8 {
-    match ::syscall::unistd::sbrk(size) {
-        // Succeeded to increment the program break.
-        Ok(ptr) => ptr,
-        // Failed to increment the program break.
-        Err(e) => {
-            // Set errno.
-            unsafe {
-                *__errno_location() = e.code.get();
-            }
-            (-1_isize) as *mut u8
-        },
-    }
-}
-
 #[unsafe(no_mangle)]
 pub extern "C" fn sleep(_seconds: c_uint) -> c_uint {
     // TODO: https://github.com/nanvix/nanvix/issues/453
