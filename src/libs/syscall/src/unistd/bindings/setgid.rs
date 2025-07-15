e928a2e78b0f0f97c341e15ad93f91dc0d20ecc5
Pedro Henrique Penna <ppenna@microsoft.com>
2025-07-15 09:29:53 -0700

    [libs] E: Move Binding for setgid()
    This commits moves the binding for the setgid() system calls to the
    syscall crate

---
+++ src/libs/syscall/src/unistd/bindings/setgid.rs
@@ -0,0 +1,61 @@
+// Copyright(c) The Maintainers of Nanvix.
+// Licensed under the MIT License.
+
+//==================================================================================================
+// Imports
+//==================================================================================================
+
+use crate::{
+    errno::__errno_location,
+    unistd,
+};
+use ::sys::error::ErrorCode;
+use ::sysapi::{
+    ffi::c_int,
+    sys_types::gid_t,
+};
+
+//==================================================================================================
+// Standalone Functions
+//==================================================================================================
+
+///
+/// # Description
+///
+/// Sets the real group ID of the calling process.
+///
+/// # Parameters
+///
+/// - `gid`: New group ID.
+///
+/// # Returns
+///
+/// Upon successful completion, `setgid()` returns `0`. Otherwise, it returns `-1` and sets
+/// `errno` to indicate the error.
+///
+/// # Safety
+///
+/// This function is unsafe because it may modify global variables.
+///
+/// This function is safe to use if the following conditions are met:
+/// - This function is not called from multiple threads at the same time.
+///
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn setgid(gid: gid_t) -> c_int {
+    ::syslog::error!("setgid(): gid={gid:?}");
+
+    // Check wether `gid` equals to the real group ID of the calling process.
+    match unistd::getgid() {
+        Ok(rgid) if gid == rgid => 0,
+        Ok(rgid) => {
+            ::syslog::error!("setgid(): operation not permitted (gid={gid:?}, rgid={rgid:?})");
+            *__errno_location() = ErrorCode::OperationNotPermitted.get();
+            -1
+        },
+        Err(error) => {
+            ::syslog::error!("setgid(): {error:?} (gid={gid:?})");
+            *__errno_location() = error.code.get();
+            -1
+        },
+    }
+}
