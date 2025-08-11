32f8179926590182eb9fe8486789903e9c826d41
Carlos Segarra <carlos@carlossegarra.com>
2025-08-11 21:54:39 +0000

    [daemons] F: Introduce User VM Handle
    Signed-off-by: Carlos Segarra <carlos@carlossegarra.com>

---
+++ src/daemons/linuxd/src/user_vm_handle.rs
@@ -0,0 +1,54 @@
+// Copyright(c) The Maintainers of Nanvix.
+// Licensed under the MIT License.
+
+//==================================================================================================
+// Imports
+//==================================================================================================
+
+use ::std::sync::{
+    Arc,
+    Mutex,
+};
+use ::syscomm::SocketStream;
+
+//==================================================================================================
+// Structures
+//==================================================================================================
+
+/// State associated with a user VM connected to this linuxd instance.
+#[derive(Clone)]
+pub struct UserVmHandle {
+    // FIXME: this field is relevant once we start managing more than one user VMs.
+    #[allow(dead_code)]
+    conn_id: usize,
+    user_vm_stream: Arc<Mutex<SocketStream>>,
+    gw_stream: Option<Arc<Mutex<SocketStream>>>,
+}
+
+impl UserVmHandle {
+    pub fn new(
+        conn_id: usize,
+        user_vm_stream: SocketStream,
+        gw_stream: Option<SocketStream>,
+    ) -> Self {
+        Self {
+            conn_id,
+            user_vm_stream: Arc::new(Mutex::new(user_vm_stream)),
+            gw_stream: gw_stream.map(|stream| Arc::new(Mutex::new(stream))),
+        }
+    }
+
+    // FIXME: this field is relevant once we start managing more than one user VMs.
+    #[allow(dead_code)]
+    pub fn get_conn_id(&self) -> usize {
+        self.conn_id
+    }
+
+    pub fn get_user_vm_stream(&self) -> Arc<Mutex<SocketStream>> {
+        self.user_vm_stream.clone()
+    }
+
+    pub fn get_gw_vm_stream(&self) -> Option<Arc<Mutex<SocketStream>>> {
+        self.gw_stream.clone()
+    }
+}
