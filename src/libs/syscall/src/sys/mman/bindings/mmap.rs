a2f3dd49b270745ad61d342f6c97ea34295e9322
Pedro Henrique Penna <ppenna@microsoft.com>
2025-08-13 16:00:16 -0700

    [libs] E: Bindings for mmap and munmap Syscalls

--- src/libs/syscall/src/sys/mman/bindings/mmap.rs
+++ src/libs/syscall/src/sys/mman/bindings/mmap.rs
@@ -5,31 +5,218 @@
 // Imports
 //==================================================================================================
 
-use ::sys::error::ErrorCode;
-use ::sysapi::errno::__errno_location;
+use crate::{
+    errno::__errno_location,
+    sys::mman::{
+        self,
+        MemoryMapFlags,
+        MemoryMapProtectionFlags,
+    },
+};
+use ::sys::{
+    error::ErrorCode,
+    mm::Address,
+};
+use ::sysapi::{
+    ffi::c_int,
+    sys_mman::MAP_FAILED,
+    sys_types::{
+        c_size_t,
+        off_t,
+    },
+};
 
 //==================================================================================================
 // Standalone Functions
 //==================================================================================================
 
-#[allow(clippy::missing_safety_doc)]
+///
+/// # Description
+///
+/// Maps files or devices into memory. The `mmap()` function creates a new mapping in the virtual
+/// address space of the calling process. The starting address for the new mapping is selected by
+/// the system unless the `MAP_FIXED` flag is used.  The mapping can control page protections via
+/// `prot` and mapping behavior via `flags`.
+///
+/// # Parameters
+///
+/// - `addr`: Address hint for the mapping. If `NULL`, the system chooses the address.
+/// - `length`: Length in bytes of the mapping; must be greater than zero.
+/// - `prot`: Desired memory protection of the mapping; bitwise OR of flags such as `PROT_NONE`,
+///   `PROT_READ`, `PROT_WRITE`, and `PROT_EXEC`.
+/// - `flags`: Determines the type of mapping; typically includes one of `MAP_PRIVATE` or
+///   `MAP_SHARED` and may include `MAP_FIXED` and `MAP_ANONYMOUS`.
+/// - `fd`: File descriptor to map.
+/// - `offset`: Offset within the file. POSIX requires it to be a multiple of the page size for
+///   file-backed mappings.
+///
+/// # Returns
+///
+/// On success, returns a pointer to the mapped area. On failure, returns `MAP_FAILED` and sets
+/// `errno` to indicate the error.
+///
+/// # Safety
+///
+/// This function is unsafe because it may modify global state.
+///
+/// It is safe to call this function if and only if the following conditions are met:
+/// - Access to `errno` is synchronized with other threads that may modify it.
+///
+/// # Known Limitations (Nanvix)
+///
+/// - Only anonymous, private mappings are supported. File-backed mappings are not implemented.
+/// - `MAP_SHARED` is not supported.
+/// - `MAP_FIXED` is not supported.
+/// - The `addr` argument is always ignored.
+/// - The `offset` parameter is always ignored.
+/// - For anonymous mappings, `fd` must be `-1`.
+/// - Any flags other than `MAP_PRIVATE | MAP_ANONYMOUS` are not supported.
+///
 #[unsafe(no_mangle)]
 pub unsafe extern "C" fn mmap(
     addr: *mut u8,
-    length: usize,
-    prot: i32,
-    flags: i32,
-    _fd: i32,
-    offset: isize,
+    length: c_size_t,
+    prot: c_int,
+    flags: c_int,
+    fd: c_int,
+    offset: off_t,
 ) -> *mut u8 {
     ::syslog::trace!(
-        "mmap(): addr={addr:?}, length={length}, prot={prot}, flags={flags}, fd={_fd}, \
+        "mmap(): addr={addr:?}, length={length}, prot={prot}, flags={flags}, fd={fd}, \
          offset={offset}"
     );
 
-    ::syslog::error!("mmap(): not implemented");
-    unsafe {
-        *__errno_location() = ErrorCode::InvalidSysCall.get();
+    // Check if mapping length is invalid.
+    if length == 0 {
+        ::syslog::error!(
+            "mmap(): invalid mapping length (addr={addr:?}, length={length}), prot={prot}, \
+             flags={flags}, fd={fd}, offset={offset})"
+        );
+        unsafe {
+            *__errno_location() = ErrorCode::InvalidArgument.get();
+        }
+        return MAP_FAILED;
     }
-    core::ptr::null_mut()
+
+    // Attempt to convert length.
+    let length: usize = match TryFrom::try_from(length) {
+        Ok(length) => length,
+        Err(error) => {
+            ::syslog::error!(
+                "mmap(): invalid mapping length (addr={addr:?}, length={length}), prot={prot}, \
+                 flags={flags}, fd={fd}, offset={offset}), error={error:?}"
+            );
+            unsafe {
+                *__errno_location() = ErrorCode::InvalidArgument.get();
+            }
+            return MAP_FAILED;
+        },
+    };
+
+    // Check if address hint was provided.
+    if !addr.is_null() {
+        ::syslog::warn!(
+            "mmap(): address hint is not supported and will be ignored (addr={addr:?}, \
+             length={length}, prot={prot}, flags={flags}, fd={fd}, offset={offset})"
+        );
+    }
+
+    // Attempt to convert flags.
+    let flags: MemoryMapFlags = match TryFrom::try_from(flags) {
+        Ok(flags) => flags,
+        Err(error) => {
+            ::syslog::error!(
+                "mmap(): invalid flags (addr={addr:?}, length={length}, prot={prot}, \
+                 flags={flags}, fd={fd}, offset={offset}), error={error:?}"
+            );
+            unsafe {
+                *__errno_location() = ErrorCode::InvalidArgument.get();
+            }
+            return MAP_FAILED;
+        },
+    };
+
+    // Check for unsupported flags.
+    if flags.is_shared() {
+        ::syslog::error!(
+            "mmap(): shared memory mapping is not supported (addr={addr:?}, length={length}, \
+             prot={prot}, flags={flags:?}, fd={fd}, offset={offset})"
+        );
+        unsafe {
+            *__errno_location() = ErrorCode::InvalidArgument.get();
+        }
+        return MAP_FAILED;
+    } else if flags.is_fixed() {
+        ::syslog::error!(
+            "mmap(): fixed memory mapping is not supported (addr={addr:?}, length={length}, \
+             prot={prot}, flags={flags:?}, fd={fd}, offset={offset})"
+        );
+        unsafe {
+            *__errno_location() = ErrorCode::InvalidArgument.get();
+        }
+        return MAP_FAILED;
+    } else if !flags.is_anonymous() {
+        ::syslog::error!(
+            "mmap(): non-anonymous memory mapping is not supported (addr={addr:?}, \
+             length={length}, prot={prot}, flags={flags:?}, fd={fd}, offset={offset})"
+        );
+        unsafe {
+            *__errno_location() = ErrorCode::InvalidArgument.get();
+        }
+        return MAP_FAILED;
+    } else if flags.is_anonymous() && fd != -1 {
+        ::syslog::error!(
+            "mmap(): anonymous memory mapping with file descriptor is not supported \
+             (addr={addr:?}, length={length}, prot={prot}, flags={flags:?}, fd={fd}, \
+             offset={offset})"
+        );
+        unsafe {
+            *__errno_location() = ErrorCode::InvalidArgument.get();
+        }
+        return MAP_FAILED;
+    }
+
+    // Check for undefined behavior.
+    if flags.is_anonymous() && fd == -1 && offset != 0 {
+        ::syslog::warn!(
+            "mmap(): offset is ignored for anonymous memory mapping (addr={addr:?}, \
+             length={length}, prot={prot}, flags={flags:?}, fd={fd}, offset={offset})"
+        );
+    }
+
+    // Attempt to convert protection flags.
+    let prot: MemoryMapProtectionFlags = match TryFrom::try_from(prot) {
+        Ok(prot) => prot,
+        Err(error) => {
+            ::syslog::error!(
+                "mmap(): invalid protection flags (addr={addr:?}, length={length}, prot={prot}, \
+                 flags={flags:?}, fd={fd}, offset={offset}), error={error:?}"
+            );
+            unsafe {
+                *__errno_location() = ErrorCode::InvalidArgument.get();
+            }
+            return MAP_FAILED;
+        },
+    };
+
+    // Map memory region and check for errors.
+    match mman::mmap(length, prot) {
+        Ok(virt_addr) => {
+            ::syslog::trace!(
+                "mmap(): success (addr={addr:?}, length={length}, prot={prot:?}, flags={flags:?}, \
+                 fd={fd}, offset={offset}), virt_addr={virt_addr:?}"
+            );
+            virt_addr.into_raw_value() as *mut u8
+        },
+        Err(error) => {
+            ::syslog::error!(
+                "mmap(): failed (addr={addr:?}, length={length}, prot={prot:?}, flags={flags:?}, \
+                 fd={fd}, offset={offset}), error={error:?}"
+            );
+            unsafe {
+                *__errno_location() = error.code.get();
+            }
+            MAP_FAILED
+        },
+    }
 }
--- src/libs/syscall/src/sys/mman/bindings/munmap.rs
+++ src/libs/syscall/src/sys/mman/bindings/munmap.rs
@@ -5,21 +5,101 @@
 // Imports
 //==================================================================================================
 
-use ::sys::error::ErrorCode;
-use ::sysapi::errno::__errno_location;
+use crate::sys::mman;
+use ::sys::{
+    error::ErrorCode,
+    mm::VirtualAddress,
+};
+use ::sysapi::{
+    errno::__errno_location,
+    ffi::c_void,
+    sys_types::c_size_t,
+};
 
 //==================================================================================================
 // Standalone Functions
 //==================================================================================================
 
-#[allow(clippy::missing_safety_doc)]
+///
+/// # Description
+///
+/// Unmaps pages of memory. The `munmap()` function removes any mappings for the entire pages
+/// covering the address range that starts at `addr` and spans `length` bytes. After a successful
+/// call, further references to those pages shall result in a fault.
+///
+/// # Parameters
+///
+/// - `addr`: Start address of the region to unmap.
+/// - `length`: Number of bytes to unmap; must be greater than zero.
+///
+/// # Returns
+///
+/// On success, returns `0`. On failure, returns `-1` and sets `errno` to indicate the error.
+///
+/// # Safety
+///
+/// This function is unsafe because it may modify global state.
+///
+/// It is safe to call this function if and only if the following conditions are met:
+/// - Access to `errno` is synchronized with other threads that may modify it.
+///
+/// # Known Limitations (Nanvix)
+///
+/// - The `addr` must match the base address previously returned by `mmap()`.
+/// - Partial unmapping is not supported.
+///
 #[unsafe(no_mangle)]
-pub unsafe extern "C" fn munmap(addr: *mut u8, length: usize) -> isize {
+pub unsafe extern "C" fn munmap(addr: *mut c_void, length: c_size_t) -> isize {
     ::syslog::trace!("munmap(): addr={addr:?}, length={length}");
 
-    ::syslog::error!("munmap(): not implemented");
-    unsafe {
-        *__errno_location() = ErrorCode::InvalidSysCall.get();
+    // Check if address is invalid.
+    if addr.is_null() {
+        ::syslog::error!("munmap(): invalid base address (addr={addr:?}, length={length})");
+        unsafe {
+            *__errno_location() = ErrorCode::InvalidArgument.get();
+        }
+        return -1;
     }
-    -1
+
+    // Check if mapping length is invalid.
+    if length == 0 {
+        ::syslog::error!("munmap(): invalid mapping length (addr={addr:?}, length={length})");
+        unsafe {
+            *__errno_location() = ErrorCode::InvalidArgument.get();
+        }
+        return -1;
+    }
+
+    // Attempt to convert length.
+    let length: usize = match TryFrom::try_from(length) {
+        Ok(length) => length,
+        Err(error) => {
+            ::syslog::error!(
+                "munmap(): invalid mapping length (addr={addr:?}, length={length}), \
+                 error={error:?}"
+            );
+            unsafe {
+                *__errno_location() = ErrorCode::InvalidArgument.get();
+            }
+            return -1;
+        },
+    };
+
+    // Convert base pointer to virtual address.
+    let base: VirtualAddress = VirtualAddress::from_raw_value(addr as usize);
+
+    // Unmap memory segment and check for errors.
+    match mman::munmap(base, length) {
+        Ok(()) => {
+            ::syslog::trace!("munmap(): success (addr={addr:?}, length={length})");
+            0
+        },
+        Err(error) => {
+            ::syslog::error!("munmap(): failed (addr={addr:?}, length={length}), error={error:?}");
+            unsafe {
+                *__errno_location() = error.code.get();
+            }
+            -1
+        },
+    }
 }
