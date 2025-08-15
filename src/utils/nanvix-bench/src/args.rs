c6b70c3cc2b558fe9bc1a67be4c3b5d4573a2b8b
Carlos Segarra <carlos@carlossegarra.com>
2025-08-15 16:23:52 +0000

    [utils] E: Add -tmp-dir To Nanvix-Bench For Nanvixd
    For a while now nanvixd supports a CLI arg, `-tmp-dir`, to provide a
    temporary directory where to store all the used socket files, and other
    execution artifacts. These should not leak across executions, but it is
    sometimes hard to know if a leak is because of a current bug, or
    something that happened in the past.
    
    As a consequence, in this commit I surface this argument all the way to
    nanvixd-bench, such that we can set it there, too.
    
    Signed-off-by: Carlos Segarra <carlos@carlossegarra.com>

--- src/utils/nanvix-bench/src/args.rs
+++ src/utils/nanvix-bench/src/args.rs
@@ -21,6 +21,7 @@ pub struct Args {
     benchmark: BenchmarkFlavour,
     hwloc_file: Option<String>,
     iterations: usize,
+    tmp_dir: String,
 }
 
 //==================================================================================================
@@ -32,15 +33,17 @@ impl Args {
     const OPT_BENCHMARK: &'static str = "-benchmark";
     const OPT_HWLOC: &'static str = "-hwloc";
     const OPT_ITERATIONS: &'static str = "-iterations";
+    const OPT_TMP_DIR: &'static str = "-tmp-dir";
 
     fn usage() -> String {
         format!(
             "usage: ./bin/nanvix-bench.elf {} \
              [boot-time,cold-start,warm-start,warm-start-vmm,echo-breakdown] [{} \
-             <path_to_hwloc.json> {} <iterations>]",
+             <path_to_hwloc.json> {} <iterations> {} <tmp_dir>]",
             Self::OPT_BENCHMARK,
             Self::OPT_HWLOC,
             Self::OPT_ITERATIONS,
+            Self::OPT_TMP_DIR,
         )
     }
 
@@ -48,6 +51,7 @@ impl Args {
         let mut benchmark_str: String = String::new();
         let mut hwloc_file: Option<String> = None;
         let mut iterations: usize = 100;
+        let mut tmp_dir: String = "/tmp/".to_string();
 
         let mut i: usize = 1;
         while i < args.len() {
@@ -81,6 +85,14 @@ impl Args {
                     }
                     iterations = args[i].parse::<usize>()?;
                 },
+                Self::OPT_TMP_DIR => {
+                    i += 1;
+                    if i >= args.len() {
+                        error!("{}", Self::usage());
+                        return Err(anyhow::anyhow!("missing value for: {}", Self::OPT_TMP_DIR));
+                    }
+                    tmp_dir = args[i].clone();
+                },
                 arg => {
                     error!("{}", Self::usage());
                     return Err(anyhow::anyhow!("invalid argument: {arg}"));
@@ -95,6 +107,7 @@ impl Args {
                 benchmark,
                 hwloc_file,
                 iterations,
+                tmp_dir,
             }),
             Err(_) => {
                 error!("{}", Self::usage());
@@ -113,5 +126,9 @@ impl Args {
 
     pub fn iterations(&self) -> usize {
         self.iterations
+    }
+
+    pub fn tmp_dir(&self) -> String {
+        self.tmp_dir.clone()
     }
 }
