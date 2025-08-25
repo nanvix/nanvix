b53f4f8c7e5a91938dadbb80c46db5e73d1c1b75
danbugs <danilochiarlone@gmail.com>
2025-08-25 22:08:12 +0000

    [kernel] E: update hyperlight kernel backend to use new credits offset
    Signed-off-by: danbugs <danilochiarlone@gmail.com>

--- src/kernel/src/hal/platform/hyperlight/peb.rs
+++ src/kernel/src/hal/platform/hyperlight/peb.rs
@@ -49,16 +49,25 @@ pub(crate) static mut GUEST_HANDLE: GuestHandle = GuestHandle::new();
 //==================================================================================================
 
 impl ProcessEnvironmentBlock {
+    /// Initializes the process environment block.
+    ///
+    /// # Safety
+    /// This function is unsafe because it handles a static mutable variable.
     pub unsafe fn init(peb_base: *mut HyperlightPEB) -> Result<(), Error> {
         if GUEST_HANDLE.peb().is_some() {
-            error!("peb already initialized");
-            Err(Error::new(ErrorCode::ResourceBusy, "peb already initialized"))
+            let reason: &'static str = "init: peb already initialized";
+            error!("{}", reason);
+            Err(Error::new(ErrorCode::ResourceBusy, reason))
         } else {
             GUEST_HANDLE = GuestHandle::init(peb_base);
             Ok(())
         }
     }
 
+    /// Sets the guest function dispatch pointer.
+    ///
+    /// # Safety
+    /// This function is unsafe because it dereferences a raw pointer.
     pub unsafe fn set_guest_function_dispatch_ptr(ptr: u64) -> Result<(), Error> {
         match GUEST_HANDLE.peb() {
             Some(peb_ptr) => {
@@ -66,36 +75,66 @@ impl ProcessEnvironmentBlock {
                 Ok(())
             },
             None => {
-                error!("peb not initialized");
-                Err(Error::new(ErrorCode::NoSuchDevice, "peb not initialized"))
+                let reason: &'static str = "set_guest_function_dispatch_ptr: peb not initialized";
+                error!("{}", reason);
+                Err(Error::new(ErrorCode::NoSuchDevice, reason))
             },
         }
     }
 
+    /// Writes a string to the guest's standard output.
+    ///
+    /// # Safety
+    /// This function is unsafe because it uses raw asm under the hood.
     pub unsafe fn puts(message: &str) -> Result<(), Error> {
         debug_print(message);
         Ok(())
     }
 
+    /// Gets the number of credits available to the guest.
+    ///
+    /// # Safety
+    /// This function is unsafe because it dereferences a raw pointer.
+    pub unsafe fn get_credits() -> Result<u64, Error> {
+        match GUEST_HANDLE.peb() {
+            Some(peb_ptr) => Ok((*peb_ptr).credits_value),
+            None => {
+                let reason: &'static str = "get_credits: peb not initialized";
+                error!("{}", reason);
+                Err(Error::new(ErrorCode::NoSuchDevice, reason))
+            },
+        }
+    }
+
+    /// Writes a message to the guest's standard output.
+    ///
+    /// # Safety
+    /// This function is unsafe because it uses a static mutable variable.
     pub unsafe fn vmbus_write(data: &[u8]) -> Result<(), Error> {
+        let failure_reason: &'static str = "vmbus_write: failed to write data";
         let count = GUEST_HANDLE
             .call_host_function::<i32>(
                 "VmbusWrite",
                 Some(Vec::from(&[ParameterValue::VecBytes(Vec::from(data))])),
                 ReturnType::Int,
             )
-            .map_err(|_| Error::new(ErrorCode::IoErr, "failed to write data"))?;
+            .map_err(|_| Error::new(ErrorCode::IoErr, failure_reason))?;
 
         if count != data.len() as i32 {
-            Err(Error::new(ErrorCode::IoErr, "failed to write data"))
+            Err(Error::new(ErrorCode::IoErr, failure_reason))
         } else {
             Ok(())
         }
     }
 
+    /// Reads a message from the guest's standard input.
+    ///
+    /// # Safety
+    /// This function is unsafe because it uses a static mutable variable.
     pub unsafe fn vmbus_read() -> Result<Vec<u8>, Error> {
+        let failure_reason: &'static str = "vmbus_read: failed to read data";
         GUEST_HANDLE
             .call_host_function::<Vec<u8>>("VmbusRead", None, ReturnType::VecBytes)
-            .map_err(|_| Error::new(ErrorCode::IoErr, "failed to read data"))
+            .map_err(|_| Error::new(ErrorCode::IoErr, failure_reason))
     }
 }
