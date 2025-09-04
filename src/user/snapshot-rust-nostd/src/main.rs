5cbf02db79d620071ad37cb10bfd9bab67edf75e
LucasGdosR <lucas.gribel.reis@gmail.com>
2025-09-04 17:24:59 -0300

    [user] E: Simplifying the program

--- src/user/snapshot-rust-nostd/src/main.rs
+++ src/user/snapshot-rust-nostd/src/main.rs
@@ -13,8 +13,13 @@
 // Imports
 //==================================================================================================
 
+extern crate alloc;
 extern crate nvx;
 
+use alloc::{
+    format,
+    string::String,
+};
 use sys::{
     error::Error,
     kcall::debug,
@@ -27,32 +32,13 @@ use sys::{
 #[unsafe(no_mangle)]
 pub fn main() -> Result<(), Error> {
     const ITERATIONS_STEP: u64 = 500_000_000;
-    let mut buffer = [0u8; 32];
 
     for i in 0..u64::MAX {
         if i % ITERATIONS_STEP == 0 {
-            let string: &str = count_to_string(i / ITERATIONS_STEP, &mut buffer);
+            let string: String = format!("{}\n", i / ITERATIONS_STEP);
             let _ = debug::debug(string.as_ptr(), string.len());
         }
     }
 
     Ok(())
-}
-
-fn count_to_string(mut num: u64, buf: &mut [u8]) -> &str {
-    if num == 0 {
-        buf[0] = b'0';
-        buf[1] = b'\n';
-        return unsafe { core::str::from_utf8_unchecked(&buf[0..2]) };
-    }
-
-    let mut pos = buf.len() - 1;
-    buf[pos] = b'\n';
-    while num > 0 {
-        pos -= 1;
-        buf[pos] = (num % 10) as u8 + b'0';
-        num /= 10;
-    }
-
-    unsafe { core::str::from_utf8_unchecked(&buf[pos..]) }
 }
