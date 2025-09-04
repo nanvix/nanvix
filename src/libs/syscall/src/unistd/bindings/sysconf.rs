d177b3b88421060a558b26c7b16039d70ee338be
Pedro Henrique Penna <ppenna@microsoft.com>
2025-09-04 12:25:41 -0700

    [libs] F: Initial Impl. for sysconf()

--- src/libs/syscall/src/unistd/bindings/sysconf.rs
+++ src/libs/syscall/src/unistd/bindings/sysconf.rs
@@ -5,6 +5,13 @@
 // Imports
 //==================================================================================================
 
+use crate::{
+    errno::__errno_location,
+    safe::sys::{
+        config::SysConfigName,
+        System,
+    },
+};
 use ::sysapi::ffi::{
     c_int,
     c_long,
@@ -14,10 +21,53 @@ use ::sysapi::ffi::{
 // Standalone Functions
 //==================================================================================================
 
+///
+/// # Description
+///
+/// Gets the value of a system configuration variable.
+///
+/// # Parameters
+///
+/// - `name` - Name of the system configuration variable to query.
+///
+/// # Return Value
+///
+/// On success, this function returns the value of the specified system configuration variable.
+/// Otherwise, it returns -1 and sets `errno` to indicate the error.
+///
+/// # Errors
+///
+/// - [`sys::error::ErrorCode::InvalidArgument`] if the specified system configuration name is invalid.
+/// - [`sys::error::ErrorCode::OperationNotSupported`] if the specified system configuration name is not
+///   supported.
+/// - [`sys::error::ErrorCode::ValueOutOfRange`] if the value of the specified system configuration variable
+///   cannot be represented by the return type.
+///
 #[unsafe(no_mangle)]
 pub extern "C" fn sysconf(name: c_int) -> c_long {
     ::syslog::trace!("sysconf(): name={name:?}");
-    // TODO: https://github.com/nanvix/nanvix/issues/342
-    ::syslog::error!("sysconf(): not implemented");
-    0
+
+    // Attempt to convert `name` to `SysConfigName`.
+    let name: SysConfigName = match SysConfigName::try_from(name) {
+        Ok(name) => name,
+        Err(error) => {
+            ::syslog::error!("sysconf(): {error:?}");
+            unsafe {
+                *__errno_location() = error.code.get();
+            }
+            return -1;
+        },
+    };
+
+    // Attempt to get system configuration value and check for errors.
+    match System::config(name) {
+        Ok(value) => value.into(),
+        Err(error) => {
+            ::syslog::error!("sysconf(): {error:?}");
+            unsafe {
+                *__errno_location() = error.code.get();
+            }
+            -1
+        },
+    }
 }
