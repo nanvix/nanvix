4ec74dfe086d5bc269a8ba30979d922624f3a688
Pedro Henrique Penna <ppenna@microsoft.com>
2025-09-05 10:52:07 -0700

    [kernel] E: Set/Clear Task Switched

--- src/kernel/src/hal/arch/x86/mod.rs
+++ src/kernel/src/hal/arch/x86/mod.rs
@@ -2,6 +2,12 @@
 // Licensed under the MIT License.
 
 //==================================================================================================
+// Lint Configuration
+//==================================================================================================
+
+#![cfg_attr(not(feature = "sse"), allow(dead_code))]
+
+//==================================================================================================
 // Modules
 //==================================================================================================
 
@@ -22,6 +28,10 @@ use crate::hal::{
         IoPortAllocator,
     },
     platform::madt::MadtInfo,
+};
+use ::arch::cpu::cr0::{
+    Cr0Register,
+    TaskSwitchedFlag,
 };
 use ::sys::error::Error;
 
@@ -59,6 +69,44 @@ pub struct Arch {
 //==================================================================================================
 // Standalone Functions
 //==================================================================================================
+
+///
+/// # Description
+///
+/// Clears the CR0.TS flag to enable FPU/SSE instructions.
+///
+/// # Safety
+///
+/// It is unsafe to call this function because it executes privileged instructions.
+///
+/// It is safe to call this function if the following conditions are met:
+/// - Calls to this function are synchronized.
+/// - The caller runs at processor privilege level 0.
+///
+pub unsafe fn clear_task_switched() {
+    let mut cr0: Cr0Register = Cr0Register::read();
+    cr0.task_switched = TaskSwitchedFlag::Clear;
+    cr0.write();
+}
+
+///
+/// # Description
+///
+/// Sets the CR0.TS flag to disable FPU/SSE instructions.
+///
+/// # Safety
+///
+/// It is unsafe to call this function because it executes privileged instructions.
+///
+/// It is safe to call this function if the following conditions are met:
+/// - Calls to this function are synchronized.
+/// - The caller runs at processor privilege level 0.
+///
+pub unsafe fn set_task_switched() {
+    let mut cr0: Cr0Register = Cr0Register::read();
+    cr0.task_switched = TaskSwitchedFlag::Set;
+    cr0.write();
+}
 
 pub fn init(
     ioports: &mut IoPortAllocator,
