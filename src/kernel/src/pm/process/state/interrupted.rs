89a295f4c4d31133c73327c96c62043ee873064a
Pedro Henrique Penna <ppenna@microsoft.com>
2025-09-05 15:04:14 -0700

    [build] E: Drop SSE Feature

--- src/kernel/src/pm/process/state/interrupted.rs
+++ src/kernel/src/pm/process/state/interrupted.rs
@@ -2,12 +2,6 @@
 // Licensed under the MIT License.
 
 //==================================================================================================
-// Lint Configuration
-//==================================================================================================
-
-#![cfg_attr(not(feature = "sse"), allow(unused_imports))]
-
-//==================================================================================================
 // Imports
 //==================================================================================================
 
@@ -154,7 +148,6 @@ impl InterruptedProcess {
     /// If a thread that matches the specified thread identifier is found, then a mutable reference
     /// to it is returned. Otherwise, empty is returned instead.
     ///
-    #[cfg(feature = "sse")]
     pub fn find_thread_mut(&mut self, tid: ThreadIdentifier) -> Option<ThreadRefMut> {
         // Search in the list of interrupted threads.
         if let Some(thread) = self
