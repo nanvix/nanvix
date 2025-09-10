88ebc1d1a527563a30ac43df9c4c233b27a448dd
Pedro Henrique Penna <ppenna@microsoft.com>
2025-09-10 07:12:08 -0700

    [libs] E: Dummy Impl pthread_rwlock_init()

--- src/libs/syscall/src/pthread/bindings/mod.rs
+++ src/libs/syscall/src/pthread/bindings/mod.rs
@@ -26,6 +26,7 @@ pub mod pthread_mutex_destroy;
 pub mod pthread_mutex_init;
 pub mod pthread_mutex_lock;
 pub mod pthread_mutex_unlock;
+pub mod pthread_rwlock_init;
 pub mod pthread_rwlock_rdlock;
 pub mod pthread_rwlock_unlock;
 pub mod pthread_rwlock_wrlock;
