fbd0d69dbe7add61da2e7e65c5ad2abd2988ae40
Pedro Henrique Penna <ppenna@microsoft.com>
2025-09-13 12:30:57 -0700

    [scripts] E: clone_repo()

--- scripts/common/utils.sh
+++ scripts/common/utils.sh
@@ -101,3 +101,78 @@ get_value_from_toml() {
     )"
     [[ -n "$val" ]] && printf '%s' "$val" || exit 1
 }
+
+#
+# Description
+#
+#   Clones a repository.
+#
+# Parameters
+#
+#   $1 - Repository URL.
+#   $2 - Repository base path.
+#   $3 - Commit to checkout.
+#
+# Return Value
+#
+#   - On success, this function returns zero.
+#   - On error, this function returns non-zero.
+#
+# Usage Example
+#
+#   clone_repo "https://github.com/nanvix/gcc" "/path/to/dir" "commit_id"
+#
+clone_repo() {
+    local repository_url=$1
+    local repository_basepath=$2
+    local commit=$3
+
+    # Check if repository URL is empty.
+    if [[ -z "${repository_url}" ]]; then
+        print_error "Repository URL is empty."
+        return 1
+    fi
+
+    # Check if repository base path is empty.
+    if [[ -z "${repository_basepath}" ]]; then
+        print_error "Repository path is empty."
+        return 1
+    fi
+
+    # Create repository base path if it does not exist.
+    mkdir -p "${repository_basepath}" || {
+        print_error "Failed to create directory '${repository_basepath}'."
+        return 1
+    }
+
+    # Infer repository name from repository url.
+    local repository_name
+    repository_name=$(basename -s .git "${repository_url}")
+
+    local repository_path="${repository_basepath}/${repository_name}"
+
+    # Clone repository if it does not exist, else fetch latest changes.
+    if [[ ! -d "${repository_path}/.git" ]]; then
+        git clone "${repository_url}" "${repository_path}" || {
+            print_error "Failed to clone repository '${repository_url}' to '${repository_path}'."
+            return 1
+        }
+    else
+        git -C "${repository_path}" fetch origin || {
+            print_error "Failed to fetch latest changes for repository '${repository_url}'."
+            return 1
+        }
+        git -C "${repository_path}" reset --hard || {
+            print_error "Failed to reset repository '${repository_url}'."
+            return 1
+        }
+    fi
+
+    # Checkout to the specified commit.
+    git -C "${repository_path}" checkout "${commit}" || {
+        print_error "Failed to checkout to commit '${commit}' in repository '${repository_url}'."
+        return 1
+    }
+
+    return 0
+}
