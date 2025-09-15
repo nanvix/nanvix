b41dbdadd6bc8ce09c4a5571b130249137c4eac9
Pedro Henrique Penna <ppenna@microsoft.com>
2025-09-15 08:13:08 -0700

    [tests] F: System Test for malloc()/free()

--- src/tests/memory-c/common.h
+++ src/tests/memory-c/common.h
@@ -12,4 +12,7 @@ extern void test_sbrk(void);
 // Tests whether we can map and unmap memory using `mmap()` and `munmap()`.
 extern void test_mmap_munmap(void);
 
+// Tests whether we can allocate and free memory using `malloc()` and `free()`.
+extern void test_malloc_free(void);
+
 #endif
