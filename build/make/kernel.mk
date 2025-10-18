7896145cbe2e801ec12a0f4f1e8f639ee6ea1791
Pedro Henrique Penna <ppenna@microsoft.com>
2025-10-18 16:20:58 -0700

    [build] B: Fix Feature Concatenation

--- build/make/kernel.mk
+++ build/make/kernel.mk
@@ -1,12 +1,16 @@
 # Copyright(c) The Maintainers of Nanvix.
 # Licensed under the MIT License.
 
+KERNEL_FEATURES := $(MACHINE) $(LOG_LEVEL)
+KERNEL_FEATURES := $(strip $(KERNEL_FEATURES))
+KERNEL_CARGO_FEATURES := $(if $(KERNEL_FEATURES),--features "$(KERNEL_FEATURES)")
+
 all-kernel: init
-	$(KERNEL_CARGO_BUILD_CMD) $(KERNEL_CARGO_FEATURES) --features $(LOG_LEVEL) -p kernel
+	$(KERNEL_CARGO_BUILD_CMD) $(KERNEL_CARGO_FEATURES) -p kernel
 	$(CP_CMD) $(OBJECTS_DIR)/$(TARGET)-kernel/$(BUILD_MODE)/kernel.elf $(BINARIES_DIR)/kernel.elf
 
 check-kernel:
-	$(KERNEL_CARGO_CHECK_CMD) $(KERNEL_CARGO_FEATURES) --features $(LOG_LEVEL) -p kernel
+	$(KERNEL_CARGO_CHECK_CMD) $(KERNEL_CARGO_FEATURES) -p kernel
 
 format-kernel:
 	$(KERNEL_CARGO_FMT_CMD) -p kernel
@@ -19,7 +23,7 @@ clean-kernel:
 	$(RM_CMD) $(BINARIES_DIR)/kernel.elf
 
 rust-lint-kernel:
-	$(KERNEL_CARGO_CLIPPY_CMD) $(KERNEL_CARGO_FEATURES) --features $(LOG_LEVEL) -p kernel --fix --allow-dirty
+	$(KERNEL_CARGO_CLIPPY_CMD) $(KERNEL_CARGO_FEATURES) -p kernel --fix --allow-dirty
 
 rust-lint-check-kernel:
-	$(KERNEL_CARGO_CLIPPY_CMD) $(KERNEL_CARGO_FEATURES) --features $(LOG_LEVEL) -p kernel
+	$(KERNEL_CARGO_CLIPPY_CMD) $(KERNEL_CARGO_FEATURES) -p kernel
