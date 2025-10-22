b750f884935c5464c39ceb7e7a6668574f1a7a09
Carlos Segarra <carlos@carlossegarra.com>
2025-10-22 08:51:22 +0000

    [build] W: Add L2 Test Blocklist
    Signed-off-by: Carlos Segarra <carlos@carlossegarra.com>

--- build/make/test/system.mk
+++ build/make/test/system.mk
@@ -3,15 +3,31 @@
 
 comma:=,
 
+# List of supported functions in the L2 VM.
+# FIXME (#986): modify tests such that they don't rely on linuxd being invoked
+# from the root of the source tree.
+MICROVM_L2_BLOCKLIST := dlfcn-c file-c file-rust python3 qjs
+
 define TEST_RULE
 test-$(2): all
 ifneq ($(strip $(filter $(MACHINE),microvm hyperlight)),)
+ifeq ($(L2_VM),yes)
 	@if [ ! -f "$(1)/$(2)$(3)" ]; then \
-		echo "\033[31mWarning: $(1)/$(2)$(3) missing, skipping test.\033[0m"; \
-		else \
-		echo "Running test $(2)..." ; \
-			$(SCRIPTS_DIR)/test-nanvixd.sh $(NANVIXD_SOCKADDR) $(1)/$(2)$(3) $(4) $(5) $(6) $(TIMEOUT); \
+		printf "\033[31mWarning: %s missing, skipping test.\033[0m\n" "$(1)/$(2)$(3)"; \
+	elif printf '%s\n' $(MICROVM_L2_BLOCKLIST) | grep -Fxq -- "$(2)"; then \
+		printf "\033[31mWarning: Skipping %s on L2 (not supported, see #986).\033[0m\n" "$(2)"; \
+	else \
+		echo "Running test $(2)..."; \
+		$(SCRIPTS_DIR)/test-nanvixd.sh $(NANVIXD_SOCKADDR) "$(1)/$(2)$(3)" $(4) $(5) $(6) $(TIMEOUT); \
 	fi
+else
+	@if [ ! -f "$(1)/$(2)$(3)" ]; then \
+		printf "\033[31mWarning: %s missing, skipping test.\033[0m\n" "$(1)/$(2)$(3)"; \
+	else \
+		echo "Running test $(2)..."; \
+		$(SCRIPTS_DIR)/test-nanvixd.sh $(NANVIXD_SOCKADDR) "$(1)/$(2)$(3)" $(4) $(5) $(6) $(TIMEOUT); \
+	fi
+endif
 endif
 endef
 
