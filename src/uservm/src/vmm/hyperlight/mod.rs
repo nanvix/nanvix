8c2cdb70fb300c6f8da33155003cae5ab85907b4
Pedro Henrique Penna <ppenna@microsoft.com>
2025-10-26 11:26:53 -0700

    [uservm] E: Improving Error Logs

--- src/uservm/src/vmm/hyperlight/mod.rs
+++ src/uservm/src/vmm/hyperlight/mod.rs
@@ -207,9 +207,9 @@ impl Vmm {
                     }
                 },
                 Err(err) => {
-                    let reason: String = format!("failed to read initrd file: {:?}", err);
-                    error!("initrd(): {}", reason);
-                    return Err(anyhow::anyhow!("initrd(): {}", reason));
+                    let reason: String = format!("failed to read initird file {err:?}");
+                    error!("initrd(): {reason} (args={args:?})");
+                    return Err(anyhow::anyhow!("{reason}"));
                 },
             }
         } else {
