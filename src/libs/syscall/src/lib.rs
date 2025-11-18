cdb62338775042d1b177b2ecf8bc295c75fa8498
Pedro Henrique Penna <ppenna@microsoft.com>
2025-11-18 08:02:27 -0800

    [ssycall] E: Gate Features with dlfcn

--- src/libs/syscall/src/lib.rs
+++ src/libs/syscall/src/lib.rs
@@ -9,8 +9,8 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 #![feature(never_type)] // pthread requires this.
 #![feature(c_variadic)] // fcntl requires this.
-#![feature(btree_extract_if)] // dlfcn requires this.
-#![feature(strict_overflow_ops)]
+#![cfg_attr(feature = "dlfcn", feature(btree_extract_if))]
+#![cfg_attr(feature = "dlfcn", feature(strict_overflow_ops))]
 
 //==================================================================================================
 // Modules
