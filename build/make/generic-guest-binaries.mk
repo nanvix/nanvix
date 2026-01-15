67a6c3bb4fa6a904894023376015700f8960559e
Pedro Henrique Penna <ppenna@microsoft.com>
2026-01-15 09:27:24 -0800

    [build] E: MAKE_NO_PRINT
    Add MAKE_NO_PRINT helper to suppress directory printing in recursive
    make.

--- build/make/generic-guest-binaries.mk
+++ build/make/generic-guest-binaries.mk
@@ -33,9 +33,9 @@ endef
 $(foreach target,$(ALL_GUEST_BINARIES),$(eval $(call GUEST_BINARY_RULES,$(target))))
 
 all-guest-binaries: $(foreach target,$(ALL_GUEST_BINARIES),all-guest-binaries-$(target))
-	$(MAKE) -C $(SOURCES_DIR)/benchmarks all
-	$(MAKE) -C $(SOURCES_DIR)/user all
-	$(MAKE) -C $(SOURCES_DIR)/tests all
+	$(MAKE_QUIET) -C $(SOURCES_DIR)/benchmarks all
+	$(MAKE_QUIET) -C $(SOURCES_DIR)/user all
+	$(MAKE_QUIET) -C $(SOURCES_DIR)/tests all
 
 check-guest-binaries: $(foreach target,$(ALL_GUEST_BINARIES),check-guest-binaries-$(target))
 
@@ -44,9 +44,9 @@ format-guest-binaries: $(foreach target,$(ALL_GUEST_BINARIES),format-guest-binar
 format-check-guest-binaries: $(foreach target,$(ALL_GUEST_BINARIES),format-check-guest-binaries-$(target))
 
 clean-guest-binaries: $(foreach target,$(ALL_GUEST_BINARIES),clean-guest-binaries-$(target))
-	$(MAKE) -C $(SOURCES_DIR)/benchmarks clean
-	$(MAKE) -C $(SOURCES_DIR)/user clean
-	$(MAKE) -C $(SOURCES_DIR)/tests clean
+	$(MAKE_QUIET) -C $(SOURCES_DIR)/benchmarks clean
+	$(MAKE_QUIET) -C $(SOURCES_DIR)/user clean
+	$(MAKE_QUIET) -C $(SOURCES_DIR)/tests clean
 
 rust-lint-guest-binaries: $(foreach target,$(ALL_GUEST_BINARIES),rust-lint-guest-binaries-$(target))
 
