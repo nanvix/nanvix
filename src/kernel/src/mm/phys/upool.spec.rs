3caa872ca84dd05de67c05973f9736cfa06afd40
Ruize Tang <1466040111@qq.com>
2026-04-24 19:45:17 +0800

    [verus] verify FAIL: kernel::mm::phys::upool (cached (no recompilation), —)

--- src/kernel/src/mm/phys/upool.spec.rs
+++ src/kernel/src/mm/phys/upool.spec.rs
@@ -1,1 +1,17 @@
-verus! { } // verus!
+verus! {
+
+impl View for UserFrame {
+    type V = int;
+
+    closed spec fn view(&self) -> int {
+        self.addr@
+    }
+}
+
+impl UserFrame {
+    pub open spec fn inv(&self) -> bool {
+        self.addr.inv()
+    }
+}
+
+} // verus!
