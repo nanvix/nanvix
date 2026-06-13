cbbd1b151e4e5d8321403427f86a34b68ea62161
Ruize Tang <1466040111@qq.com>
2026-06-14 00:36:48 +0800

    [verus] verify FAIL: kernel::mm::phys (0 verified, compilation/setup error (verus did not run))

--- src/kernel/src/mm/phys/mod.rs
+++ src/kernel/src/mm/phys/mod.rs
@@ -173,7 +173,7 @@ pub fn init(
     info!("initializing the frame allocator ...");
     // Safety: called exactly once during single-threaded boot.
     unsafe { frame::init(physical_memory_layout)? };
-    proof {
+    proof!{
         lemma_frame_initialized();
     }
     book_physical_memory_regions(physical_memory_regions)?;
@@ -187,7 +187,7 @@ pub fn init(
     // Initialize physical memory manager singleton.
     info!("initializing the physical memory manager ...");
     PhysMemoryManager::init(upool)?;
-    proof {
+    proof!{
         lemma_manager_ready();
     }
 
