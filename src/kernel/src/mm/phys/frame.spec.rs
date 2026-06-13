c18fbdfb113dff0aee856cfe37e1e1f2a9daaea5
Ruize Tang <1466040111@qq.com>
2026-06-14 03:58:59 +0800

    [verus] verify FAIL: kernel::mm::phys (0 verified, compilation/setup error (verus did not run))

--- src/kernel/src/mm/phys/frame.spec.rs
+++ src/kernel/src/mm/phys/frame.spec.rs
@@ -8,4 +8,58 @@ impl Inner {
     }
 }
 
+// =================================================================================================
+// Dependency contracts for not-yet-verified modules.
+//
+// The functions below live in the `arch` crate and in the kernel HAL
+// address/region layer, neither of which is verified yet. They are given
+// trusted external specifications here so that the frame-allocator bodies can be
+// translated by Verus. These declarations are placeholders: when the underlying
+// modules are verified, their real specifications will supersede these and the
+// declarations below will be removed.
+// =================================================================================================
+
+#[verifier::external_type_specification]
+#[verifier::external_body]
+pub struct ExFrameNumber(::arch::mem::paging::FrameNumber);
+
+pub assume_specification[ ::arch::mem::FRAME_SIZE ] -> (result: usize)
+    ensures
+        result == crate::hal::mem::types::address::frame::spec_page_size(),
+;
+
+pub assume_specification[ ::arch::mem::paging::FrameNumber::from_raw_value ](_v: usize)
+    -> Option<::arch::mem::paging::FrameNumber>;
+
+pub assume_specification[ ::arch::mem::paging::FrameNumber::into_raw_value ](_f: ::arch::mem::paging::FrameNumber)
+    -> usize;
+
+pub assume_specification[ crate::hal::mem::types::address::frame::FrameAddress::from_frame_number ](
+    _f: ::arch::mem::paging::FrameNumber,
+) -> Result<crate::hal::mem::types::address::frame::FrameAddress, ::sys::error::Error>;
+
+pub assume_specification[ crate::hal::mem::types::address::frame::FrameAddress::into_frame_number ](
+    _a: crate::hal::mem::types::address::frame::FrameAddress,
+) -> ::arch::mem::paging::FrameNumber;
+
+pub assume_specification[ crate::hal::mem::types::address::phys::PhysicalAddress::into_frame_number ](
+    _a: crate::hal::mem::types::address::phys::PhysicalAddress,
+) -> ::arch::mem::paging::FrameNumber;
+
+pub assume_specification<T: ::sys::mm::Address>[ crate::hal::mem::types::region::TruncatedMemoryRegion::<T>::start ](
+    _r: &crate::hal::mem::types::region::TruncatedMemoryRegion<T>,
+) -> crate::hal::mem::types::address::aligned::page::PageAligned<T>;
+
+pub assume_specification<T: ::sys::mm::Address>[ crate::hal::mem::types::region::TruncatedMemoryRegion::<T>::size ](
+    _r: &crate::hal::mem::types::region::TruncatedMemoryRegion<T>,
+) -> usize;
+
+pub assume_specification<T: ::sys::mm::Address>[ <crate::hal::mem::types::address::aligned::page::PageAligned<T> as ::sys::mm::Address>::into_raw_value ](
+    _a: crate::hal::mem::types::address::aligned::page::PageAligned<T>,
+) -> usize;
+
+pub assume_specification<T: ::sys::mm::Address>[ <crate::hal::mem::types::address::aligned::page::PageAligned<T> as ::core::ops::Deref>::deref ](
+    _a: &crate::hal::mem::types::address::aligned::page::PageAligned<T>,
+) -> &T;
+
 }
