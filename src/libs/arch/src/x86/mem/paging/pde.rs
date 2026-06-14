2d962e77296a27761fd73933bcbd6913c0ea5e24
Ruize Tang <1466040111@qq.com>
2026-06-15 01:47:39 +0800

    [verus] verify FAIL: arch::all (0 verified, compilation/setup error (verus did not run))

--- src/libs/arch/src/x86/mem/paging/pde.rs
+++ src/libs/arch/src/x86/mem/paging/pde.rs
@@ -80,7 +80,6 @@ impl PageDirectoryEntryFlags {
     ///
     /// A [`PageDirectoryEntryFlags`].
     ///
-    #[cfg_attr(verus_keep_ghost, allow(unused, verus_impl_method_marker))]
     #[verus_spec(result =>
         ensures
             result@ == spec_pde_flags_new(
@@ -304,7 +303,6 @@ impl PageDirectoryEntry {
     ///
     /// A [`PageDirectoryEntry`].
     ///
-    #[cfg_attr(verus_keep_ghost, allow(unused, verus_impl_method_marker))]
     #[verus_spec(result =>
         ensures
             result@ == spec_pde_new(flags@, frame@),
