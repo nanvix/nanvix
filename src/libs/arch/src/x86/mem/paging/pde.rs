a2dbbed6c3fe79367f9d2c04fee545bc059aedb1
Ruize Tang <1466040111@qq.com>
2026-06-15 02:00:47 +0800

    [verus] verify FAIL: arch::all (46 verified, 1 errors)

--- src/libs/arch/src/x86/mem/paging/pde.rs
+++ src/libs/arch/src/x86/mem/paging/pde.rs
@@ -418,9 +418,7 @@ impl PageDirectoryEntry {
             result as int % (crate::mem::FRAME_SIZE as int) == 0,
     )]
     pub fn frame_address(&self) -> usize {
-        let raw: usize = self.frame.into_raw_value();
-        proof! { lemma_frame_address(raw); }
-        raw << crate::mem::FRAME_SHIFT
+        self.frame.into_raw_value() << crate::mem::FRAME_SHIFT
     }
 
     ///
