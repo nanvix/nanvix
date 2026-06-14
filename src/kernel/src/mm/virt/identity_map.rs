4668208c5e6443b937896fc0ad43cc4838839fdc
Ruize Tang <1466040111@qq.com>
2026-06-15 02:54:43 +0800

    [verus] verify FAIL: kernel::mm::virt::identity_map (0 verified, compilation/setup error (verus did not run))

--- src/kernel/src/mm/virt/identity_map.rs
+++ src/kernel/src/mm/virt/identity_map.rs
@@ -629,7 +629,7 @@ fn ensure_pte(
     pte_idx: TableIndex,
     phys_addr: usize,
 ) -> Result<(), Error> {
-    proof! { admit(); }
+    proof! { use_type_invariant(pte_idx); }
     let pte: PageTableEntry = unsafe { pt.read(pte_idx) }.ok_or_else(|| {
         let reason: &str = "invalid PTE read from page table";
         #[cfg(not(verus_keep_ghost))]
