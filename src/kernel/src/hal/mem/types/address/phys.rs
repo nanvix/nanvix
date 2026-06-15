16bdf6096144834c9c70d0592fb0d070b673c486
Ruize Tang <1466040111@qq.com>
2026-06-15 09:24:59 +0800

    [verus] verify FAIL: kernel::hal::mem::types::address::phys (0 verified, compilation/setup error (verus did not run))

--- src/kernel/src/hal/mem/types/address/phys.rs
+++ src/kernel/src/hal/mem/types/address/phys.rs
@@ -141,6 +141,7 @@ impl PhysicalAddress {
     )]
     pub fn from_number(frame: FrameNumber) -> Self {
         proof! {
+            use_type_invariant(frame);
             lemma_from_number_no_overflow(frame);
         }
         let addr: usize = frame.into_raw_value() * mem::FRAME_SIZE;
