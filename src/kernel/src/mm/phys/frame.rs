041d320bbd6bda7c88fff52531e17d52421c7d55
Ruize Tang <1466040111@qq.com>
2026-06-15 08:09:41 +0800

    [verus] verify FAIL: kernel::mm::phys (46 verified, 1 errors)

--- src/kernel/src/mm/phys/frame.rs
+++ src/kernel/src/mm/phys/frame.rs
@@ -533,7 +533,6 @@ impl Inner {
             ),
     )]
     fn is_covered(&self, phys_addr: PageAligned<PhysicalAddress>) -> bool {
-        proof! { admit(); }
         let frame_number: usize = phys_addr.into_frame_number().into_raw_value();
         frame_number < self.bitmap.number_of_bits()
     }
